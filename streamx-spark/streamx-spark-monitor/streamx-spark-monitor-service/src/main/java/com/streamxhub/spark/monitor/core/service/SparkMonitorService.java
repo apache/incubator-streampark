2dbf0884b00e70ed2391f3592bdbc63715b933ff
benjobs <benjobs@qq.com>
2019-06-26 19:52:46 +0800

    监控后台&前端

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/service/SparkMonitorService.java
+++ streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/service/SparkMonitorService.java
@@ -1,9 +1,16 @@
 package com.streamxhub.spark.monitor.core.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.streamxhub.spark.monitor.common.domain.QueryRequest;
+import com.streamxhub.spark.monitor.core.domain.SparkMonitor;
+
 import java.util.Map;
 
 public interface SparkMonitorService {
     void publish(String id, Map<String, String> confMap);
 
     void shutdown(String id, Map<String, String> confMap);
+
+    IPage<SparkMonitor> getMonitor(SparkMonitor sparkMonitor,QueryRequest request);
+
 }
