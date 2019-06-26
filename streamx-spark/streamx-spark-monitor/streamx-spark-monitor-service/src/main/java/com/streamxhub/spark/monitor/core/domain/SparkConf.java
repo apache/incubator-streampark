2ddf718ae1600a1a5b385c980fc593f9905bcded
benjobs <benjobs@qq.com>
2019-06-26 14:57:37 +0800

    监控后台&前端

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/domain/SparkConf.java
+++ streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/domain/SparkConf.java
@@ -6,7 +6,6 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
