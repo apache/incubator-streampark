0f3c8a15e970a2667f2a0cc46087fd91baf808f7
benjobs <benjobs@qq.com>
2019-07-01 21:14:21 +0800

    监控后台&前端

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/service/SparkConfRecordService.java
+++ streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/core/service/SparkConfRecordService.java
@@ -3,10 +3,14 @@ package com.streamxhub.spark.monitor.core.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.streamxhub.spark.monitor.core.domain.SparkConfRecord;
 
+import java.util.List;
+
 
 /**
  * @author benjobs
  */
 public interface SparkConfRecordService extends IService<SparkConfRecord> {
     void delete(String myId);
+
+    List<SparkConfRecord> getRecords(String myId);
 }
