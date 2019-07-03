110d8a17388db337387786b3aed06bf30e1b8a57
benjobs <benjobs@qq.com>
2019-07-03 17:56:23 +0800

    监控后台&前端

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-api/src/main/scala/com/streamxhub/spark/monitor/api/Const.scala
+++ streamx-spark/streamx-spark-monitor/streamx-spark-monitor-api/src/main/scala/com/streamxhub/spark/monitor/api/Const.scala
@@ -23,9 +23,9 @@ package com.streamxhub.spark.monitor.api
 
 object Const {
 
-  val SPARK_CONF_PATH_PREFIX = "/StreamX/spark/conf"
+  val SPARK_CONF_PATH_PREFIX = "/streamx/spark/conf"
 
-  val SPARK_MONITOR_PATH_PREFIX = "/StreamX/spark/monitor"
+  val SPARK_MONITOR_PATH_PREFIX = "/streamx/spark/monitor"
 
   val SPARK_CONF_TYPE_REGEXP = "\n(\\s+|)spark\\.main\\.class"
 
