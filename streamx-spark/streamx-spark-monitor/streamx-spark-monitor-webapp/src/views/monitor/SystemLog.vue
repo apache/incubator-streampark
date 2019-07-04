386225ba90264d9c07934eaed6a3b7171a0b0260
benjobs <benjobs@qq.com>
2019-07-04 14:47:24 +0800

    code Format

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-webapp/src/views/monitor/SystemLog.vue
+++ streamx-spark/streamx-spark-monitor/streamx-spark-monitor-webapp/src/views/monitor/SystemLog.vue
@@ -93,224 +93,208 @@
 <script>
     import RangeDate from '@/components/datetime/RangeDate'
 
-    export default {
-        name: 'SystemLog',
-        components: {RangeDate},
-        data() {
-            return {
-                advanced: false,
-                dataSource: [],
-                sortedInfo: null,
-                paginationInfo: null,
-                selectedRowKeys: [],
-                queryParams: {},
-                pagination: {
-                    pageSizeOptions: ['10', '20', '30', '40', '100'],
-                    defaultCurrent: 1,
-                    defaultPageSize: 10,
-                    showQuickJumper: true,
-                    showSizeChanger: true,
-                    showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
-                },
-                loading: false
-            }
-        },
-        computed: {
-            columns() {
-                let {sortedInfo} = this
-                sortedInfo = sortedInfo || {}
-                return [{
-                    title: '操作人',
-                    dataIndex: 'username'
-                }, {
-                    title: '操作描述',
-                    dataIndex: 'operation'
-                }, {
-                    title: '耗时',
-                    dataIndex: 'time',
-                    customRender: (text, row, index) => {
-                        if (text < 500) {
-                            return
-                        <
-                            a - tag
-                            color = "green" > {text}
-                            ms < /a-tag>
-                        } else if (text < 1000) {
-                            return
-                        <
-                            a - tag
-                            color = "cyan" > {text}
-                            ms < /a-tag>
-                        } else if (text < 1500) {
-                            return
-                        <
-                            a - tag
-                            color = "orange" > {text}
-                            ms < /a-tag>
-                        } else {
-                            return
-                        <
-                            a - tag
-                            color = "red" > {text}
-                            ms < /a-tag>
-                        }
-                    },
-                    sorter: true,
-                    sortOrder: sortedInfo.columnKey === 'time' && sortedInfo.order
-                }, {
-                    title: '执行方法',
-                    dataIndex: 'method',
-                    scopedSlots: {customRender: 'method'}
-                }, {
-                    title: '方法参数',
-                    dataIndex: 'params',
-                    scopedSlots: {customRender: 'params'},
-                    width: 100
-                }, {
-                    title: 'IP地址',
-                    dataIndex: 'ip'
-                }, {
-                    title: '操作地点',
-                    dataIndex: 'location'
-                }, {
-                    title: '操作时间',
-                    dataIndex: 'createTime',
-                    sorter: true,
-                    sortOrder: sortedInfo.columnKey === 'createTime' && sortedInfo.order
-                }]
-            }
-        },
-        mounted() {
-            this.fetch()
-        },
-        methods: {
-            onSelectChange(selectedRowKeys) {
-                this.selectedRowKeys = selectedRowKeys
-            },
-            toggleAdvanced() {
-                this.advanced = !this.advanced
-                if (!this.advanced) {
-                    this.queryParams.createTimeFrom = ''
-                    this.queryParams.createTimeTo = ''
-                    this.queryParams.location = ''
-                }
-            },
-            handleDateChange(value) {
-                if (value) {
-                    this.queryParams.createTimeFrom = value[0]
-                    this.queryParams.createTimeTo = value[1]
-                }
-            },
-            batchDelete() {
-                if (!this.selectedRowKeys.length) {
-                    this.$message.warning('请选择需要删除的记录')
-                    return
-                }
-                let that = this
-                this.$confirm({
-                    title: '确定删除所选中的记录?',
-                    content: '当您点击确定按钮后，这些记录将会被彻底删除',
-                    centered: true,
-                    onOk() {
-                        let ids = []
-                        for (let key of that.selectedRowKeys) {
-                            ids.push(that.dataSource[key].id)
-                        }
-                        that.$delete('log/' + ids.join(',')).then(() => {
-                            that.$message.success('删除成功')
-                            that.selectedRowKeys = []
-                            that.search()
-                        })
-                    },
-                    onCancel() {
-                        that.selectedRowKeys = []
-                    }
-                })
-            },
-            exprotExccel() {
-                let {sortedInfo} = this
-                let sortField, sortOrder
-                // 获取当前列的排序和列的过滤规则
-                if (sortedInfo) {
-                    sortField = sortedInfo.field
-                    sortOrder = sortedInfo.order
-                }
-                this.$export('log/excel', {
-                    sortField: sortField,
-                    sortOrder: sortOrder,
-                    ...this.queryParams
-                })
-            },
-            search() {
-                let {sortedInfo} = this
-                let sortField, sortOrder
-                // 获取当前列的排序和列的过滤规则
-                if (sortedInfo) {
-                    sortField = sortedInfo.field
-                    sortOrder = sortedInfo.order
-                }
-                this.fetch({
-                    sortField: sortField,
-                    sortOrder: sortOrder,
-                    ...this.queryParams
-                })
-            },
-            reset() {
-                // 取消选中
-                this.selectedRowKeys = []
-                // 重置分页
-                this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
-                if (this.paginationInfo) {
-                    this.paginationInfo.current = this.pagination.defaultCurrent
-                    this.paginationInfo.pageSize = this.pagination.defaultPageSize
-                }
-                // 重置列排序规则
-                this.sortedInfo = null
-                // 重置查询参数
-                this.queryParams = {}
-                // 清空时间选择
-                if (this.advanced) {
-                    this.$refs.createTime.reset()
-                }
-                this.fetch()
-            },
-            handleTableChange(pagination, filters, sorter) {
-                // 将这两个参数赋值给Vue data，用于后续使用
-                this.paginationInfo = pagination
-                this.sortedInfo = sorter
-                this.fetch({
-                    sortField: sorter.field,
-                    sortOrder: sorter.order,
-                    ...this.queryParams,
-                    ...filters
-                })
-            },
-            fetch(params = {}) {
-                this.loading = true
-                if (this.paginationInfo) {
-                    // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
-                    this.$refs.TableInfo.pagination.current = this.paginationInfo.current
-                    this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
-                    params.pageSize = this.paginationInfo.pageSize
-                    params.pageNum = this.paginationInfo.current
-                } else {
-                    // 如果分页信息为空，则设置为默认值
-                    params.pageSize = this.pagination.defaultPageSize
-                    params.pageNum = this.pagination.defaultCurrent
-                }
-                this.$get('log', {
-                    ...params
-                }).then((r) => {
-                    let data = r.data
-                    const pagination = {...this.pagination}
-                    pagination.total = data.total
-                    this.loading = false
-                    this.dataSource = data.rows
-                    this.pagination = pagination
-                })
-            }
-        }
+export default {
+  name: 'SystemLog',
+  components: {RangeDate},
+  data () {
+    return {
+      advanced: false,
+      dataSource: [],
+      sortedInfo: null,
+      paginationInfo: null,
+      selectedRowKeys: [],
+      queryParams: {},
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      loading: false
     }
+  },
+  computed: {
+    columns () {
+      let { sortedInfo } = this
+      sortedInfo = sortedInfo || {}
+      return [{
+        title: '操作人',
+        dataIndex: 'username'
+      }, {
+        title: '操作描述',
+        dataIndex: 'operation'
+      }, {
+        title: '耗时',
+        dataIndex: 'time',
+        customRender: (text, row, index) => {
+          if (text < 500) {
+            return <a-tag color="green">{text} ms</a-tag>
+          } else if (text < 1000) {
+            return <a-tag color="cyan">{text} ms</a-tag>
+          } else if (text < 1500) {
+            return <a-tag color="orange">{text} ms</a-tag>
+          } else {
+            return <a-tag color="red">{text} ms</a-tag>
+          }
+        },
+        sorter: true,
+        sortOrder: sortedInfo.columnKey === 'time' && sortedInfo.order
+      }, {
+        title: '执行方法',
+        dataIndex: 'method',
+        scopedSlots: { customRender: 'method' }
+      }, {
+        title: '方法参数',
+        dataIndex: 'params',
+        scopedSlots: { customRender: 'params' },
+        width: 100
+      }, {
+        title: 'IP地址',
+        dataIndex: 'ip'
+      }, {
+        title: '操作地点',
+        dataIndex: 'location'
+      }, {
+        title: '操作时间',
+        dataIndex: 'createTime',
+        sorter: true,
+        sortOrder: sortedInfo.columnKey === 'createTime' && sortedInfo.order
+      }]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+      if (!this.advanced) {
+        this.queryParams.createTimeFrom = ''
+        this.queryParams.createTimeTo = ''
+        this.queryParams.location = ''
+      }
+    },
+    handleDateChange (value) {
+      if (value) {
+        this.queryParams.createTimeFrom = value[0]
+        this.queryParams.createTimeTo = value[1]
+      }
+    },
+    batchDelete () {
+      if (!this.selectedRowKeys.length) {
+        this.$message.warning('请选择需要删除的记录')
+        return
+      }
+      let that = this
+      this.$confirm({
+        title: '确定删除所选中的记录?',
+        content: '当您点击确定按钮后，这些记录将会被彻底删除',
+        centered: true,
+        onOk () {
+          let ids = []
+          for (let key of that.selectedRowKeys) {
+            ids.push(that.dataSource[key].id)
+          }
+          that.$delete('log/' + ids.join(',')).then(() => {
+            that.$message.success('删除成功')
+            that.selectedRowKeys = []
+            that.search()
+          })
+        },
+        onCancel () {
+          that.selectedRowKeys = []
+        }
+      })
+    },
+    exprotExccel () {
+      let {sortedInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.$export('log/excel', {
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams
+      })
+    },
+    search () {
+      let {sortedInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      // 清空时间选择
+      if (this.advanced) {
+        this.$refs.createTime.reset()
+      }
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      // 将这两个参数赋值给Vue data，用于后续使用
+      this.paginationInfo = pagination
+      this.sortedInfo = sorter
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.$refs.TableInfo.pagination.current = this.paginationInfo.current
+        this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
+        params.pageSize = this.paginationInfo.pageSize
+        params.pageNum = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.pageSize = this.pagination.defaultPageSize
+        params.pageNum = this.pagination.defaultCurrent
+      }
+      this.$get('log', {
+        ...params
+      }).then((r) => {
+        let data = r.data
+        const pagination = { ...this.pagination }
+        pagination.total = data.total
+        this.loading = false
+        this.dataSource = data.rows
+        this.pagination = pagination
+      })
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
     @import "../../../static/less/Common";
