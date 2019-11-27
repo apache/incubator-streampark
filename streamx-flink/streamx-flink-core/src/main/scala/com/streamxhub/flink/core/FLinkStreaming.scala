dd62f23281ca0b5651168dd228a8242cab662a2b
benjobs <benjobs@qq.com>
2019-11-27 23:06:33 +0800

    StreamX flink部分极速开发封装

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/FLinkStreaming.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/FLinkStreaming.scala
@@ -10,7 +10,7 @@ import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 import scala.annotation.meta.getter
 import scala.util.Try
 
-trait FlinkStreaming extends Logger {
+trait FLinkStreaming extends Logger {
 
   @(transient@getter)
   private var env: StreamExecutionEnvironment = _
@@ -22,7 +22,7 @@ trait FlinkStreaming extends Logger {
   def handler(context: StreamingContext): Unit
 
   private def initialize(args: Array[String]): Unit = {
-    SystemPropertyUtil.setAppHome(KEY_APP_HOME, classOf[FlinkStreaming])
+    SystemPropertyUtil.setAppHome(KEY_APP_HOME, classOf[FLinkStreaming])
     //parameter from args...
     val argsMap = ParameterTool.fromArgs(args).toMap
     val file = argsMap.get(FLINK_CONF)
