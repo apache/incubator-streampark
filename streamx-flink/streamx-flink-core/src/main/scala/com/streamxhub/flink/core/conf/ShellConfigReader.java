8d1e8315ec04ffbd59ea689023af760735f192d9
王华杰 <190648@lifeat.cn>
2019-12-31 21:00:47 +0800

    JavaMySQLSink

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/conf/ShellConfigReader.java
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/conf/ShellConfigReader.java
@@ -19,25 +19,26 @@ public class ShellConfigReader implements Serializable {
         if (action.equals("--conf")) {
             JarFile jarFile = new JarFile(jarPath);
             Enumeration<JarEntry> entries = jarFile.entries();
-            StringBuffer stringBuffer = new StringBuffer();
+            StringBuffer configBuffer = new StringBuffer();
             while (entries.hasMoreElements()) {
                 JarEntry jarEntry = entries.nextElement();
                 String[] entryInfo = jarEntry.getName().split("\\.");
                 if (entryInfo.length == 2) {
                     String name = entryInfo[0];
                     String type = entryInfo[1];
-                    if(name.startsWith("application-")) {
-                        if(type.equals("properties") || type.equals("yml")) {
-                            stringBuffer.append(jarEntry.getName()).append(" ");
+                    //配置文件必须以application为前缀。。。。
+                    if (name.startsWith("application-")) {
+                        if (type.equals("properties") || type.equals("yml")) {
+                            configBuffer.append("\"").append(jarEntry.getName()).append("\"").append(" ");
                         }
                     }
                 }
             }
-            System.out.println(stringBuffer.toString().trim());
+            System.out.println(configBuffer.toString().trim());
         } else if (action.equals("--read")) {
             JarFile jarFile = new JarFile(jarPath);
             String conf = args[2];
-            JarEntry jarEntry = jarFile.getJarEntry(conf);
+            JarEntry jarEntry = jarFile.getJarEntry(conf.replace("\"", ""));
             scala.collection.immutable.Map<String, String> configArgs;
             if (conf.endsWith(".properties")) {
                 configArgs = PropertiesUtils.fromPropertiesFile(jarFile.getInputStream(jarEntry));
@@ -50,7 +51,7 @@ public class ShellConfigReader implements Serializable {
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 if (StringUtils.isNoneBlank(entry.getValue()) && entry.getKey().startsWith("flink.deploy")) {
                     buffer.append(" --")
-                            .append(entry.getKey().replace("flink.deploy.",""))
+                            .append(entry.getKey().replace("flink.deploy.", ""))
                             .append(" ")
                             .append(entry.getValue());
                 }
