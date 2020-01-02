0a17dafa7b3801380044e45eae6d28d552e5a99c
王华杰 <190648@lifeat.cn>
2020-01-02 20:25:54 +0800

    flink读取外部配置文件

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/MySQLSink.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/MySQLSink.scala
@@ -7,7 +7,7 @@ import java.util.Properties
 import com.streamxhub.flink.core.StreamingContext
 import com.streamxhub.flink.core.conf.Config
 import com.streamxhub.flink.core.conf.ConfigConst._
-import com.streamxhub.flink.core.util.{Logger, MySQLUtils}
+import com.streamxhub.flink.core.util.Logger
 import org.apache.flink.api.common.ExecutionConfig
 import org.apache.flink.api.common.typeutils.base.VoidSerializer
 import org.apache.flink.api.java.typeutils.runtime.kryo.KryoSerializer
@@ -73,7 +73,11 @@ class MySQLSinkFunction[T](config: Properties, toSQLFn: T => String) extends Two
 
   override def beginTransaction(): Connection = {
     logInfo("[StreamX] MySQLSink beginTransaction ....")
-    val connection = MySQLUtils.getConnection(config)
+    Class.forName(config(KEY_MYSQL_DRIVER))
+    val connection = Try(config(KEY_MYSQL_USER)).getOrElse(null) match {
+      case null => DriverManager.getConnection(config(KEY_MYSQL_URL))
+      case _ => DriverManager.getConnection(config(KEY_MYSQL_URL), config(KEY_MYSQL_USER), config(KEY_MYSQL_PASSWORD))
+    }
     connection.setAutoCommit(false)
     connection
   }
@@ -81,6 +85,7 @@ class MySQLSinkFunction[T](config: Properties, toSQLFn: T => String) extends Two
   override def invoke(transaction: Connection, value: T, context: SinkFunction.Context[_]): Unit = {
     logInfo("[StreamX] MySQLSink invoke ....")
     val sql = toSQLFn(value)
+    println(sql)
     transaction.prepareStatement(sql).executeUpdate()
   }
 
@@ -114,7 +119,8 @@ class MySQLSinkFunction[T](config: Properties, toSQLFn: T => String) extends Two
     }
   }
 
-  private def close(conn: Connection): Unit = MySQLUtils.close(conn,null,null)
+  private def close(conn: Connection): Unit = Try(conn.close())
 
 }
+
 
