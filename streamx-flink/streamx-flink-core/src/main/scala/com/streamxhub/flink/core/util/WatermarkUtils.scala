84921733024a5bd7ed18b164950394d98adc4905
王华杰 <190648@lifeat.cn>
2020-01-04 15:24:06 +0800

    启动logo

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/util/WatermarkUtils.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/util/WatermarkUtils.scala
@@ -8,23 +8,23 @@ import org.apache.flink.streaming.api.windowing.time.Time
 
 object WatermarkUtils {
 
-  def boundedOutOfOrdernessWatermark[T](f: T => Long)(implicit maxOutOfOrderness: Time): AssignerWithPeriodicWatermarks[T] = {
+  def boundedOutOfOrdernessWatermark[T](fun: T => Long)(implicit maxOutOfOrderness: Time): AssignerWithPeriodicWatermarks[T] = {
     new BoundedOutOfOrdernessTimestampExtractor[T](maxOutOfOrderness) {
-      override def extractTimestamp(element: T): Long =  f(element)
+      override def extractTimestamp(element: T): Long =  fun(element)
     }
   }
 
-  def timeLagWatermark[T](f: T => Long)(implicit maxTimeLag: Long): AssignerWithPeriodicWatermarks[T] = {
+  def timeLagWatermark[T](fun: T => Long)(implicit maxTimeLag: Long): AssignerWithPeriodicWatermarks[T] = {
     new AssignerWithPeriodicWatermarks[T] {
-      override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = f(element)
+      override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = fun(element)
 
       override def getCurrentWatermark: Watermark = new Watermark(System.currentTimeMillis() - maxTimeLag)
     }
   }
 
-  def punctuatedWatermark[T](maxTimeLag: Long)(implicit f: T => Long, f1: T => Boolean): AssignerWithPunctuatedWatermarks[T] = {
+  def punctuatedWatermark[T](maxTimeLag: Long)(implicit fun: T => Long, f1: T => Boolean): AssignerWithPunctuatedWatermarks[T] = {
     new AssignerWithPunctuatedWatermarks[T] {
-      override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = f(element)
+      override def extractTimestamp(element: T, previousElementTimestamp: Long): Long = fun(element)
 
       override def checkAndGetNextWatermark(lastElement: T, extractedTimestamp: Long): Watermark = {
         if (f1(lastElement)) new Watermark(extractedTimestamp) else null
