a15f84b3e160520409921fb6e661b5b212aac109
王华杰 <190648@lifeat.cn>
2020-01-11 17:34:47 +0800

    JDBCSink 封装完毕

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/JdbcSink.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/JdbcSink.scala
@@ -9,7 +9,9 @@ import java.util.Properties
 import com.streamxhub.flink.core.StreamingContext
 import com.streamxhub.flink.core.conf.Config
 import com.streamxhub.flink.core.util.{Logger, MySQLUtils}
+import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.datastream.DataStreamSink
+import org.apache.flink.streaming.api.environment.CheckpointConfig
 import org.apache.flink.streaming.api.scala.DataStream
 
 import scala.collection.Map
@@ -36,6 +38,19 @@ class JdbcSink(@transient ctx: StreamingContext,
                name: String = null,
                uid: String = null)(implicit instance: String = "") extends Sink with Logger {
 
+  //每隔10s进行启动一个检查点
+  ctx.enableCheckpointing(10000)
+  //设置模式为：exactly_one，仅一次语义
+  ctx.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+  //确保检查点之间有1s的时间间隔【checkpoint最小间隔】
+  ctx.getCheckpointConfig.setMinPauseBetweenCheckpoints(1000)
+  //检查点必须在10s之内完成，或者被丢弃【checkpoint超时时间】
+  ctx.getCheckpointConfig.setCheckpointTimeout(10000)
+  //同一时间只允许进行一次检查点
+  ctx.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+  //被cancel会保留Checkpoint数据
+  ctx.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
   /**
    *
    * @param stream  : DataStream
@@ -55,17 +70,17 @@ class JdbcSink(@transient ctx: StreamingContext,
 
 class JdbcSinkFunction[T](config: Properties, toSQLFn: T => String) extends RichSinkFunction[T] with Logger {
 
-  private var connection: Connection = null
-  private var preparedStatement: PreparedStatement = null
+  private var connection: Connection = _
+  private var preparedStatement: PreparedStatement = _
 
   @throws[Exception]
   override def open(parameters: Configuration): Unit = {
     logInfo("[StreamX] JdbcSink Open....")
     connection = MySQLUtils.getConnection(config)
+    connection.setAutoCommit(false)
   }
 
   override def invoke(value: T, context: SinkFunction.Context[_]): Unit = {
-    logInfo("[StreamX] JdbcSink invoke....")
     require(connection != null)
     val sql = toSQLFn(value)
     preparedStatement = connection.prepareStatement(sql)
@@ -74,7 +89,8 @@ class JdbcSinkFunction[T](config: Properties, toSQLFn: T => String) extends Rich
       connection.commit()
     }catch {
       case e:Exception =>
-        System.err.println(s"[StreamX] JdbcSink invoke error:${sql}")
+        logError(s"[StreamX] JdbcSink invoke error:${sql}")
+        throw e
     }
   }
 
