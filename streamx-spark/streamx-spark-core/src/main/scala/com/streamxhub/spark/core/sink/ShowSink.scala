c21026af21c22d0e7dec071b16833c4681e85111
streamxhub <benjobs@qq.com>
2020-01-13 23:51:29 +0800

    HBaseSink封装

--- streamx-spark/streamx-spark-core/src/main/scala/com/streamxhub/spark/core/sink/ShowSink.scala
+++ streamx-spark/streamx-spark-core/src/main/scala/com/streamxhub/spark/core/sink/ShowSink.scala
@@ -21,13 +21,9 @@
 
 package com.streamxhub.spark.core.sink
 
-import java.util.Properties
-
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.Time
-import scala.collection.JavaConversions._
-
 /**
   *
   */
