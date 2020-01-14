e9b66c139f97aae46590f8adc6b986deadb73312
王华杰 <190648@lifeat.cn>
2020-01-14 16:43:25 +0800

    重新调整层级结构

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSinkApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSinkApp.scala
@@ -5,15 +5,17 @@ import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.util.Bytes
+import java.util.Random
 
 object HBaseSinkApp extends FlinkStreaming {
 
   override def handler(context: StreamingContext): Unit = {
     implicit val orderType = TypeInformation.of[TestEntity](classOf[TestEntity])
     val source = context.addSource(new TestSource)
+    val random = new Random()
 
     HBaseSink(context).sink[TestEntity](source, "order")(x => {
-      val put = new Put(Bytes.toBytes(x.timestamp),x.timestamp)
+      val put = new Put(Bytes.toBytes(System.nanoTime()+random.nextInt(1000000)),x.timestamp)
       put.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("cid"),Bytes.toBytes(x.cityId))
       put.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("oid"),Bytes.toBytes(x.orderId))
       put.addColumn(Bytes.toBytes("cf"),Bytes.toBytes("os"),Bytes.toBytes(x.orderStatus))
