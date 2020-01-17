589afdd88c63c83e0f870510d799baf768045b7d
streamxhub <benjobs@qq.com>
2020-01-17 22:59:49 +0800

    HBase Sink

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/HBaseSink.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/sink/HBaseSink.scala
@@ -77,6 +77,7 @@ class HBaseSinkFunction[T](tabName: String, fun: T => Mutation)(implicit prop: P
   private var table: Table = _
   private var mutator: BufferedMutator = _
   val offset: AtomicLong = new AtomicLong(0L)
+  val timer:Timer = new Timer()
 
   private val commitBatch = prop.getOrElse(KEY_HBASE_COMMIT_BATCH, s"$DEFAULT_HBASE_COMMIT_BATCH").toInt
   private val writeBufferSize = prop.getOrElse(KEY_HBASE_WRITE_SIZE, s"$DEFAULT_HBASE_WRITE_SIZE").toLong
@@ -111,7 +112,7 @@ class HBaseSinkFunction[T](tabName: String, fun: T => Mutation)(implicit prop: P
       case _ =>
     }
 
-    new Timer().schedule(new TimerTask {
+    timer.schedule(new TimerTask {
       override def run(): Unit = {
         if (offset.get() > 0) execBatch()
       }
