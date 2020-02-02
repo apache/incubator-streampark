349436ccbfff94a21d2eb93c31a11630e6a5faf9
王华杰 <190648@lifeat.cn>
2020-02-02 13:48:59 +0800

    HttpSink support!

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/TestSource.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/TestSource.scala
@@ -37,7 +37,7 @@ class TestSource extends SourceFunction[TestEntity] {
   var index = 0
 
   override def run(ctx: SourceFunction.SourceContext[TestEntity]): Unit = {
-    while (isRunning && index <= 10001) {
+    while (isRunning && index <= 1001) {
       index += 1
       val userId = System.currentTimeMillis()
       val orderId = random.nextInt(100)
