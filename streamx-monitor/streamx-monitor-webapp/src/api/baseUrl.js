002346175be16082f5e6d8e780592df50a3753d1
王华杰 <190648@lifeat.cn>
2020-02-27 18:17:55 +0800

    flink monitor eslint format

--- streamx-monitor/streamx-monitor-webapp/src/api/baseUrl.js
+++ streamx-monitor/streamx-monitor-webapp/src/api/baseUrl.js
@@ -1,10 +1,10 @@
 let baseUrl = ''
 switch (process.env.NODE_ENV) {
   case 'development':
-    baseUrl = 'http://localhost:10001/api'
+    baseUrl = 'http://localhost:10001'
     break
   case 'production':
-    baseUrl = 'http://10.2.39.89:10001/api'
+    baseUrl = 'http://10.2.39.89:10001'
     break
 }
 export default baseUrl
