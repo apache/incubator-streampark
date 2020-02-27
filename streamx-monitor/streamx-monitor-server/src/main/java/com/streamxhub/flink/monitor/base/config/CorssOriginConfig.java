60d9a76e64c805bfe97137536f9ad804bc1f682d
王华杰 <190648@lifeat.cn>
2020-02-27 20:06:30 +0800

    flink monitor eslint format

--- streamx-monitor/streamx-monitor-server/src/main/java/com/streamxhub/flink/monitor/base/config/CorssOriginConfig.java
+++ streamx-monitor/streamx-monitor-server/src/main/java/com/streamxhub/flink/monitor/base/config/CorssOriginConfig.java
@@ -30,11 +30,17 @@ public class CorssOriginConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")	// 允许跨域访问的路径
-        .allowedOrigins("*")	// 允许跨域访问的源
-        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")	// 允许请求方法
-        .maxAge(168000)	// 预检间隔时间
-        .allowedHeaders("*")  // 允许头部设置
-        .allowCredentials(true);	// 是否发送cookie
+        //允许跨域访问的路径
+        registry.addMapping("/**")
+                //允许跨域访问的源
+                .allowedOrigins("*")
+                // 允许请求方法
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                // 预检间隔时间
+                .maxAge(168000)
+                // 允许头部设置
+                .allowedHeaders("*")
+                // 是否发送cookie
+                .allowCredentials(true);
     }
 }
