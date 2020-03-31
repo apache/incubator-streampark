c00d41b1df13c46e3309d3a03a73585db5db6252
王华杰 <190648@lifeat.cn>
2020-03-31 09:50:39 +0800

    mongo source support!

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MongoSource.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MongoSource.scala
@@ -2,7 +2,10 @@ package com.streamxhub.flink.core.source
 
 import java.util.Properties
 
-import com.streamxhub.common.util.{JdbcUtils, Logger}
+import com.mongodb.MongoClient
+import org.bson.Document
+import com.mongodb.client.{FindIterable, MongoDatabase}
+import com.streamxhub.common.util.{Logger, MongoConfig}
 import com.streamxhub.flink.core.StreamingContext
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.configuration.Configuration
@@ -13,51 +16,63 @@ import scala.annotation.meta.param
 import scala.collection.Map
 
 
-
 class MongoSource(@(transient@param) val ctx: StreamingContext, overrideParams: Map[String, String] = Map.empty[String, String]) {
 
   /**
    *
-   * @param sqlFun
-   * @param fun
-   * @param interval sql查询的间隔时间.
+   * @param queryFun
+   * @param resultFun
+   * @param interval
    * @param config
    * @tparam R
    * @return
    */
-  def getDataStream[R: TypeInformation](sqlFun: => String, fun: List[Map[String, _]] => List[R], interval: Long = 1000L)(implicit config: Properties): DataStream[R] = {
-    val mysqlFun = new MongoSourceFunction[R](sqlFun, fun, interval)
-    ctx.addSource(mysqlFun)
+  def getDataStream[R: TypeInformation](queryFun: MongoDatabase => FindIterable[Document], resultFun: Document => List[R], interval: Long)(implicit config: Properties): DataStream[R] = {
+    val mongoFun = new MongoSourceFunction[R](queryFun, resultFun, interval)
+    ctx.addSource(mongoFun)
   }
 
 }
 
 /**
  *
- * @param sqlFun
+ * @param queryFun
  * @param resultFun
  * @param interval 两次sql执行查询的间隔.防止死循环密集的查询sql
  * @param typeInformation$R$0
  * @param config
  * @tparam R
  */
-private[this] class MongoSourceFunction[R: TypeInformation](sqlFun: => String, resultFun: List[Map[String, _]] => List[R], interval: Long)(implicit config: Properties) extends RichSourceFunction[R] with Logger {
+private[this] class MongoSourceFunction[R: TypeInformation](queryFun: MongoDatabase => FindIterable[Document], resultFun: Document => List[R], interval: Long)(implicit config: Properties) extends RichSourceFunction[R] with Logger {
 
   private[this] var isRunning = true
+
+  var client: MongoClient = _
+
+  var database: MongoDatabase = _
 
   override def cancel(): Unit = this.isRunning = false
 
   override def open(parameters: Configuration): Unit = {
-
-
+    client = MongoConfig.getClient(config)
+    val db = MongoConfig.getProperty(config, MongoConfig.database)
+    database = client.getDatabase(db)
   }
 
   @throws[Exception]
   override def run(@(transient@param) ctx: SourceFunction.SourceContext[R]): Unit = {
     while (true) {
-      resultFun(JdbcUtils.select(sqlFun)).foreach(ctx.collect)
+      val find = queryFun(database)
+      val cursor = find.iterator()
+      while (cursor.hasNext) {
+        resultFun(cursor.next()).foreach(ctx.collect)
+      }
       Thread.sleep(interval)
     }
+  }
+
+  override def close(): Unit = {
+    client.close()
   }
 
 }
