90f13bc4719e53dcf2d090ef81872774e69c207b
王华杰 <190648@lifeat.cn>
2020-04-02 15:56:18 +0800

    streamx

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/MongoSourceApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/MongoSourceApp.scala
@@ -5,16 +5,20 @@ import com.mongodb.client.model.Filters
 import com.streamxhub.flink.core.source.MongoSource
 import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
 import org.apache.flink.streaming.api.scala._
+import com.streamxhub.common.util.DateUtils
 
 object MongoSourceApp extends FlinkStreaming {
 
   override def handler(context: StreamingContext): Unit = {
     implicit val prop = context.parameter.getProperties
     val source = new MongoSource(context)
-    source.getDataStream[String](
-      _.getCollection("shop").find(Filters.gt("updateTime", "2020-03-27 00:00:00")),
-      _.toJson(),
-      10000L)
+    source.getDataStream[String](x => {
+      val collection = x.getCollection("shop")
+      collection.find(Filters.gte("updateTime", DateUtils.parse("2019-09-27 00:00:00")))
+    }, x => {
+      x.toJson()
+    },
+      1000000L)
       .print()
   }
 
