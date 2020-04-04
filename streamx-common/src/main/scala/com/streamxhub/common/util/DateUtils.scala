8fe91afd320ece943b055cb0c221aab36155d88b
王华杰 <190648@lifeat.cn>
2020-04-04 17:08:45 +0800

    source apply

--- streamx-common/src/main/scala/com/streamxhub/common/util/DateUtils.scala
+++ streamx-common/src/main/scala/com/streamxhub/common/util/DateUtils.scala
@@ -21,7 +21,7 @@
 package com.streamxhub.common.util
 
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import java.util.{Calendar, Date, TimeZone}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Try
@@ -35,8 +35,9 @@ object DateUtils {
 
   val dayFormat2 = "yyyy-MM-dd"
 
-  def parse(date: String, format: String = fullFormat): Date = {
+  def parse(date: String, format: String = fullFormat,timeZone:TimeZone = TimeZone.getDefault): Date = {
     val df: SimpleDateFormat = new SimpleDateFormat(format)
+    df.setTimeZone(timeZone)
     df.parse(date)
   }
 
@@ -48,8 +49,9 @@ object DateUtils {
     milliSecond2Date(time * 1000)
   }
 
-  def now(dateFormat: String = dayFormat1) = {
+  def now(dateFormat: String = dayFormat1,timeZone:TimeZone = TimeZone.getDefault) = {
     val df: SimpleDateFormat = new SimpleDateFormat(dateFormat)
+    df.setTimeZone(timeZone)
     df.format(new Date())
   }
 
@@ -73,43 +75,41 @@ object DateUtils {
     minuteOfDay(date) * 60 + calendar.get(Calendar.SECOND)
   }
 
-  def format(fmt: String = fullFormat, date: Date = new Date()): String = {
-    if (date == null) null else new SimpleDateFormat(fmt).format(date)
+  def format(date: Date = new Date(),fmt: String = fullFormat,timeZone:TimeZone = TimeZone.getDefault): String = {
+    if (date == null) null else {
+      val simpleDateFormat = new SimpleDateFormat(fmt)
+        simpleDateFormat.setTimeZone(timeZone)
+        simpleDateFormat.format(date)
+    }
   }
 
-  def getTime(time: String, fmt: String = fullFormat): Long = {
-    Try(new SimpleDateFormat(fmt).parse(time).getTime)
+  def getTime(time: String, fmt: String = fullFormat,timeZone:TimeZone = TimeZone.getDefault): Long = {
+    val simpleDateFormat = new SimpleDateFormat(fmt)
+    simpleDateFormat.setTimeZone(timeZone)
+    Try(simpleDateFormat.parse(time).getTime)
       .filter(_ > 0).getOrElse(System.currentTimeMillis())
   }
 
-  /**
-   *
-   * @param start
-   * @param end
-   * @param fmt
-   * @param open
-   * @return
-   */
-  def rangeStr(start: String, end: String, fmt: String = dayFormat1, open: Boolean = false): List[Date] = {
-    val df: SimpleDateFormat = new SimpleDateFormat(fmt)
-    range(df.parse(start), df.parse(end), open)
-  }
 
   //获取今天之前的n天
-  def +-(i: Int, date: Date = new Date)(implicit format: String = dayFormat2): String = {
+  def +-(i: Int, date: Date = new Date,timeZone:TimeZone = TimeZone.getDefault)(implicit format: String = dayFormat2): String = {
     val cal = Calendar.getInstance
     cal.setTime(date)
     cal.add(Calendar.DATE, i)
-    val statTime = new SimpleDateFormat(format).format(cal.getTime)
+    val sdf = new SimpleDateFormat(format)
+    sdf.setTimeZone(timeZone)
+    val statTime = sdf.format(cal.getTime)
     statTime
   }
 
   //获取今天之前的n天
-  def option(format: String, i: Int, date: Date = new Date()): String = {
+  def option(format: String, i: Int, date: Date = new Date(),timeZone:TimeZone = TimeZone.getDefault): String = {
     val cal = Calendar.getInstance
     cal.setTime(date)
     cal.add(Calendar.DATE, i)
-    new SimpleDateFormat(format).format(cal.getTime)
+    val sdf = new SimpleDateFormat(format)
+    sdf.setTimeZone(timeZone)
+    sdf.format(cal.getTime)
   }
 
   /**
