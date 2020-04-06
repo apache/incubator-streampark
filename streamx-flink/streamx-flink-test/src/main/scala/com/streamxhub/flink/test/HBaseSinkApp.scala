0906c2b09b7d07131a07671916b5d30338062f56
王华杰 <190648@lifeat.cn>
2020-04-06 22:35:36 +0800

    bug fix!!!

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSinkApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSinkApp.scala
@@ -2,11 +2,13 @@ package com.streamxhub.flink.test
 
 import com.streamxhub.flink.core.sink.{HBaseOutputFormat, HBaseSink}
 import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
-import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.{Mutation, Put}
 import org.apache.hadoop.hbase.util.Bytes
-import java.util.Random
+import java.util.{Collections, Random}
+
 import org.apache.flink.streaming.api.scala._
 
+import scala.collection.JavaConverters._
 import com.streamxhub.common.util.ConfigUtils
 
 object HBaseSinkApp extends FlinkStreaming {
@@ -16,14 +18,14 @@ object HBaseSinkApp extends FlinkStreaming {
     val random = new Random()
 
     //定义转换规则...
-    implicit def entry2Put(entity: TestEntity): List[Put] = {
+    implicit def entry2Put(entity: TestEntity): java.lang.Iterable[Mutation] = {
       val put = new Put(Bytes.toBytes(System.nanoTime() + random.nextInt(1000000)), entity.timestamp)
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cid"), Bytes.toBytes(entity.cityId))
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("oid"), Bytes.toBytes(entity.orderId))
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("os"), Bytes.toBytes(entity.orderStatus))
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("oq"), Bytes.toBytes(entity.quantity))
       put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sid"), Bytes.toBytes(entity.siteId))
-      List(put,put)
+      Collections.singleton(put)
     }
 
     //1）插入方式1
