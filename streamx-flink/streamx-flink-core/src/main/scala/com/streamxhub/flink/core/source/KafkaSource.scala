02584c3dd4bc8d932c68bed8b0c714604f5d75d1
benjobs <190648@lifeat.cn>
2020-04-29 10:36:45 +0800

    bug fix!!!

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/KafkaSource.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/KafkaSource.scala
@@ -20,9 +20,8 @@
  */
 package com.streamxhub.flink.core.source
 
-import com.streamxhub.common.conf.ConfigConst
 import com.streamxhub.common.util.ConfigUtils
-import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.serialization.{DeserializationSchema,SimpleStringSchema}
 import org.apache.flink.streaming.api.scala.{DataStream, _}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
 import com.streamxhub.flink.core.StreamingContext
@@ -45,24 +44,27 @@ class KafkaSource(@(transient@param) val ctx: StreamingContext, overrideParams: 
 
   /**
    *
-   * commit offset 方式:
-   *    Flink kafka consumer commit offset 方式需要区分是否开启了 checkpoint。
-   * 1) checkpoint 关闭: commit offset 要依赖于 kafka 客户端的 auto commit。
-   *    需设置 enable.auto.commit，auto.commit.interval.ms 参数到 consumer properties，
-   *    就会按固定的时间间隔定期 auto commit offset 到 kafka。
-   * 2) checkpoint 开启: 这个时候作业消费的 offset 是 Flink 在 state 中自己管理和容错。
+   * commit offset 方式:<br/>
+   *    &nbsp;&nbsp;Flink kafka consumer commit offset 方式需要区分是否开启了 checkpoint。<br/>
+   *  &nbsp;&nbsp; 1) checkpoint 关闭: commit offset 要依赖于 kafka 客户端的 auto commit。
+   *  需设置 enable.auto.commit，auto.commit.interval.ms 参数到 consumer properties，
+   *  就会按固定的时间间隔定期 auto commit offset 到 kafka。<br/>
+   * &nbsp;&nbsp; 2) checkpoint 开启: 这个时候作业消费的 offset 是 Flink 在 state 中自己管理和容错。
    *    此时提交 offset 到 kafka，一般都是作为外部进度的监控，想实时知道作业消费的位置和 lag 情况。
    *    此时需要 setCommitOffsetsOnCheckpoints 为 true 来设置当 checkpoint 成功时提交 offset 到 kafka。
    *    此时 commit offset 的间隔就取决于 checkpoint 的间隔
    *
    * 获取DStream 流
-   *
-   * @return
+   * @param topics topic集合
+   * @param alias 别名,区分不同的kafka连接实例
+   * @param deserializer DeserializationSchema
+   * @tparam T
    */
-  def getDataStream(topic: String = "", alias: String = ""): DataStream[String] = {
-    val prop = ConfigUtils.getKafkaSourceConf(ctx.paramMap, topic, alias)
+  def getDataStream[T](topics: List[String], alias: String = "",deserializer: DeserializationSchema[T] = new SimpleStringSchema().asInstanceOf[DeserializationSchema[T]]): DataStream[T] = {
+    require(topics.nonEmpty,"[Streamx-Flink] topics can not be null")
+    val prop = ConfigUtils.getKafkaSourceConf(ctx.paramMap, topics.head, alias)
     overrideParams.foreach(x => prop.put(x._1, x._2))
-    val consumer = new FlinkKafkaConsumer011[String](prop.remove(ConfigConst.KEY_KAFKA_TOPIC).toString, new SimpleStringSchema(), prop)
+    val consumer = new FlinkKafkaConsumer011(topics, deserializer, prop)
     val enableChk = ctx.getCheckpointConfig.isCheckpointingEnabled
     val autoCommit = prop.getOrElse(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true").toBoolean
     (enableChk, autoCommit) match {
