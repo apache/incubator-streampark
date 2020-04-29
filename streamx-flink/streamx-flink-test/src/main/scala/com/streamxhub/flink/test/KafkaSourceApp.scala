38a31f3f9411d991537f3e2b24c8161968225b83
benjobs <190648@lifeat.cn>
2020-04-29 21:26:34 +0800

    kafka source返回结果封装,返回更多信息

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/KafkaSourceApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/KafkaSourceApp.scala
@@ -9,7 +9,7 @@ object KafkaSourceApp extends FlinkStreaming {
   override def handler(context: StreamingContext): Unit = {
 
     //one topic
-    new KafkaSource(context).getDataStream[String]("hopsonone_park_sh_tc_record")
+    new KafkaSource(context).getDataStream[String]("kfk1")
       .uid("kfkSource1")
       .name("kfkSource1")
       .map(x=>{
