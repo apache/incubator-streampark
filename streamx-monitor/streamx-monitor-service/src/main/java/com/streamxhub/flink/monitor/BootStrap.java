d5d2fff72b2d1ca285240e8f0e4b99e17f9966bf
benjobs <190648@lifeat.cn>
2020-06-26 22:12:28 +0800

    HikariPC连接池反射设置参数bug修复

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/BootStrap.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/BootStrap.java
@@ -24,6 +24,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+/**
+ * @author benjobs
+ */
 @CrossOrigin
 @SpringBootApplication
 public class BootStrap {
