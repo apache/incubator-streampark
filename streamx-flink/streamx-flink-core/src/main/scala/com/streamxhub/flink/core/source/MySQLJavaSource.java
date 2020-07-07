962a1d929a8d4da9f18d3a5649fe161efeec0e0a
benjobs <190648@lifeat.cn>
2020-07-07 09:36:13 +0800

    MySQLSource Java Api支持.

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MySQLJavaSource.java
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MySQLJavaSource.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.streamxhub.flink.core.source;
 
 import com.streamxhub.common.util.ConfigUtils;
@@ -19,6 +39,8 @@ public class MySQLJavaSource<T> {
 
     private StreamingContext context;
     private Properties jdbc;
+    private SQLFunction sqlFunc;
+    private ResultSetFunction<T> resultSetFunc;
 
     public MySQLJavaSource(StreamingContext context) {
         this(context,(String) null);
@@ -34,9 +56,23 @@ public class MySQLJavaSource<T> {
         this.jdbc = jdbc;
     }
 
+    public DataStreamSource<T> getDataStream() {
+        return getDataStream(this.sqlFunc,this.resultSetFunc);
+    }
+
     public DataStreamSource<T> getDataStream(SQLFunction sqlFun,ResultSetFunction<T> fun) {
-        MySQLJavaSourceFunction<T> sourceFunction = new MySQLJavaSourceFunction<>(jdbc, sqlFun, fun);
+        MySQLJavaSourceFunction<T> sourceFunction = new MySQLJavaSourceFunction<T>(jdbc, sqlFun, fun);
         return context.getJavaEnv().addSource(sourceFunction);
+    }
+
+    public MySQLJavaSource<T> sql(SQLFunction func) {
+        this.sqlFunc = func;
+        return this;
+    }
+
+    public MySQLJavaSource<T> result(ResultSetFunction<T> func) {
+        this.resultSetFunc = func;
+        return this;
     }
 }
 
--- streamx-flink/streamx-flink-test/src/main/java/com/streamxhub/flink/javacase/MySQLSourceApp.java
+++ streamx-flink/streamx-flink-test/src/main/java/com/streamxhub/flink/javacase/MySQLSourceApp.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.streamxhub.flink.javacase;
 
 import com.streamxhub.common.util.ConfigUtils;
@@ -36,10 +56,12 @@ public class MySQLSourceApp {
         Properties jdbc1 = ConfigUtils.getJdbcConf(context.parameter().toMap(), Dialect.MYSQL().toString(), "");
 
         new MySQLJavaSource<LogBean>(context, prop)
-                .getDataStream((SQLFunction) () -> "select * from orders limit 10", (ResultSetFunction<LogBean>) map -> {
+                .sql((SQLFunction) () -> "select * from orders limit 10")
+                .result((ResultSetFunction<LogBean>) map -> {
                     System.out.println(map);
                     return new LogBean();
-                }).returns(TypeInformation.of(LogBean.class))
+                })
+                .getDataStream().returns(TypeInformation.of(LogBean.class))
                 .print("Java MySQLSource");
 
         context.execute();
