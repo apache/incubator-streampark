3ca5f7b633d93e00a8224441e7750f5579d068c2
benjobs <190648@lifeat.cn>
2020-07-15 12:06:49 +0800

    mysql async request testcase

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/request/MySQLRequest.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/request/MySQLRequest.scala
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit
 
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.scala.{AsyncDataStream, DataStream}
-import io.vertx.core.{AsyncResult, Handler, ServiceHelper, Vertx, VertxOptions}
+import io.vertx.core.{AsyncResult, Handler, Vertx, VertxOptions}
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.sql.ResultSet
 import org.apache.flink.configuration.Configuration
@@ -36,9 +36,12 @@ import scala.collection.JavaConverters._
 import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.ext.sql.SQLClient
 import io.vertx.ext.sql.SQLConnection
+import io.vertx.core.spi.resolver.ResolverProvider.DISABLE_DNS_RESOLVER_PROP_NAME
 import java.util.Collections
 
-import io.vertx.core.spi.VertxFactory
+import com.streamxhub.common.conf.ConfigConst.KEY_INSTANCE
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+import javax.sql.DataSource
 
 import scala.annotation.meta.param
 
@@ -57,12 +60,12 @@ class MySQLRequest[T: TypeInformation](@(transient@param) private val stream: Da
    * @tparam R
    * @return
    */
-  def requestOrdered[R: TypeInformation](sqlFun: T => String, resultFun: java.util.Map[String, _] => R, timeout: Long = 1000, capacity: Int = 10)(implicit jdbc: Properties): DataStream[R] = {
+  def requestOrdered[R: TypeInformation](@(transient@param) sqlFun: T => String, @(transient@param) resultFun: Map[String, _] => R, timeout: Long = 1000, capacity: Int = 10)(implicit jdbc: Properties): DataStream[R] = {
     val async = new MySQLASyncIOFunction[T, R](sqlFun, resultFun, jdbc)
     AsyncDataStream.orderedWait(stream, async, timeout, TimeUnit.MILLISECONDS, capacity)
   }
 
-  def requestUnordered[R: TypeInformation](sqlFun: T => String, resultFun: java.util.Map[String, _] => R, timeout: Long = 1000, capacity: Int = 10)(implicit jdbc: Properties): DataStream[R] = {
+  def requestUnordered[R: TypeInformation](@(transient@param) sqlFun: T => String, @(transient@param) resultFun: Map[String, _] => R, timeout: Long = 1000, capacity: Int = 10)(implicit jdbc: Properties): DataStream[R] = {
     val async = new MySQLASyncIOFunction[T, R](sqlFun, resultFun, jdbc)
     AsyncDataStream.unorderedWait(stream, async, timeout, TimeUnit.MILLISECONDS, capacity)
   }
@@ -70,15 +73,17 @@ class MySQLRequest[T: TypeInformation](@(transient@param) private val stream: Da
 }
 
 
-class MySQLASyncIOFunction[T: TypeInformation, R: TypeInformation](sqlFun: T => String, resultFun: java.util.Map[String, _] => R, jdbc: Properties) extends RichAsyncFunction[T, R] {
+class MySQLASyncIOFunction[T: TypeInformation, R: TypeInformation](sqlFun: T => String, resultFun: Map[String, _] => R, jdbc: Properties) extends RichAsyncFunction[T, R] {
   private var client: SQLClient = null
 
   override def open(parameters: Configuration): Unit = {
     super.open(parameters)
+    System.getProperties().setProperty(DISABLE_DNS_RESOLVER_PROP_NAME, "true")
     val clientConfig = new JsonObject()
     jdbc.foreach(x => clientConfig.put(x._1, x._2))
-    //HikariCP连接池.
-    clientConfig.put("provider_class", "io.vertx.ext.jdbc.spi.impl.HikariCPDataSourceProvider")
+    clientConfig.remove(KEY_INSTANCE)
+    //使用HikariCP连接池.
+    clientConfig.put("provider_class", classOf[HikariCPDataSourceProvider].getName)
     val vertxOpts = new VertxOptions()
     val vertx = Vertx.vertx(vertxOpts)
     client = JDBCClient.createNonShared(vertx, clientConfig)
@@ -99,7 +104,7 @@ class MySQLASyncIOFunction[T: TypeInformation, R: TypeInformation](sqlFun: T => 
             override def handle(event: AsyncResult[ResultSet]): Unit = {
               if (event.succeeded) {
                 event.result().getRows().foreach(x => {
-                  resultFuture.complete(Collections.singleton(resultFun(x.getMap.asScala)))
+                  resultFuture.complete(Collections.singleton(resultFun(x.getMap.asScala.toMap)))
                 })
               } else {
                 throw event.cause()
@@ -111,6 +116,50 @@ class MySQLASyncIOFunction[T: TypeInformation, R: TypeInformation](sqlFun: T => 
         }
       }
     })
+  }
+
+}
+
+class HikariCPDataSourceProvider extends io.vertx.ext.jdbc.spi.impl.HikariCPDataSourceProvider {
+  override def getDataSource(json: JsonObject): DataSource = {
+    val config = new HikariConfig
+    json.filterNot(_.getKey == "provider_class").foreach(entry => {
+      val value = entry.getValue.toString
+      entry.getKey match {
+        case "dataSourceClassName" => config.setDataSourceClassName(value)
+        case "jdbcUrl" => config.setJdbcUrl(value)
+        case "username" => config.setUsername(value)
+        case "password" => config.setPassword(value)
+        case "autoCommit" => config.setAutoCommit(value.toBoolean)
+        case "connectionTimeout" => config.setConnectionTimeout(value.toLong)
+        case "idleTimeout" => config.setIdleTimeout(value.toLong)
+        case "maxLifetime" => config.setMaxLifetime(value.toLong)
+        case "connectionTestQuery" => config.setConnectionTestQuery(value)
+        case "minimumIdle" => config.setMinimumIdle(value.toInt)
+        case "maximumPoolSize" => config.setMaximumPoolSize(value.toInt)
+        case "poolName" => config.setPoolName(value)
+        case "initializationFailTimeout" => config.setInitializationFailTimeout(value.toLong)
+        case "isolationInternalQueries" => config.setIsolateInternalQueries(value.toBoolean)
+        case "allowPoolSuspension" => config.setAllowPoolSuspension(value.toBoolean)
+        case "readOnly" => config.setReadOnly(value.toBoolean)
+        case "registerMBeans" => config.setRegisterMbeans(value.toBoolean)
+        case "catalog" => config.setCatalog(value)
+        case "connectionInitSql" => config.setConnectionInitSql(value)
+        case "driverClassName" => config.setDriverClassName(value)
+        case "transactionIsolation" => config.setTransactionIsolation(value)
+        case "validationTimeout" => config.setValidationTimeout(value.toLong)
+        case "leakDetectionThreshold" => config.setLeakDetectionThreshold(value.toLong)
+        case "datasource" =>
+          for (key <- entry.getValue.asInstanceOf[JsonObject]) {
+            config.addDataSourceProperty(key.getKey, key.getValue)
+          }
+        case "metricRegistry" => throw new UnsupportedOperationException(entry.getKey)
+        case "healthCheckRegistry" => throw new UnsupportedOperationException(entry.getKey)
+        case "dataSource" => throw new UnsupportedOperationException(entry.getKey)
+        case "threadFactory" => throw new UnsupportedOperationException(entry.getKey)
+      }
+    })
+    new HikariDataSource(config)
   }
 
 }
