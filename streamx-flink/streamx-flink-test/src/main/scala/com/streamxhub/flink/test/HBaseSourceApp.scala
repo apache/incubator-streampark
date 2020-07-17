42bb61ef5f22f601522079bcd294680c4cf6caa0
benjobs <190648@lifeat.cn>
2020-07-17 23:41:39 +0800

    hbase scan get序列化支持.

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSourceApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/HBaseSourceApp.scala
@@ -1,21 +1,42 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.streamxhub.flink.test
 
 import java.util
 
 import com.streamxhub.common.util.ConfigUtils
 import com.streamxhub.flink.core.source.HBaseSource
-import com.streamxhub.flink.core.wrapper.{HBaseGet, HBaseScan}
+import com.streamxhub.flink.core.wrapper.HBaseQuery
 import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
 import org.apache.hadoop.hbase.CellUtil
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.flink.api.scala._
+import org.apache.hadoop.hbase.client.Get
 
 object HBaseSourceApp extends FlinkStreaming {
 
   override def handler(context: StreamingContext): Unit = {
 
     implicit val conf = ConfigUtils.getHBaseConfig(context.parameter.toMap)
-    val get = new HBaseScan()
+    val get = new HBaseQuery(new Get("123322242".getBytes()))
     new HBaseSource(context).getDataStream[String]("person", get, r => {
       val map = new util.HashMap[String, String]()
       val cellScanner = r.cellScanner()
