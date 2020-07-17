ffe9df9b0763b37973eda28425fe74c1e55bc8b6
benjobs <190648@lifeat.cn>
2020-07-17 16:37:44 +0800

    重启策略支持

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/enums/RestartStrategy.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/enums/RestartStrategy.scala
@@ -1,9 +1,38 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.streamxhub.flink.core.enums
 
 
 object RestartStrategy extends Enumeration {
   type RestartStrategy = Value
   val `fixed-delay`, `failure-rate`, `none` = Value
+
+  /**
+   * fuck....
+   *
+   * @param name
+   * @return
+   */
+  def byName(name: String): Value = values.find(_.toString.replace("$minus", "-").equalsIgnoreCase(name)).get
+
 }
 
 
