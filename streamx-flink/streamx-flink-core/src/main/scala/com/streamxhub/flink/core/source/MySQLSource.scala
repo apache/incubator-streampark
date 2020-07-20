225171cd155223c2feac3d9713ef98e0f2931335
benjobs <190648@lifeat.cn>
2020-07-20 19:08:53 +0800

    MySQL HBase 可回放source支持.

--- streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MySQLSource.scala
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/source/MySQLSource.scala
@@ -26,9 +26,12 @@ import com.streamxhub.common.util.{JdbcUtils, Logger}
 import com.streamxhub.flink.core.StreamingContext
 import com.streamxhub.flink.core.enums.ApiType
 import com.streamxhub.flink.core.enums.ApiType.ApiType
-import com.streamxhub.flink.core.function.{ResultSetFunction, GetSQLFunction}
+import com.streamxhub.flink.core.function.{GetSQLFunction, ResultSetFunction}
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.runtime.state.{CheckpointListener, FunctionInitializationContext, FunctionSnapshotContext}
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
+import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
 import org.apache.flink.streaming.api.scala.DataStream
 
 import scala.annotation.meta.param
@@ -53,7 +56,7 @@ class MySQLSource(@(transient@param) val ctx: StreamingContext, overrideParams: 
    * @tparam R
    * @return
    */
-  def getDataStream[R: TypeInformation](sqlFun: => String, fun: List[Map[String, _]] => List[R])(implicit jdbc: Properties): DataStream[R] = {
+  def getDataStream[R: TypeInformation](sqlFun: () => String, fun: List[Map[String, _]] => List[R])(implicit jdbc: Properties): DataStream[R] = {
     overrideParams.foreach(x => jdbc.put(x._1, x._2))
     val mysqlFun = new MySQLSourceFunction[R](jdbc, sqlFun, fun)
     ctx.addSource(mysqlFun)
@@ -65,16 +68,23 @@ class MySQLSource(@(transient@param) val ctx: StreamingContext, overrideParams: 
  *
  * @tparam R
  */
-private[this] class MySQLSourceFunction[R: TypeInformation](apiType: ApiType = ApiType.Scala, jdbc: Properties) extends SourceFunction[R] with Logger {
+private[this] class MySQLSourceFunction[R: TypeInformation](apiType: ApiType = ApiType.Scala, jdbc: Properties) extends RichSourceFunction[R] with CheckpointListener with CheckpointedFunction with Logger {
 
-  private[this] var isRunning = true
-  private[this] var scalaSqlFunc: String = _
+  @volatile private[this] var running = true
+  private[this] var scalaSqlFunc: () => String = _
   private[this] var scalaResultFunc: Function[List[Map[String, _]], List[R]] = _
   private[this] var javaSqlFunc: GetSQLFunction = _
   private[this] var javaResultFunc: ResultSetFunction[R] = _
 
+  @transient private var sql: String = _
+
+  //value保存的是sql查询条件..
+  @transient private var state: ListState[String] = _
+
+  private val OFFSETS_STATE = "offset-states"
+
   //for Scala
-  def this(jdbc: Properties, sqlFunc: => String, resultFunc: List[Map[String, _]] => List[R]) = {
+  def this(jdbc: Properties, sqlFunc: () => String, resultFunc: List[Map[String, _]] => List[R]) = {
     this(ApiType.Scala, jdbc)
     this.scalaSqlFunc = sqlFunc
     this.scalaResultFunc = resultFunc
@@ -89,13 +99,37 @@ private[this] class MySQLSourceFunction[R: TypeInformation](apiType: ApiType = A
 
   @throws[Exception]
   override def run(@(transient@param) ctx: SourceFunction.SourceContext[R]): Unit = {
-    while (this.isRunning) {
-      apiType match {
-        case ApiType.Scala => scalaResultFunc(JdbcUtils.select(scalaSqlFunc)(jdbc)).foreach(ctx.collect)
-        case ApiType.JAVA => javaResultFunc.result(JdbcUtils.select(javaSqlFunc.getSQL)(jdbc).map(_.asJava).asJava).foreach(ctx.collect)
+    while (this.running) {
+      ctx.getCheckpointLock.synchronized {
+        sql = apiType match {
+          case ApiType.Scala => scalaSqlFunc()
+          case ApiType.JAVA => javaSqlFunc.getSQL
+        }
+        apiType match {
+          case ApiType.Scala => scalaResultFunc(JdbcUtils.select(sql)(jdbc)).foreach(ctx.collect)
+          case ApiType.JAVA => javaResultFunc.result(JdbcUtils.select(sql)(jdbc).map(_.asJava).asJava).foreach(ctx.collect)
+        }
       }
     }
   }
 
-  override def cancel(): Unit = this.isRunning = false
+  override def cancel(): Unit = this.running = false
+
+  override def notifyCheckpointComplete(checkpointId: Long): Unit = {
+    logger.info(s"[StreamX] MySQLSource checkpointComplete: $checkpointId")
+  }
+
+  override def snapshotState(context: FunctionSnapshotContext): Unit = {
+    if (!running) {
+      logger.error("[StreamX] MySQLSource snapshotState called on closed source")
+    } else {
+      state.clear()
+      state.add(sql)
+    }
+  }
+
+  override def initializeState(context: FunctionInitializationContext): Unit = {
+    state = context.getOperatorStateStore.getListState(new ListStateDescriptor(OFFSETS_STATE, classOf[String]))
+    state.get.foreach(x => sql = x)
+  }
 }
