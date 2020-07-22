188e01f5bbb1a447bdecd3dc9e2384dbea5e7c84
benjobs <190648@lifeat.cn>
2020-07-23 03:06:43 +0800

    MySQL 可回放source支持.

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/MySQLSourceApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/MySQLSourceApp.scala
@@ -4,7 +4,7 @@ import java.util.Properties
 
 import com.streamxhub.common.conf.ConfigConst._
 import com.streamxhub.flink.core.source.MySQLSource
-import com.streamxhub.flink.core.wrapper.MySQLQuery
+import com.streamxhub.flink.core.wrapper.JdbcQuery
 import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
 import org.apache.flink.streaming.api.scala._
 
@@ -21,7 +21,7 @@ object MySQLSourceApp extends FlinkStreaming {
     val mysqlSource = new MySQLSource(context)
     mysqlSource.getDataStream[Orders](q => {
       if (q == null) {
-        val query = new MySQLQuery()
+        val query = new JdbcQuery()
         query.setTable("tc_record")
         query.setField("sync_time")
         query.setOffset("2020-07-20 00:00:00")
