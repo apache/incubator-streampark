f6b12988e222157d88fab34041cc67ca36c34323
benjobs <190648@lifeat.cn>
2020-08-09 09:27:29 +0800

    Flink monitor..

--- streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
+++ streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
@@ -77,7 +77,7 @@ object FlinkSubmit extends Logger {
     val flinkHdfsPlugins = new Path(s"$flinkHdfsDir/plugins")
 
     //用户jar
-    val flinkHdfsDistJar = new File(s"$flinkHome/lib").list().filter(_.matches("flink-dist-.*\\.jar")) match {
+    val flinkHdfsDistJar = new File(s"$flinkHome/lib").list().filter(_.matches("flink-dist_.*\\.jar")) match {
       case Array() => throw new IllegalArgumentException(s"[StreamX] can no found flink-dist jar in $flinkHome/lib")
       case array if array.length == 1 => s"$flinkHdfsDir/lib/${array.head}"
       case more => throw new IllegalArgumentException(s"[StreamX] found multiple flink-dist jar in $flinkHome/lib,[${more.mkString(",")}]")
