894d33f92946280f7320aef9e11ebfc6d0007483
benjobs <190648@lifeat.cn>
2020-08-12 11:21:35 +0800

    Flink monitor..

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/enums/AppState.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/enums/AppState.java
@@ -21,43 +21,68 @@
 package com.streamxhub.flink.monitor.core.enums;
 
 import lombok.Getter;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+
+import java.util.Arrays;
 
 @Getter
 public enum AppState {
 
-    /** Application which is currently deploying. */
+    /**
+     * Application which is currently deploying.
+     */
     CREATED(0),
 
-    /** Application which is currently deploying. */
+    /**
+     * Application which is currently deploying.
+     */
     DEPLOYING(1),
 
-    /** Application which was just created. */
+    /**
+     * Application which was just created.
+     */
     NEW(2),
 
-    /** Application which is being saved. */
+    /**
+     * Application which is being saved.
+     */
     NEW_SAVING(3),
 
-    /** Application which has been submitted. */
+    /**
+     * Application which has been submitted.
+     */
     SUBMITTED(4),
 
-    /** Application has been accepted by the scheduler */
+    /**
+     * Application has been accepted by the scheduler
+     */
     ACCEPTED(5),
 
-    /** Application which is currently running. */
+    /**
+     * Application which is currently running.
+     */
     RUNNING(6),
 
-    /** Application which finished successfully. */
+    /**
+     * Application which finished successfully.
+     */
     FINISHED(7),
 
-    /** Application which failed. */
+    /**
+     * Application which failed.
+     */
     FAILED(8),
 
-    /** Application which was terminated by a user or admin. */
+    /**
+     * Application which was terminated by a user or admin.
+     */
     KILLED(9);
 
     int value;
+
     AppState(int value) {
         this.value = value;
     }
+
 
 }
