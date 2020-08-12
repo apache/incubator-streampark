060ab13296c3a40efc490b7a7b060aeaa23f51c0
benjobs <190648@lifeat.cn>
2020-08-12 17:57:49 +0800

    Flink monitor..

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/metrics/yarn/AppInfo.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/metrics/yarn/AppInfo.java
@@ -1,6 +1,5 @@
 package com.streamxhub.flink.monitor.core.metrics.yarn;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
