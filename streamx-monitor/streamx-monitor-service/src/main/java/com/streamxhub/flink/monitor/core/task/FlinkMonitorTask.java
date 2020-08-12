2a12e0ff57a8ae68410cd33b2ba794606d5e86c8
benjobs <190648@lifeat.cn>
2020-08-12 17:50:06 +0800

    Flink monitor..

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/task/FlinkMonitorTask.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/core/task/FlinkMonitorTask.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.streamxhub.flink.monitor.core.task;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -34,7 +54,7 @@ public class FlinkMonitorTask {
                 AppState.CREATED.getValue(),
                 AppState.FINISHED.getValue(),
                 AppState.FAILED.getValue(),
-                AppState.KILLED.getValue(),
+                AppState.CANCELED.getValue(),
                 AppState.LOST.getValue()
         );
         List<Application> appList = applicationService.list(queryWrapper);
@@ -62,7 +82,7 @@ public class FlinkMonitorTask {
                     applicationService.updateById(application);
                     jobStateMap.put(application.getId(), state);
                 }
-                if (state == AppState.FAILED || state == AppState.FINISHED || state == AppState.KILLED) {
+                if (state == AppState.FAILED || state == AppState.FINISHED || state == AppState.CANCELED) {
                     jobStateMap.remove(application.getId());
                 }
             } catch (Exception e) {
@@ -72,11 +92,17 @@ public class FlinkMonitorTask {
                          * 2)到yarn的restApi中查询状态
                          */
                         AppInfo appInfo = application.getYarnAppInfo();
-                        String state = appInfo.getState();
-                        application.setState(AppState.valueOf(state).getValue());
+                        String state = appInfo.getApp().getFinalStatus();
+                        AppState appState = null;
+                        if("KILLED".equals(state)) {
+                            appState = AppState.CANCELED;
+                        }else {
+                            appState = AppState.valueOf(state);
+                        }
+                        application.setState(appState.getValue());
                         applicationService.updateById(application);
                     } catch (Exception e1) {
-                        /**
+                        /**s
                          * 3)如果从flink的restAPI和yarn的restAPI都查询失败,则任务失联.
                          */
                         application.setState(AppState.LOST.getValue());
