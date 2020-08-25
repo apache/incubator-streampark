705934994612f681d0bc8615b72cbe0005f549bf
benjobs <190648@lifeat.cn>
2020-08-25 10:19:37 +0800

    JdbcSink 两阶段精准一次sink

--- streamx-monitor/streamx-monitor-webapp/src/views/flink/app/View.vue
+++ streamx-monitor/streamx-monitor-webapp/src/views/flink/app/View.vue
@@ -71,9 +71,9 @@
       </template>
 
       <template slot="deployMode" slot-scope="mode">
-        <a-tag color="blue" v-if="mode === 'yarn-per-job'">Per-Job</a-tag>
-        <a-tag color="cyan" v-if="mode === 'yarn-session'">Session</a-tag>
-        <a-tag color="green" v-if="mode === 'yarn-application'">Application</a-tag>
+        <a-tag color="blue" v-if="mode === 'PER-JOB'">Per-Job</a-tag>
+        <a-tag color="cyan" v-if="mode === 'SESSION'">Session</a-tag>
+        <a-tag color="green" v-if="mode === 'APPLICATION'">Application</a-tag>
       </template>
 
       <template slot="state" slot-scope="state">
