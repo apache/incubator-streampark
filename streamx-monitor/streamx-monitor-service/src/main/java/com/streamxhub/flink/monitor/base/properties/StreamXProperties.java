6f4974f79f5548992789692918f79a98f1d17c1e
benjobs <190648@lifeat.cn>
2020-08-27 08:02:06 +0800

    JdbcSink 两阶段精准一次sink

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/base/properties/StreamXProperties.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/flink/monitor/base/properties/StreamXProperties.java
@@ -24,16 +24,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "streamx")
 public class StreamXProperties {
 
     private String appHome;
-
-    private String nameService;
 
     private String yarn;
 
