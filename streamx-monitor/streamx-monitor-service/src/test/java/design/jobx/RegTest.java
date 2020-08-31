4370d1645ea35d7b08d23d64650c78f1f5eb3e14
benjobs <190648@lifeat.cn>
2020-08-31 17:27:57 +0800

    去除monitor模块对redis的依赖

--- streamx-monitor/streamx-monitor-service/src/test/java/design/jobx/RegTest.java
+++ streamx-monitor/streamx-monitor-service/src/test/java/design/jobx/RegTest.java
@@ -1,10 +1,18 @@
 package design.jobx;
 
+import java.util.Optional;
+
 public class RegTest {
     public static void main(String[] args) {
         String uploadFormat = ".*tar\\.gz$|.*jar$";
 
         String file = "flink.tar.gz";
         System.out.println(file.matches(uploadFormat));
+
+        String application = null;
+
+        String[] array =  Optional.of(application.split("\\s+")).orElse(new String[0]);
+        System.out.println(array);
+
     }
 }
