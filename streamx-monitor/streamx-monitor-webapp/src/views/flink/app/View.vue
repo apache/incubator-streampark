f72dd71c7cd1ecce92165b969b0ceb28ed30274e
benjobs <190648@lifeat.cn>
2020-09-02 12:01:15 +0800

    bug fix!

--- streamx-monitor/streamx-monitor-webapp/src/views/flink/app/View.vue
+++ streamx-monitor/streamx-monitor-webapp/src/views/flink/app/View.vue
@@ -119,18 +119,18 @@
         -->
         <div class="app_state">
           <a-tag color="#2f54eb" v-if="state === 0">CREATED</a-tag>
-          <a-tag color="#1ABBDC" v-if="state === 1">DEPLOYING</a-tag>
+          <a-tag color="#1ABBDC" v-if="state === 1" class="status-processing-deploying">DEPLOYING</a-tag>
           <a-tag color="#108ee9" v-if="state === 2">DEPLOYED</a-tag>
-          <a-tag color="#1AB58E" v-if="state === 3">STARTING</a-tag>
-          <a-tag color="#13c2c2" v-if="state === 4">RESTARTING</a-tag>
-          <a-tag color="#52c41a" v-if="state === 5">RUNNING</a-tag>
-          <a-tag color="#fa541c" v-if="state === 6">FAILING</a-tag>
+          <a-tag color="#1AB58E" v-if="state === 3" class="status-processing-starting">STARTING</a-tag>
+          <a-tag color="#13c2c2" v-if="state === 4" class="status-processing-restarting">RESTARTING</a-tag>
+          <a-tag color="#52c41a" v-if="state === 5" class="status-processing-running">RUNNING</a-tag>
+          <a-tag color="#fa541c" v-if="state === 6" class="status-processing-failing">FAILING</a-tag>
           <a-tag color="#f5222d" v-if="state === 7">FAILED</a-tag>
-          <a-tag color="#faad14" v-if="state === 8">CANCELLING</a-tag>
-          <a-tag color="#fa8c16" v-if="state === 9">CANCELED</a-tag>
+          <a-tag color="#faad14" v-if="state === 8" class="status-processing-cancelling">CANCELLING</a-tag>
+          <a-tag color="#fa8c16" v-if="state === 9" >CANCELED</a-tag>
           <a-tag color="#1890ff" v-if="state === 10">FINISHED</a-tag>
           <a-tag color="#722ed1" v-if="state === 11">SUSPENDED</a-tag>
-          <a-tag color="#eb2f96" v-if="state === 12">RECONCILING</a-tag>
+          <a-tag color="#eb2f96" v-if="state === 12" class="status-processing-reconciling">RECONCILING</a-tag>
           <a-tag color="#000000" v-if="state === 13">LOST</a-tag>
         </div>
       </template>
@@ -351,9 +351,9 @@ export default {
   mounted() {
     this.handleYarn()
     this.handleFetch(true)
-    const timer = window.setInterval(() => this.handleFetch(false), 1000)
+    //const timer = window.setInterval(() => this.handleFetch(false), 1000)
     this.$once('hook:beforeDestroy', () => {
-      clearInterval(timer);
+      //clearInterval(timer);
     })
   },
 
@@ -582,6 +582,92 @@ export default {
 
 .app_list >>> .ant-table-thead > tr > td, .app_list >>> .ant-table-tbody > tr > td {
   padding: 9px 9px !important;
+}
+
+.status-processing-deploying{ animation: deploying 1000ms ease-out infinite alternate; }
+.status-processing-starting{ animation: starting 1000ms ease-out infinite alternate; }
+.status-processing-restarting{ animation: restarting 1000ms ease-out infinite alternate; }
+.status-processing-running { animation: running 1000ms ease-out infinite alternate; }
+.status-processing-failing { animation: failing 1000ms ease-out infinite alternate; }
+.status-processing-cancelling { animation: cancelling 1000ms ease-out infinite alternate; }
+.status-processing-reconciling { animation: reconciling 1000ms ease-out infinite alternate; }
+
+@keyframes deploying {
+  0% {
+    border-color: #1ABBDC;
+    box-shadow: 0 0 1px #1ABBDC, inset 0 0 2px #1ABBDC;
+  }
+  100% {
+    border-color: #1ABBDC;
+    box-shadow: 0 0 10px #1ABBDC, inset 0 0 5px #1ABBDC;
+  }
+}
+
+@keyframes starting {
+  0% {
+    border-color: #1AB58E;
+    box-shadow: 0 0 1px #1AB58E, inset 0 0 2px #1AB58E;
+  }
+  100% {
+    border-color: #1AB58E;
+    box-shadow: 0 0 10px #1AB58E, inset 0 0 5px #1AB58E;
+  }
+}
+
+@keyframes restarting {
+  0% {
+    border-color: #13c2c2;
+    box-shadow: 0 0 1px #13c2c2, inset 0 0 2px #13c2c2;
+  }
+  100% {
+    border-color: #13c2c2;
+    box-shadow: 0 0 10px #13c2c2, inset 0 0 5px #13c2c2;
+  }
+}
+
+@keyframes running {
+  0% {
+    border-color: #52c41a;
+    box-shadow: 0 0 1px #52c41a, inset 0 0 2px #52c41a;
+  }
+  100% {
+    border-color: #52c41a;
+    box-shadow: 0 0 10px #52c41a, inset 0 0 5px #52c41a;
+  }
+}
+
+@keyframes failing {
+  0% {
+    border-color: #fa541c;
+    box-shadow: 0 0 1px #fa541c, inset 0 0 2px #fa541c;
+  }
+  100% {
+    border-color: #fa541c;
+    box-shadow: 0 0 10px #fa541c, inset 0 0 5px #fa541c;
+  }
+}
+
+
+@keyframes cancelling {
+  0% {
+    border-color: #faad14;
+    box-shadow: 0 0 1px #faad14, inset 0 0 2px #faad14;
+  }
+  100% {
+    border-color: #faad14;
+    box-shadow: 0 0 10px #faad14, inset 0 0 5px #faad14;
+  }
+}
+
+@keyframes reconciling {
+  0% {
+    border-color: #eb2f96;
+    box-shadow: 0 0 1px #eb2f96, inset 0 0 2px #eb2f96;
+  }
+  100% {
+    border-color: #eb2f96;
+    box-shadow: 0 0 10px #eb2f96, inset 0 0 5px #eb2f96;
+  }
 }
 
 </style>
