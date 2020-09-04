eb5b6f0b01a95a41a7440dd12abdb717f2c05b00
benjobs <190648@lifeat.cn>
2020-09-04 21:52:24 +0800

    RedisSink 两阶段提交,精准一次语义实现.

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/ConnectorApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/ConnectorApp.scala
@@ -2,7 +2,6 @@ package com.streamxhub.flink.test
 
 import java.util.Date
 
-import com.streamxhub.common.util.Logger
 import com.streamxhub.flink.core.sink.ESSink
 import com.streamxhub.flink.core.{FlinkStreaming, StreamingContext}
 import com.streamxhub.flink.core.util.EsIndexUtils
