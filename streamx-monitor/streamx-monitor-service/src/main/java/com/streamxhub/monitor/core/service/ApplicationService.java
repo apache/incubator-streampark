28a159449b9ac9fbc76afeb16f026e99a496a9ff
benjobs <190648@lifeat.cn>
2020-09-16 17:00:52 +0800

    flink REPL初步实现

--- streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/monitor/core/service/ApplicationService.java
+++ streamx-monitor/streamx-monitor-service/src/main/java/com/streamxhub/monitor/core/service/ApplicationService.java
@@ -43,7 +43,7 @@ public interface ApplicationService extends IService<Application> {
 
     AppExistsState checkExists(Application app);
 
-    void deploy(Application app,boolean backUp,boolean restart) throws Exception;
+    void deploy(Application app) throws Exception;
 
     void updateDeploy(Application app);
 
