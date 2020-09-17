ae8ec198596f6f17713424e424a9a7d7b2d9d615
benjobs <190648@lifeat.cn>
2020-09-17 10:17:46 +0800

    streamx-monitor rename to streamx-console

--- streamx-monitor/streamx-monitor-webapp/src/layouts/index.js
+++
@@ -1,6 +0,0 @@
-import EmptyView from './EmptyView'
-import BasicView from './BasicView'
-import RouteView from './RouteView'
-import PageView from './PageView'
-
-export { BasicView, EmptyView, RouteView, PageView }
