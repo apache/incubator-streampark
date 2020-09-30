319b9952a0d2842b4606eced817b13d7dc2e1496
benjobs <190648@lifeat.cn>
2020-09-30 16:39:01 +0800

    console-service去除多余的模块

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/system/dao/LogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.streamxhub.spark.monitor.system.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.streamxhub.spark.monitor.system.domain.SysLog;
-
-public interface LogMapper extends BaseMapper<SysLog> {
-}
--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/system/service/LoginLogService.java
+++
@@ -1,9 +0,0 @@
-package com.streamxhub.spark.monitor.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.streamxhub.spark.monitor.system.domain.LoginLog;
-
-public interface LoginLogService extends IService<LoginLog> {
-
-    void saveLoginLog(LoginLog loginLog);
-}
