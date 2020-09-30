2b9a4754133e67e7b139014eeb7114a002960d42
benjobs <190648@lifeat.cn>
2020-09-30 16:39:01 +0800

    console-service去除多余的模块

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/common/function/CacheSelector.java
+++
@@ -1,6 +0,0 @@
-package com.streamxhub.spark.monitor.common.function;
-
-@FunctionalInterface
-public interface CacheSelector<T> {
-    T select() throws Exception;
-}
--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/system/dao/DictMapper.java
+++
@@ -1,7 +0,0 @@
-package com.streamxhub.spark.monitor.system.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.streamxhub.spark.monitor.system.domain.Dict;
-
-public interface DictMapper extends BaseMapper<Dict> {
-}
