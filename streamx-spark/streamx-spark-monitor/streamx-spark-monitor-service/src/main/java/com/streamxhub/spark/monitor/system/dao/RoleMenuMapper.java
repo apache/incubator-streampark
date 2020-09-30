829e202c2e60a7769454a88a0d0123ada232ff5a
benjobs <190648@lifeat.cn>
2020-09-30 16:39:01 +0800

    console-service去除多余的模块

--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-service/src/main/java/com/streamxhub/spark/monitor/system/dao/RoleMenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.streamxhub.spark.monitor.system.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.streamxhub.spark.monitor.system.domain.RoleMenu;
-
-public interface RoleMenuMapper extends BaseMapper<RoleMenu> {
-}
--- streamx-spark/streamx-spark-monitor/streamx-spark-monitor-webapp/src/utils/common.js
+++
@@ -1,6 +0,0 @@
-export function triggerWindowResizeEvent() {
-    let event = document.createEvent('HTMLEvents')
-    event.initEvent('resize', true, true)
-    event.eventType = 'message'
-    window.dispatchEvent(event)
-}
