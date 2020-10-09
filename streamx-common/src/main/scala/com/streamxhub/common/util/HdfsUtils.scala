4f619b0826af705a26dd5224f6983b0cee7543b5
benjobs <190648@lifeat.cn>
2020-10-09 09:17:08 +0800

    console-service去除多余的模块

--- streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
+++ streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
@@ -21,7 +21,7 @@
 package com.streamxhub.common.util
 
 import org.apache.hadoop.hdfs.HAUtil
-import java.io.{ByteArrayOutputStream, File, FileWriter, IOException}
+import java.io.{ByteArrayOutputStream, FileWriter, IOException}
 
 import org.apache.commons.lang.StringUtils
 import org.apache.hadoop.conf.Configuration
@@ -44,28 +44,14 @@ object HdfsUtils extends Logger {
   /**
    * 注意:加载hadoop配置文件,有两种方式:
    * 1) 将hadoop的core-site.xml,hdfs-site.xml,yarn-site.xml copy到 resources下
-   * 2) 可以在部署机器中找到 $HADOOP_HOME/etc/hadoop下的core-site.xml,hdfs-site.xml,yarn-site.xml
+   * 2) 项目在启动时动态加载 $HADOOP_HOME/etc/hadoop下的core-site.xml,hdfs-site.xml,yarn-site.xml 到 classpath中
+   *    e.g: java -cp $HADOOP_HOME/etc/hadoop ...
    * 推荐第二种方法,不用copy配置文件.
    */
   lazy val conf: Configuration = {
-
-    def checkConfInClassPath(): Boolean =
-      classLoader.getResource("core-site.xml") != null &&
-        classLoader.getResource("hdfs-site.xml") != null
-
     val conf = {
-      if (!checkConfInClassPath) {
-        logger.warn("[StreamX] can't found (core-default.xml|core-site.xml) in classpath,now load conf in $HADOOP_HOME/etc/hadoop ...")
-        val hadoopHome = SystemPropertyUtils.get("HADOOP_HOME") match {
-          case null => System.getenv("HADOOP_HOME") match {
-            case null => throw new IllegalArgumentException("[StreamX] HADOOP_HOME is not defined ")
-            case other => other
-          }
-          case other => other
-        }
-        ClassLoaderUtils.loadResourceDir(s"$hadoopHome/etc/hadoop/")
-      }
       val conf = new Configuration()
+      conf.setClassLoader(classLoader)
       if (StringUtils.isBlank(conf.get("hadoop.tmp.dir"))) {
         conf.set("hadoop.tmp.dir", "/tmp")
       }
