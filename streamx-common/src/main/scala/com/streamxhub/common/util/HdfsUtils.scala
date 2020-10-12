0f4790578a3cab7e30fecadae5bdb3feeeb7df85
benjobs <190648@lifeat.cn>
2020-10-12 09:36:17 +0800

    动态加载hadoop conf到classpath.

--- streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
+++ streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
@@ -45,7 +45,7 @@ object HdfsUtils extends Logger {
    * 注意:加载hadoop配置文件,有两种方式:
    * 1) 将hadoop的core-site.xml,hdfs-site.xml,yarn-site.xml copy到 resources下
    * 2) 项目在启动时动态加载 $HADOOP_HOME/etc/hadoop下的配置 到 classpath中
-   * e.g: java -cp $HADOOP_HOME/etc/hadoop ...
+   * e.g: -Xbootclasspath/a:$HADOOP_HOME/etc/hadoop ...
    * 推荐第二种方法,不用copy配置文件.
    */
   lazy val conf: Configuration = {
@@ -134,7 +134,7 @@ object HdfsUtils extends Logger {
 
   @throws[IOException] def mkdirs(fileName: String): Unit = {
     val path: Path = getPath(fileName)
-    hdfs.mkdirs(path);
+    hdfs.mkdirs(path)
   }
 
   @throws[Exception] def download(fileName: String, localPath: String): Unit = {
