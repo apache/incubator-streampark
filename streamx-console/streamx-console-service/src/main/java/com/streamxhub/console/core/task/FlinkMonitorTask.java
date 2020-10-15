39e23191c2a6e9797d6b7a64d086299132d027d1
benjobs <190648@lifeat.cn>
2020-10-15 13:12:22 +0800

    big fix

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/task/FlinkMonitorTask.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/task/FlinkMonitorTask.java
@@ -104,7 +104,7 @@ public class FlinkMonitorTask {
                 /**
                  * 上一次的状态为canceling(在获取上次信息的时候flink restServer还未关闭为canceling),且本次如获取不到状态(flink restServer已关闭),则认为任务已经CANCELED
                  */
-                if (canceling.containsKey(application.getId()) && canceling.get(application.getId()).getNext() == index) {
+                if (canceling.containsKey(application.getId()) && canceling.get(application.getId()).isPrevious(index)) {
                     application.setState(FlinkAppState.CANCELED.getValue());
                     applicationService.updateMonitor(application);
                     canceling.remove(application.getId());
@@ -215,20 +215,18 @@ public class FlinkMonitorTask {
 
     }
 
-
     @Getter
     class Tracker implements Serializable {
-        private Long index;
-        private Long appId;
+        private long index;
+        private long appId;
 
-        public Tracker(Long index, Long appId) {
+        public Tracker(long index, long appId) {
             this.index = index;
             this.appId = appId;
         }
 
-        public Long getNext() {
-            return index + 1;
+        public boolean isPrevious(long index) {
+            return index - this.index == 1;
         }
-
     }
 }
