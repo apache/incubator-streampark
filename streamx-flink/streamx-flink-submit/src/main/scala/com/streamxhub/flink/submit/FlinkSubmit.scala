c5d118e5c27058aa64958b17bc55f5ffadcc2ee3
benjobs <190648@lifeat.cn>
2020-10-21 13:45:35 +0800

    load plugins

--- streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
+++ streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
@@ -313,7 +313,7 @@ object FlinkSubmit extends Logger {
         submitInfo.dynamicOption.foreach(x => array += x.replaceFirst("^-D|^", "-D"))
 
         //-jvm profile support
-        array += "-Denv.java.opts=-javaagent:jvm-profiler-1.0.0.jar=sampleInterval=50"
+        array += "-Denv.java.opts.taskmanager=-javaagent:jvm-profiler-1.0.0.jar=sampleInterval=50"
 
         array.toArray
 
