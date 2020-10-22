9eb79cc8659012fc03816de0beefaca2facd67d6
benjobs <190648@lifeat.cn>
2020-10-22 20:36:09 +0800

    load plugins

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/controller/ApplicationController.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/controller/ApplicationController.java
@@ -42,7 +42,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.*;
 
 /**
