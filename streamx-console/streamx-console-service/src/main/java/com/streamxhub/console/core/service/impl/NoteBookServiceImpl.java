d35930d3f463f8b6c3120fd07ad124ce68f0434a
benjobs <190648@lifeat.cn>
2020-10-23 18:07:40 +0800

    noteboot support!

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/service/impl/NoteBookServiceImpl.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/service/impl/NoteBookServiceImpl.java
@@ -6,9 +6,11 @@ import com.streamxhub.repl.flink.interpreter.FlinkInterpreter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zeppelin.display.AngularObjectRegistry;
 import org.apache.zeppelin.interpreter.*;
+import org.apache.zeppelin.interpreter.remote.RemoteInterpreterEventClient;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import static org.mockito.Mockito.mock;
+
 import java.util.Properties;
 import java.util.concurrent.Executors;
 
@@ -19,28 +21,22 @@ import static org.junit.Assert.assertEquals;
 public class NoteBookServiceImpl implements NoteBookService {
 
     private FlinkInterpreter interpreter;
-    private AngularObjectRegistry angularObjectRegistry;
 
-    @PostConstruct
-    public void setUp() throws InterpreterException {
+    private InterpreterContext getInterpreterContext() throws InterpreterException {
         Properties prop = new Properties();
         prop.setProperty("zeppelin.flink.printREPLOutput", "true");
         prop.setProperty("zeppelin.flink.scala.color", "false");
         prop.setProperty("flink.execution.mode", "local");
-        prop.setProperty("local.number-taskmanager", "4");
         interpreter = new FlinkInterpreter(prop);
         InterpreterGroup interpreterGroup = new InterpreterGroup();
         interpreter.setInterpreterGroup(interpreterGroup);
         interpreter.open();
-        angularObjectRegistry = new AngularObjectRegistry("flink", null);
-    }
-
-    private InterpreterContext getInterpreterContext() {
+        AngularObjectRegistry angularObjectRegistry = new AngularObjectRegistry("flink", null);
         InterpreterContext context = InterpreterContext.builder()
                 .setParagraphId("paragraphId")
                 .setInterpreterOut(new InterpreterOutput(null))
                 .setAngularObjectRegistry(angularObjectRegistry)
-                //.setIntpEventClient(mock(RemoteInterpreterEventClient.class))
+                .setIntpEventClient(mock(RemoteInterpreterEventClient.class))
                 .build();
         InterpreterContext.set(context);
         return context;
@@ -49,13 +45,21 @@ public class NoteBookServiceImpl implements NoteBookService {
     @Override
     public void submit(Note note) throws Exception {
         Executors.newSingleThreadExecutor().submit(() -> {
-            try{
+            try {
                 InterpreterContext context = getInterpreterContext();
                 InterpreterResult result = interpreter.interpret(note.getSourceCode(), context);
                 System.out.println(context.out.toString());
                 assertEquals(InterpreterResult.Code.SUCCESS, result.code());
-            }catch (Exception e) {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
+            } finally {
+                if (interpreter != null) {
+                    try {
+                        interpreter.close();
+                    } catch (InterpreterException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
