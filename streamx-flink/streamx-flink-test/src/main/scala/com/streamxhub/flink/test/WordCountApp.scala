7fd96d8534dba629f1a2e56b36257d8795bd7347
benjobs <190648@lifeat.cn>
2020-10-31 12:48:24 +0800

    notebook support

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/WordCountApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/flink/test/WordCountApp.scala
@@ -7,11 +7,17 @@ object WordCountApp extends App {
 
   val senv = StreamExecutionEnvironment.getExecutionEnvironment
 
-  case class WoldCount(wold: String,count: Int)
-  senv.fromElements("hello world", "hello flink", "hello hadoop").flatMap(_.split("\\s+")).map(WoldCount(_, 1)).keyBy("wold").sum("count").print()
+  case class WoldCount(wold: String, count: Int)
+
+  senv.fromElements("hello world", "hello flink", "hello hadoop")
+    .flatMap(_.split("\\s+"))
+    .map(WoldCount(_, 1))
+    .keyBy(_.wold)
+    .sum("count")
+    .print()
 
   senv.execute()
 
 }
 
-case class WoldCount(wold: String,count: Int)
+case class WoldCount(wold: String, count: Int)
