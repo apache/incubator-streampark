3a88bd5b0ce7635165531e9945d0292f120590b7
benjobs <190648@lifeat.cn>
2020-11-02 13:11:27 +0800

    notebook support

--- streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
+++ streamx-common/src/main/scala/com/streamxhub/common/util/HdfsUtils.scala
@@ -21,9 +21,7 @@
 package com.streamxhub.common.util
 
 import org.apache.hadoop.hdfs.HAUtil
-import java.io.{ByteArrayOutputStream, File, FileWriter, IOException}
-import java.net.URLClassLoader
-
+import java.io.{ByteArrayOutputStream, FileWriter, IOException}
 import org.apache.commons.lang.StringUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FSDataOutputStream
@@ -40,7 +38,6 @@ object HdfsUtils extends Logger {
    * 注意:加载hadoop配置文件,有两种方式:
    * 1) 将hadoop的core-site.xml,hdfs-site.xml,yarn-site.xml copy到 resources下
    * 2) 项目在启动时动态加载 $HADOOP_HOME/etc/hadoop下的配置 到 classpath中
-   * e.g: -Xbootclasspath/a:$HADOOP_HOME/etc/hadoop ...
    * 推荐第二种方法,不用copy配置文件.
    */
   lazy val conf: Configuration = {
