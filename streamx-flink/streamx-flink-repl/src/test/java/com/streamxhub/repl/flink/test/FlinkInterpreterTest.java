e9a63ef6d5178c1113493b0c2dbda4202994a5ea
benjobs <190648@lifeat.cn>
2020-11-05 11:45:29 +0800

    notebook

--- streamx-flink/streamx-flink-repl/src/test/java/com/streamxhub/repl/flink/test/FlinkInterpreterTest.java
+++ streamx-flink/streamx-flink-repl/src/test/java/com/streamxhub/repl/flink/test/FlinkInterpreterTest.java
@@ -1,4 +1,5 @@
-package com.streamxhub.repl.flink.test;/*
+package com.streamxhub.repl.flink.test;
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
