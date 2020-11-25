f214154893e7315e654789c2c38c5656c07daf97
benjobs <190648@lifeat.cn>
2020-11-25 16:07:44 +0800

    scala和java api包结构重新规整

---
+++ streamx-flink/streamx-flink-core/src/main/scala/com/streamxhub/flink/core/scala/util/ElasticSearchUtils.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.streamxhub.flink.core.scala.util
+
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.common.bytes.BytesArray
+import org.elasticsearch.common.xcontent.XContentType
+
+object ElasticSearchUtils {
+
+  def indexRequest(index: String, indexType: String, id: String, source: String)(implicit xContentType: XContentType = XContentType.JSON): IndexRequest = {
+    require(source != null, "indexRequest error:source can not be null...")
+    require(xContentType != null, "indexRequest error:xContentType can not be null...")
+    val indexReq = new IndexRequest(index, indexType, id)
+    val mapping = List("source" -> new BytesArray(source), "contentType" -> xContentType)
+    mapping.foreach { x =>
+      val field = indexReq.getClass.getDeclaredField(x._1)
+      field.setAccessible(true)
+      field.set(indexReq, x._2)
+    }
+    indexReq
+  }
+
+
+}
