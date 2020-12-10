bfa9cc1d4904375f2bb3da3f945395c95b89db1a
benjobs <benjobs@qq.com>
2020-12-10 17:46:53 +0800

    metrics dashboard support!

--- streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
+++ streamx-flink/streamx-flink-submit/src/main/scala/com/streamxhub/flink/submit/FlinkSubmit.scala
@@ -314,7 +314,7 @@ object FlinkSubmit extends Logger {
         //-D 动态参数配置....
         submitInfo.dynamicOption.foreach(x => array += x.replaceFirst("^-D|^", "-D"))
         //-jvm profile support,what fuck......
-        //array +="-Denv.java.opts.jobmanager=-javaagent:jvm-profiler-1.0.0.jar=sampleInterval=50"
+        array +="-Denv.java.opts.taskmanager=-javaagent:lib/jvm-profiler-1.0.0.jar=sampleInterval=50"
 
         array.toArray
 
