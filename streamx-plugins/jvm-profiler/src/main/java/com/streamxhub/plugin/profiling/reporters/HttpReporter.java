da00baf8bbe86ba3d54f056ab8e063c72bc32258
benjobs <benjobs@qq.com>
2020-12-11 14:47:38 +0800

    flame graph support!

--- streamx-plugins/jvm-profiler/src/main/java/com/streamxhub/plugin/profiling/reporters/HttpReporter.java
+++ streamx-plugins/jvm-profiler/src/main/java/com/streamxhub/plugin/profiling/reporters/HttpReporter.java
@@ -59,20 +59,17 @@ public class HttpReporter implements Reporter {
 
     @Override
     public void report(String profilerName, Map<String, Object> metrics) {
-        new Thread(() -> {
-            metrics.put("$id", id);
-            metrics.put("$token", token);
-            metrics.put("$type", type);
-            String json = JsonUtils.serialize(metrics);
-            System.out.println(json);
-            Map<String, Object> params = new HashMap<>(1);
-            params.put("text", IOUtils.zipString(json));
-            try {
-                HttpClientUtils.httpPostRequest(url, params);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        }).start();
+        metrics.put("$id", id);
+        metrics.put("$token", token);
+        metrics.put("$type", type);
+        String json = JsonUtils.serialize(metrics);
+        Map<String, Object> params = new HashMap<>(1);
+        params.put("text", IOUtils.zipString(json));
+        try {
+            HttpClientUtils.httpPostRequest(url, params);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
