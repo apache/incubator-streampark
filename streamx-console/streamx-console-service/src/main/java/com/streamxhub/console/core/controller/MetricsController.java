a653add1b09faad6956a8e259d875517c6f3fa2f
benjobs <benjobs@qq.com>
2020-12-24 12:19:59 +0800

    flamegraph support!

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/controller/MetricsController.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/controller/MetricsController.java
@@ -33,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -68,7 +69,7 @@ public class MetricsController {
 
 
     @PostMapping("flamegraph")
-    public RestResponse flameGraph(FlameGraph flameGraph) {
+    public RestResponse flameGraph(FlameGraph flameGraph) throws IOException {
         return flameGraphService.generateFlameGraph(flameGraph);
     }
 
