0c1107b6f0cb85c840da3b03935be8a692ddc083
benjobs <benjobs@qq.com>
2020-12-27 11:37:09 +0800

    Source返回最后一个查询对象.

--- streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/test/stream/MongoSourceApp.scala
+++ streamx-flink/streamx-flink-test/src/main/scala/com/streamxhub/test/stream/MongoSourceApp.scala
@@ -13,10 +13,9 @@ object MongoSourceApp extends FlinkStreaming {
   override def handle(context: StreamingContext): Unit = {
     implicit val prop = context.parameter.getProperties
     val source = new MongoSource(context)
-    source.getDataStream[String](x => {
-      val collection = x.getCollection("shop")
+    source.getDataStream[String]("shop", (a, d) => {
       val cond = new BasicDBObject().append("updateTime", new BasicDBObject("$gte", DateUtils.parse("2019-09-27 00:00:00")))
-      collection.find(cond)
+      d.find(cond)
     }, x => {
       val list = new ListBuffer[String]
       while (x.hasNext) {
