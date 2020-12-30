7cb7bd78e8b22dc02d10049e4b1f68b63b56ebed
benjobs <benjobs@qq.com>
2020-12-30 11:13:33 +0800

    flink sql submit support!

---
+++ streamx-flink/streamx-flink-cli/src/main/scala/com/streamxhub/flink/cli/SQLCommandUtil.scala
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.streamxhub.flink.cli
+
+import java.util.List
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ArrayBuffer
+import scala.util.matching.Regex
+import enumeratum.EnumEntry
+
+object SQLCommandUtil {
+
+  def parseSQL(lines: List[String]): List[SQLCommandCall] = {
+    val calls = new ArrayBuffer[SQLCommandCall]
+    val stmt = new StringBuilder
+    for (line <- lines if line.trim.nonEmpty && !line.startsWith("--")) {
+      stmt.append("\n").append(line)
+      if (line.trim.endsWith(";")) {
+        parseSQL(stmt.toString.trim) match {
+          case Some(x) => calls += x
+          case _ => throw new RuntimeException("Unsupported command '" + stmt.toString + "'")
+        }
+        // clear string builder
+        stmt.clear()
+      }
+    }
+    calls
+  }
+
+  def parseSQL(sqlLine: String): Option[SQLCommandCall] = { // normalize
+    var stmt = sqlLine.trim
+    // remove ';' at the end
+    if (stmt.endsWith(";")) stmt = stmt.dropRight(1)
+    // parse
+    val sqlType = SQLType.values.filter(_.regex.pattern.matcher(stmt).matches()).head
+    if (sqlType != null) {
+      val matcher = sqlType.regex.pattern.matcher(stmt)
+      val groups = new Array[String](matcher.groupCount)
+      for (i <- 0 until groups.length) {
+        groups(i) = matcher.group(i + 1)
+      }
+      return sqlType.converter(groups).map(operands => new SQLCommandCall(sqlType, operands))
+    }
+    None
+  }
+
+}
+
+
+sealed abstract class SQLType(val regex: Regex, val converter: Array[String] => Option[Array[String]]) extends EnumEntry
+
+object SQLType extends enumeratum.Enum[SQLType] {
+
+  val values = findValues
+
+  case object SET extends SQLType(
+    "(\\s+(\\S+)\\s*=(.*))?".r,
+    operands =>
+      operands match {
+        case x if x.length < 3 => None
+        case x if x(0) == null => Some(new Array[String](0))
+        case _ => Some(Array[String](operands(1), operands(2)))
+      }
+  )
+
+  case object INSERT_INTO extends SQLType(
+    "(INSERT\\s+INTO.*)".r,
+    (operands: Array[String]) => Some(Array[String](operands(0)))
+  )
+
+  case object CREATE_TABLE extends SQLType(
+    "(CREATE\\s+TABLE.*)".r,
+    (operands: Array[String]) => Some(Array[String](operands(0)))
+  )
+
+  case object CREATE_VIEW extends SQLType(
+    "(CREATE\\s+VIEW.*)".r,
+    (operands: Array[String]) => Some(Array[String](operands(0)))
+  )
+
+}
+
+/**
+ * Call of SQL command with operands and command type.
+ */
+class SQLCommandCall(val command: SQLType, val operands: Array[String]) {
+  def this(command: SQLType) {
+    this(command, new Array[String](0))
+  }
+}
