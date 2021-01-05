a245a47169062c497e9d01a12749ced4138372e8
benjobs <benjobs@qq.com>
2021-01-05 19:01:07 +0800

    shell unix style

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/service/impl/FlameGraphServiceImpl.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/console/core/service/impl/FlameGraphServiceImpl.java
@@ -36,14 +36,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import scala.Function1;
-import scala.runtime.BoxedUnit;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * @author benjobs
