e7e753128e26fe64d82713c8774d36e6a451b137
benjobs <benjobs@qq.com>
2021-03-03 15:03:29 +0800

    HdfsUtils copyDir support

---
+++ streamx-common/src/main/scala/com/streamxhub/streamx/common/util/HadoopUtils.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2019 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.streamxhub.streamx.common.util
+
+
+import com.google.common.io.Files
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.yarn.api.records.ApplicationId
+import org.apache.hadoop.yarn.client.api.YarnClient
+import org.apache.hadoop.yarn.conf.YarnConfiguration
+import org.apache.hadoop.yarn.exceptions.YarnException
+
+import java.io.{File, IOException}
+
+
+object HadoopUtils extends Logger {
+
+  @throws[IOException]
+  @throws[YarnException]
+  def getYarnAppTrackingUrl(applicationId: ApplicationId): String = {
+    val yarnClient = YarnClient.createYarnClient
+    val yarnConf = new YarnConfiguration
+    // disable timeline service as we only query yarn app here.
+    // Otherwise we may hit this kind of ERROR:
+    // java.lang.ClassNotFoundException: com.sun.jersey.api.client.config.ClientConfig
+    yarnConf.set("yarn.timeline-service.enabled", "false")
+    yarnClient.init(yarnConf)
+    yarnClient.start()
+    yarnClient.getApplicationReport(applicationId).getTrackingUrl
+  }
+
+  @throws[IOException]
+  def downloadJar(jarOnHdfs: String): String = {
+    val tmpDir = Files.createTempDir
+    val fs = FileSystem.get(new Configuration)
+    val sourcePath = fs.makeQualified(new Path(jarOnHdfs))
+    if (!fs.exists(sourcePath)) throw new IOException("jar file: " + jarOnHdfs + " doesn't exist.")
+    val destPath = new Path(tmpDir.getAbsolutePath + "/" + sourcePath.getName)
+    fs.copyToLocalFile(sourcePath, destPath)
+    new File(destPath.toString).getAbsolutePath
+  }
+}
