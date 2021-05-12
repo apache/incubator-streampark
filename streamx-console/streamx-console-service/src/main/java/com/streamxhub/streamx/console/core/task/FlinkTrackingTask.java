b33a847202361e788a4914875fad69b6c5b29597
benjobs <benjobs@qq.com>
2021-05-12 12:20:51 +0800

    flink app state bug fixed

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/core/task/FlinkTrackingTask.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/core/task/FlinkTrackingTask.java
@@ -479,7 +479,7 @@ public class FlinkTrackingTask {
                 throw new RuntimeException("flinkTrackingTask getFromYarnRestApi failed ");
             } else {
                 try {
-                    String state = appInfo.getApp().getFinalStatus();
+                    String state = appInfo.getApp().getState();
                     FlinkAppState flinkAppState = FlinkAppState.of(state);
                     if (FlinkAppState.OTHER.equals(flinkAppState)) {
                         return;
