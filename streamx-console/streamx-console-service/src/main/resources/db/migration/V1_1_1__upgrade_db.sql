81ae37fbecb41c166a61835bb090551b7b72f3cc
benjobs <benjobs@qq.com>
2021-07-08 13:28:56 +0800

    [bugfix] sent email bug fixed.

---
+++ streamx-console/streamx-console-service/src/main/resources/db/migration/V1_1_1__upgrade_db.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_setting
+-- ----------------------------
+update `t_setting` set `id` = `id` + 1 where `id` > 6;
+
+update `t_setting` set `KEY` = 'alert.email.sender', DESCRIPTION='用来发送告警邮箱的认证用户名' where `KEY` = 'alert.email.address';
+
+update `t_setting` set `DESCRIPTION`= '用来发送告警邮箱的认证密码' where `KEY` = 'alert.email.password';
+
+INSERT INTO `t_setting` VALUES (7, 'alert.email.from', NULL, 'Alert  Email From', '告警邮箱发送人', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
