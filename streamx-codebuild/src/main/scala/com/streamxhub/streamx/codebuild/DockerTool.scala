0f65044344a792f6c57cdf289498b8574a9f843d
Al-assad <yulin.ying@outlook.com>
2021-08-03 14:06:25 +0800

    add default image namespace const

--- streamx-codebuild/src/main/scala/com/streamxhub/streamx/codebuild/DockerTool.scala
+++ streamx-codebuild/src/main/scala/com/streamxhub/streamx/codebuild/DockerTool.scala
@@ -111,12 +111,12 @@ object DockerTool {
    * compile image tag with namespace and remote address.
    */
   private[codebuild] def compileTag(tag: String): String = {
-    val tagName = {
+    var tagName = {
       if (tag.contains("/")) tag
       else s"$IMAGE_NAMESPACE/$tag"
     }
     if (K8S_IMAGE_REGISTER_ADDRESS.nonEmpty && !tagName.startsWith(K8S_IMAGE_REGISTER_ADDRESS)) {
-      s"$K8S_IMAGE_REGISTER_ADDRESS/$tagName"
+      tagName =  s"$K8S_IMAGE_REGISTER_ADDRESS/$tagName"
     }
   }
 
