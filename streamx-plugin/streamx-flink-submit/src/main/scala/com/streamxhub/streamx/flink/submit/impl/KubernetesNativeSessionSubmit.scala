ce5bf3abccbe02ae1ccf845a6279ae43d0ec1699
Al-assad <yulin.ying@outlook.com>
2021-10-24 03:53:29 +0800

    Modify the flink session job submission solution to be via the rest api.

--- streamx-plugin/streamx-flink-submit/src/main/scala/com/streamxhub/streamx/flink/submit/impl/KubernetesNativeSessionSubmit.scala
+++ streamx-plugin/streamx-flink-submit/src/main/scala/com/streamxhub/streamx/flink/submit/impl/KubernetesNativeSessionSubmit.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 The StreamX Project
+ * Copyright (c) 2021 The StreamX Project
  * <p>
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -23,9 +23,14 @@ package com.streamxhub.streamx.flink.submit.impl
 import com.google.common.collect.Lists
 import com.streamxhub.streamx.common.enums.ExecutionMode
 import com.streamxhub.streamx.common.util.Logger
+import com.streamxhub.streamx.flink.kubernetes.KubernetesRetriever
+import com.streamxhub.streamx.flink.kubernetes.enums.FlinkK8sExecuteMode
+import com.streamxhub.streamx.flink.kubernetes.model.ClusterKey
 import com.streamxhub.streamx.flink.packer.maven.MavenTool
+import com.streamxhub.streamx.flink.repl.shims.FlinkShimsClassLoader
 import com.streamxhub.streamx.flink.submit.`trait`.KubernetesNativeSubmitTrait
 import com.streamxhub.streamx.flink.submit.domain._
+import com.streamxhub.streamx.flink.submit.tool.FlinkSessionSubmitHelper
 import org.apache.commons.lang3.StringUtils
 import org.apache.flink.api.common.JobID
 import org.apache.flink.client.deployment.application.ApplicationConfiguration
@@ -35,6 +40,7 @@ import org.apache.flink.kubernetes.KubernetesClusterDescriptor
 import org.apache.flink.kubernetes.configuration.KubernetesConfigOptions
 import org.apache.flink.util.IOUtils
 
+import java.io.File
 import scala.collection.JavaConversions._
 import scala.util.Try
 
@@ -70,45 +76,82 @@ object KubernetesNativeSessionSubmit extends KubernetesNativeSubmitTrait with Lo
       // fatJarCached.getOrElseUpdate(flinkLibs._1, MavenTool.buildFatJar(flinkLibs._2, fatJarPath))
     }
     logInfo(s"[flink-submit] already built flink job fat-jar. " +
-      s"${flinkConfIdentifierInfo(flinkConfig)}, jobId=${jobID.toString}, fatJarPath=${fatJar.getAbsolutePath}")
+      s"${flinkConfIdentifierInfo(flinkConfig)}, fatJarPath=${fatJar.getAbsolutePath}")
 
-    // retrieve k8s cluster and submit flink job on session mode
-    var clusterDescriptor: KubernetesClusterDescriptor = null
-    var packageProgram: PackagedProgram = null
-    var client: ClusterClient[String] = null
+    // rest api submit plan
+    restApiSubmitPlan(submitRequest, flinkConfig, fatJar)
+
+    // old submit plan
+    // jobGraphSubmitPlan(submitRequest, flinkConfig, jobID, fatJar)
+  }
+
+  /**
+   * Submit flink session job via rest api.
+   */
+  // noinspection DuplicatedCode
+  @throws[Exception]
+  private def restApiSubmitPlan(submitRequest: SubmitRequest, flinkConfig: Configuration, fatJar: File): SubmitResponse = {
     try {
-      clusterDescriptor = getK8sClusterDescriptor(flinkConfig)
-      // build JobGraph
-      packageProgram = PackagedProgram.newBuilder()
-        .setJarFile(fatJar)
-        .setConfiguration(flinkConfig)
-        .setEntryPointClassName(flinkConfig.get(ApplicationConfiguration.APPLICATION_MAIN_CLASS))
-        .setArguments(flinkConfig.getOptional(ApplicationConfiguration.APPLICATION_ARGS)
-          .orElse(Lists.newArrayList())
-          : _*
-        ).build()
-      val jobGraph = PackagedProgramUtils.createJobGraph(
-        packageProgram,
-        flinkConfig,
-        flinkConfig.getInteger(CoreOptions.DEFAULT_PARALLELISM),
-        jobID,
-        false)
-
-      // retrieve client and submit JobGraph
-      client = clusterDescriptor.retrieve(flinkConfig.getString(KubernetesConfigOptions.CLUSTER_ID)).getClusterClient
-      val submitResult = client.submitJob(jobGraph)
-      val jobId = submitResult.get().toString
-      val result = SubmitResponse(client.getClusterId, flinkConfig, jobId)
-      logInfo(s"[flink-submit] flink job has been submitted. ${flinkConfIdentifierInfo(flinkConfig)}, jobId=${jobID.toString}")
-      result
+      // get jm rest url of flink session cluster
+      val clusterKey = ClusterKey(FlinkK8sExecuteMode.SESSION,
+        submitRequest.k8sSubmitParam.kubernetesNamespace, submitRequest.k8sSubmitParam.clusterId)
+      val jmRestUrl = KubernetesRetriever.retrieveFlinkRestUrl(clusterKey)
+        .getOrElse(throw new Exception(s"[flink-submit] retrieve flink session rest url failed, clusterKey=$clusterKey"))
+      // submit job via rest api
+      val jobId = FlinkSessionSubmitHelper.submitWithRestApi(jmRestUrl, fatJar, flinkConfig)
+      SubmitResponse(clusterKey.clusterId, flinkConfig, jobId)
     } catch {
       case e: Exception =>
         logError(s"submit flink job fail in ${submitRequest.executionMode} mode")
         e.printStackTrace()
         throw e
-    } finally {
-      IOUtils.closeAll(client, packageProgram, clusterDescriptor)
     }
+  }
+
+  /**
+   * Submit flink session job with building JobGraph via ClusterClient api.
+   */
+  // noinspection DuplicatedCode
+  @throws[Exception]
+  private def jobGraphSubmitPlan(submitRequest: SubmitRequest, flinkConfig: Configuration, jobID: JobID, fatJar: File): SubmitResponse = {
+    FlinkShimsClassLoader.runAsSpecVersion(submitRequest.replFlinkVersion, fromCache = false, () => {
+      // retrieve k8s cluster and submit flink job on session mode
+      var clusterDescriptor: KubernetesClusterDescriptor = null
+      var packageProgram: PackagedProgram = null
+      var client: ClusterClient[String] = null
+      try {
+        clusterDescriptor = getK8sClusterDescriptor(flinkConfig)
+        // build JobGraph
+        packageProgram = PackagedProgram.newBuilder()
+          .setJarFile(fatJar)
+          .setConfiguration(flinkConfig)
+          .setEntryPointClassName(flinkConfig.get(ApplicationConfiguration.APPLICATION_MAIN_CLASS))
+          .setArguments(flinkConfig.getOptional(ApplicationConfiguration.APPLICATION_ARGS)
+            .orElse(Lists.newArrayList())
+            : _*
+          ).build()
+        val jobGraph = PackagedProgramUtils.createJobGraph(
+          packageProgram,
+          flinkConfig,
+          flinkConfig.getInteger(CoreOptions.DEFAULT_PARALLELISM),
+          jobID,
+          false)
+        // retrieve client and submit JobGraph
+        client = clusterDescriptor.retrieve(flinkConfig.getString(KubernetesConfigOptions.CLUSTER_ID)).getClusterClient
+        val submitResult = client.submitJob(jobGraph)
+        val jobId = submitResult.get().toString
+        val result = SubmitResponse(client.getClusterId, flinkConfig, jobId)
+        logInfo(s"[flink-submit] flink job has been submitted. ${flinkConfIdentifierInfo(flinkConfig)}, jobId=${jobID.toString}")
+        result
+      } catch {
+        case e: Exception =>
+          logError(s"submit flink job fail in ${submitRequest.executionMode} mode")
+          e.printStackTrace()
+          throw e
+      } finally {
+        IOUtils.closeAll(client, packageProgram, clusterDescriptor)
+      }
+    })
   }
 
   override def doStop(stopInfo: StopRequest): StopResponse = {
