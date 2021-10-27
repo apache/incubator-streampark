ee8a9bca5c2ebd55e024e0e081a4d9688b998daf
benjobs <benjobs@qq.com>
2021-10-27 18:55:04 +0800

    clean up Maven dependencies,fix classLoader bugs.

--- streamx-common/src/main/scala/com/streamxhub/streamx/common/domain/FlinkVersion.scala
+++ streamx-common/src/main/scala/com/streamxhub/streamx/common/domain/FlinkVersion.scala
@@ -10,18 +10,26 @@ import java.util.regex.Pattern
  */
 case class FlinkVersion(version: String, flinkHome: String) {
 
-  private val FLINK_VER_PATTERN = Pattern.compile("^(\\d+\\.\\d+)(\\.)?.*$")
+  private[this] lazy val FLINK_VER_PATTERN = Pattern.compile("^(\\d+\\.\\d+)(\\.)?.*$")
 
   // flink major version, like "1.13", "1.14"
-  val majorVersion: String = {
+  lazy val majorVersion: String = {
     val matcher = FLINK_VER_PATTERN.matcher(version)
     matcher.matches()
     matcher.group(1)
   }
 
   // streamx flink shims version, like "streamx-flink-shims_flink-1.13"
-  val shimsVersion: String = s"streamx-flink-shims_flink-$majorVersion"
+  lazy val shimsVersion: String = s"streamx-flink-shims_flink-$majorVersion"
 
   override def toString: String =
-    s"FlinkVersion@(version=$version, flinkHome=$flinkHome, majorVersion=$majorVersion, shimsVersion=$shimsVersion)"
+    s"""
+       |-------------------------<<flinkVersion>>------------------------
+       | flinkVersion : $version
+       | flinkHome    : $flinkHome
+       | majorVersion : $majorVersion
+       | shimsVersion : $shimsVersion
+       |__________________________________________________________________
+       |""".stripMargin
+
 }
