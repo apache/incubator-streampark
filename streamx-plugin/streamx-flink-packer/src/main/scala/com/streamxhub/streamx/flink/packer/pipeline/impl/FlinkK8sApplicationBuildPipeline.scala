77ab5614b1be4fdf7ee6b183089599116c23a35a
Al-assad <yulin.ying@outlook.com>
2021-11-24 13:34:00 +0800

    build pipeline for flink k8s-application mode

---
+++ streamx-plugin/streamx-flink-packer/src/main/scala/com/streamxhub/streamx/flink/packer/pipeline/impl/FlinkK8sApplicationBuildPipeline.scala
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2021 The StreamX Project
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.streamxhub.streamx.flink.packer.pipeline.impl
+
+import com.github.dockerjava.api.command.PushImageCmd
+import com.github.dockerjava.core.command.HackBuildImageCmd
+import com.google.common.collect.Sets
+import com.streamxhub.streamx.common.conf.ConfigConst.DOCKER_IMAGE_NAMESPACE
+import com.streamxhub.streamx.common.conf.Workspace
+import com.streamxhub.streamx.common.enums.DevelopmentMode
+import com.streamxhub.streamx.common.fs.LfsOperator
+import com.streamxhub.streamx.flink.kubernetes.PodTemplateTool
+import com.streamxhub.streamx.flink.packer.docker.{FlinkDockerfileTemplate, FlinkHadoopDockerfileTemplate, usingDockerClient, watchDockerBuildStep}
+import com.streamxhub.streamx.flink.packer.maven.MavenTool
+import com.streamxhub.streamx.flink.packer.pipeline._
+
+import java.io.File
+
+/**
+ * Building pipeline for flink kubernetes-native application mode
+ *
+ * @author Al-assad
+ */
+class FlinkK8sApplicationBuildPipeline(params: FlinkK8sApplicationBuildRequest) extends BuildPipeline {
+
+  override val pipeType: PipeType = PipeType.FlinkNativeK8sApplication
+
+  override protected def offerBuildParam: FlinkK8sApplicationBuildRequest = params
+
+  @throws[Throwable]
+  override protected def buildProcess(): FlinkK8sApplicationBuildResponse = {
+
+    // Step-1: init build workspace of flink job
+    // the sub workspace dir like: APP_WORKSPACE/k8s-clusterId@k8s-namespace/
+    val buildWorkspace =
+    execStep(1) {
+      val buildWorkspace = s"${Workspace.local.APP_WORKSPACE}/${params.clusterId}@${params.k8sNamespace}"
+      LfsOperator.mkCleanDirs(buildWorkspace)
+      logInfo(s"recreate building workspace: $buildWorkspace")
+      buildWorkspace
+    }.getOrElse(throw getError.exception)
+
+    // Step-2: export k8s pod template files
+    val podTemplatePaths = params.flinkPodTemplate match {
+      case podTemplate if podTemplate.isEmpty =>
+        skipStep(2)
+        Map[String, String]()
+      case podTemplate =>
+        execStep(2) {
+          val podTemplateFiles = PodTemplateTool.preparePodTemplateFiles(buildWorkspace, podTemplate).tmplFiles
+          logInfo(s"export flink podTemplates: ${podTemplateFiles.values.mkString(",")}")
+          podTemplateFiles
+        }.getOrElse(throw getError.exception)
+    }
+
+    // Step-3: build shaded flink job jar and handle extra jars
+    // the output shaded jar file name like: streamx-flinkjob_myjob-test.jar
+    val (shadedJar, extJarLibs) =
+    execStep(3) {
+      val appName = BuildPipelineHelper.letAppNameSafe(params.appName)
+      val shadedJarOutputPath = s"$buildWorkspace/streamx-flinkjob_${appName}.jar"
+
+      val (shadedJar, extJarLibs) = params.developmentMode match {
+        case DevelopmentMode.FLINKSQL =>
+          val providedLibs = BuildPipelineHelper.extractFlinkProvidedLibs(params)
+          val (jarPackDeps, extJarLibs) = params.jarPackDeps.clearExtJarLibs
+          val shadedJar = MavenTool.buildFatJar(jarPackDeps.merge(providedLibs), shadedJarOutputPath)
+          shadedJar -> extJarLibs
+
+        case DevelopmentMode.CUSTOMCODE =>
+          val providedLibs = BuildPipelineHelper.extractFlinkProvidedLibs(params)
+          val shadedJar = MavenTool.buildFatJar(params.jarPackDeps.merge(providedLibs), shadedJarOutputPath)
+          shadedJar -> Set[String]()
+      }
+      logInfo(s"output shaded flink job jar: ${shadedJar.getAbsolutePath}")
+      shadedJar -> extJarLibs
+    }.getOrElse(throw getError.exception)
+
+    // Step-4: generate and Export flink image dockerfiles
+    val (dockerfile, dockerFileTemplate) =
+      execStep(4) {
+        val dockerFileTemplate = {
+          if (params.integrateWithHadoop)
+            FlinkHadoopDockerfileTemplate.fromSystemHadoopConf(
+              buildWorkspace,
+              params.flinkBaseImage,
+              shadedJar.getAbsolutePath,
+              extJarLibs)
+          else
+            FlinkDockerfileTemplate(
+              buildWorkspace,
+              params.flinkBaseImage,
+              shadedJar.getAbsolutePath,
+              extJarLibs)
+        }
+        val dockerFile = dockerFileTemplate.writeDockerfile
+        logInfo(s"output flink dockerfile: ${dockerFile.getAbsolutePath}, content: \n${dockerFileTemplate.offerDockerfileContent}")
+        dockerFile -> dockerFileTemplate
+      }.getOrElse(throw getError.exception)
+
+    val authConf = params.dockerAuthConfig
+    val baseImageTag = params.flinkBaseImage
+    val pushImageTag = {
+      val expectedImageTag = s"streamxflinkjob-${params.k8sNamespace}-${params.clusterId}"
+      compileTag(expectedImageTag, authConf.registerAddress)
+    }
+
+    // Step-5: pull flink base image
+    execStep(5) {
+      usingDockerClient {
+        dockerClient =>
+          val pullImageCmd = {
+            // when the register address prefix is explicitly identified on base image tag,
+            // the user's pre-saved docker register auth info would be used.
+            if (!baseImageTag.startsWith(authConf.registerAddress))
+              dockerClient.pullImageCmd(baseImageTag)
+            else
+              dockerClient.pullImageCmd(baseImageTag).withAuthConfig(authConf.toDockerAuthConf)
+          }
+          // todo watch pull progress
+          pullImageCmd.start.awaitCompletion()
+          logInfo(s"already pulled docker image from remote register, imageTag=$baseImageTag")
+      }(err => throw new Exception(s"pull docker image failed, imageTag=$baseImageTag", err))
+    }.getOrElse(throw getError.exception)
+
+    // Step-6: build flink image
+    execStep(6) {
+      usingDockerClient {
+        dockerClient =>
+          val buildImageCmd = dockerClient.buildImageCmd()
+            .withBaseDirectory(new File(buildWorkspace))
+            .withDockerfile(dockerfile)
+            .withTags(Sets.newHashSet(pushImageTag))
+          // todo watch build progress
+          val buildCmdCallback = buildImageCmd.asInstanceOf[HackBuildImageCmd]
+            .start(watchDockerBuildStep(buildStep =>
+              logInfo(s"building docker image ${pushImageTag} => ${buildStep}" )
+            ))
+          val imageId = buildCmdCallback.awaitImageId
+          logInfo(s"built docker image, imageId=$imageId, imageTag=$pushImageTag")
+      }(err => throw new Exception(s"build docker image failed. tag=${pushImageTag}", err))
+    }.getOrElse(throw getError.exception)
+
+    // Step-7: push flink image
+    execStep(7) {
+      usingDockerClient {
+        dockerClient =>
+          val pushCmd: PushImageCmd = dockerClient.pushImageCmd(pushImageTag).withAuthConfig(authConf.toDockerAuthConf)
+          // todo watch push progress
+          pushCmd.start.awaitCompletion
+          logInfo(s"already pushed docker image, imageTag=$pushImageTag")
+      }(err => throw new Exception(s"push docker image failed. tag=${pushImageTag}", err))
+    }.getOrElse(throw getError.exception)
+
+    FlinkK8sApplicationBuildResponse(buildWorkspace, pushImageTag, podTemplatePaths, dockerFileTemplate.innerMainJarPath)
+  }
+
+
+  /**
+   * compile image tag with namespace and remote address.
+   */
+  private[this] def compileTag(tag: String, registerAddress: String): String = {
+    var tagName = if (tag.contains("/")) tag else s"$DOCKER_IMAGE_NAMESPACE/$tag"
+    if (registerAddress.nonEmpty && !tagName.startsWith(registerAddress))
+      tagName = s"$registerAddress/$tagName"
+    tagName.toLowerCase
+  }
+
+}
