843da1599fd40d65d6017ce2b73a743a1e395859
Al-assad <yulin.ying@outlook.com>
2021-12-15 11:32:49 +0800

    format sql

--- streamx-console/streamx-console-service/src/main/resources/db/migration/V1_2_1__upgrate_app.sql
+++ streamx-console/streamx-console-service/src/main/resources/db/migration/V1_2_1__upgrate_app.sql
@@ -8,15 +8,16 @@ ALTER TABLE `t_flink_app` ADD COLUMN `K8S_HADOOP_INTEGRATION` tinyint(1) default
 DROP TABLE IF EXISTS `t_app_build_pipe`;
 CREATE TABLE `t_app_build_pipe`
 (
-    `APP_ID`       BIGINT PRIMARY KEY,
-    `PIPE_TYPE`    TINYINT,
-    `PIPE_STATUS`  TINYINT,
-    `CUR_STEP`     SMALLINT,
-    `TOTAL_STEP`   SMALLINT,
-    `STEPS_STATUS` TEXT,
-    `ERROR`        TEXT,
-    `BUILD_RESULT` TEXT,
-    `UPDATE_TIME`  DATETIME
+    `APP_ID`          BIGINT PRIMARY KEY,
+    `PIPE_TYPE`       TINYINT,
+    `PIPE_STATUS`     TINYINT,
+    `CUR_STEP`        SMALLINT,
+    `TOTAL_STEP`      SMALLINT,
+    `STEPS_STATUS`    TEXT,
+    `STEPS_STATUS_TS` TEXT,
+    `ERROR`           TEXT,
+    `BUILD_RESULT`    TEXT,
+    `UPDATE_TIME`     DATETIME
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
