5f321c2c3461057600e7349905526f3bdabcb175
阿洋 <xinzhuxiansheng@126.com>
2022-01-19 00:39:50 +0800

    Set t_role_menu initialization data (#647)

--- streamx-console/streamx-console-service/src/assembly/script/upgrade/v1.2.1.sql
+++ streamx-console/streamx-console-service/src/assembly/script/upgrade/v1.2.1.sql
@@ -28,4 +28,27 @@ CREATE TABLE `t_app_build_pipe`
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
 
+BEGIN;
+delete from `t_role_menu` where id > 36 AND id < 55;
+
+INSERT INTO `t_role_menu` VALUES (37, 2, 14);
+INSERT INTO `t_role_menu` VALUES (38, 2, 16);
+INSERT INTO `t_role_menu` VALUES (39, 2, 17);
+INSERT INTO `t_role_menu` VALUES (40, 2, 18);
+INSERT INTO `t_role_menu` VALUES (41, 2, 19);
+INSERT INTO `t_role_menu` VALUES (42, 2, 20);
+INSERT INTO `t_role_menu` VALUES (43, 2, 21);
+INSERT INTO `t_role_menu` VALUES (44, 2, 22);
+INSERT INTO `t_role_menu` VALUES (45, 2, 25);
+INSERT INTO `t_role_menu` VALUES (46, 2, 26);
+INSERT INTO `t_role_menu` VALUES (47, 2, 27);
+INSERT INTO `t_role_menu` VALUES (48, 2, 28);
+INSERT INTO `t_role_menu` VALUES (49, 2, 29);
+INSERT INTO `t_role_menu` VALUES (50, 2, 30);
+INSERT INTO `t_role_menu` VALUES (51, 2, 31);
+INSERT INTO `t_role_menu` VALUES (52, 2, 32);
+INSERT INTO `t_role_menu` VALUES (53, 2, 33);
+INSERT INTO `t_role_menu` VALUES (54, 2, 34);
+COMMIT;
+
 SET FOREIGN_KEY_CHECKS = 1;
