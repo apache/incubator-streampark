a0da284091a1214fb95af67e455a7513283b177e
benjobs <benjobs@qq.com>
2022-02-16 00:15:40 +0800

    [improve] job submit optimization

--- streamx-console/streamx-console-webapp/src/views/flink/app/EditFlink.vue
+++ streamx-console/streamx-console-webapp/src/views/flink/app/EditFlink.vue
@@ -699,10 +699,10 @@ export default {
         {name: 'NodePort', order: 2}
       ],
       executionModes: [
-        {mode: 'standalone', value: 1, disabled: false},
         {mode: 'yarn application', value: 4, disabled: false},
         {mode: 'kubernetes session', value: 5, disabled: false},
         {mode: 'kubernetes application', value: 6, disabled: false},
+        {mode: 'standalone', value: 1, disabled: false},
         {mode: 'local (coming soon)', value: 0, disabled: true},
         {mode: 'yarn session (coming soon)', value: 3, disabled: true},
         {mode: 'yarn per-job (deprecated, please use yarn-application mode)', value: 2, disabled: true}
