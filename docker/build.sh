a7f6b8acafe8f8e0814ee3526baacc664058c53a
Whojohn <18689235591@163.com>
2022-02-23 14:52:37 +0800

    [fix] Fix docker build way.

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,14 @@
 # Any Error will stop build.
 set -e
 if [ ! $1 ]; then
-  echo "Please input Build version"
+  echo "Please input Build version."
   exit 0
 else
   version=$1
   v_num=`echo $version | sed -r "s/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/g"`
   echo $v_num
 fi
-echo "Build version is: $1";
+echo "Build version is: $1 .";
 
 # mysql
 wget  https://raw.githubusercontent.com/streamxhub/streamx/$version/streamx-console/streamx-console-service/src/assembly/script/final/v$v_num.sql
@@ -19,7 +19,6 @@ mv -f streamx_init.sql ./mysql/
 docker build -t streamx/mysql:$v_num mysql
 wget -O jdk-8.tar.gz --no-check-certificate https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_openj9_linuxXL_8u282b08_openj9-0.24.0.tar.gz
 mv -f jdk-8.tar.gz ./streamx/
-wget  --no-check-certificate  https://github.com/streamxhub/streamx/releases/download/$1/streamx-console-service-${v_num}-bin.tar.gz
-mv -f streamx-console-service-$v_num-bin.tar.gz ./streamx/
-docker build --build-arg streamx_version=$v_num -t streamx/streamx-console-service:$v_num streamx
-
+wget  --no-check-certificate -O streamx.gz   https://github.com/streamxhub/streamx/releases/download/$1/streamx-console-service-${v_num}-release.gz
+mv -f streamx.gz ./streamx/
+docker build --build-arg streamx_version=$v_num -t streamx/streamx-console-service:$v_num  streamx
--- docker/streamx/start.sh
+++ docker/streamx/start.sh
@@ -2,19 +2,20 @@
 set -e
 WORK_SPACE="/opt/streamx/bin"
 if [ ! -d $WORK_SPACE ]; then
- tar xf /tmp/streamx-console-service-${STREAMX_VERSION}-bin.tar.gz -C /opt  \
+ tar xf /tmp/streamx.gz -C /opt  \
  && cp -R /opt/streamx-console-service-${STREAMX_VERSION}/* /opt/streamx     \
  && mkdir /opt/jdk  \
  && tar xf /tmp/jdk-8.tar.gz -C /opt/jdk --strip-components 1  \
  && sed -i 's/localhost:3306/streamx-mysql-docker:3306/g' /opt/streamx/conf/application.yml \
  && rm -rf /tmp/* \
  && rm -rf /opt/streamx-console-service-${STREAMX_VERSION} \
- && echo "Ini project success"
+ && echo "Initialize the project successfully."
  /bin/bash /opt/streamx/bin/startup.sh
  tail -f /opt/streamx/logs/*
 else
-    #sleep 5
-    echo "check ok"
+    # Prevent mysql not ready.
+    sleep 5
+    echo "Starting streamx."
     /bin/bash /opt/streamx/bin/startup.sh
     tail -f /opt/streamx/logs/*
 fi
