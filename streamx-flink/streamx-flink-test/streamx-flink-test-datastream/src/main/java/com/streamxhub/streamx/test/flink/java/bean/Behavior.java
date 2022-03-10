9ae6d531be387af7ecc25bb3a9598885a72f44c9
benjobs <benjobs@qq.com>
2022-03-11 00:04:46 +0800

    [hotfix] kafka java sink testCase bug fixed. (#803)

---
+++ streamx-flink/streamx-flink-test/streamx-flink-test-datastream/src/main/java/com/streamxhub/streamx/test/flink/java/bean/Behavior.java
@@ -0,0 +1,9 @@
+package com.streamxhub.streamx.test.flink.java.bean;
+
+public class Behavior {
+    private String user_id;
+    private Long item_id;
+    private Long category_id;
+    private String behavior;
+    private Long ts;
+}
