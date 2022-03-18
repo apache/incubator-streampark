985fc0617fe7f8528261e613967dd8c2b5f46c02
benjobs <benjobs@qq.com>
2022-03-18 08:25:59 +0800

    [hotfix] jackson bug fixed.

--- streamx-common/src/main/scala/com/streamxhub/streamx/common/util/JsonUtils.scala
+++ streamx-common/src/main/scala/com/streamxhub/streamx/common/util/JsonUtils.scala
@@ -18,11 +18,14 @@
  */
 package com.streamxhub.streamx.common.util
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
+import com.fasterxml.jackson.annotation.{JsonInclude, JsonProperty}
+import com.fasterxml.jackson.databind.cfg.MapperConfig
+import com.fasterxml.jackson.databind.introspect.AnnotatedField
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, PropertyNamingStrategy, SerializationFeature}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 import java.text.SimpleDateFormat
+import java.util.regex.Pattern
 import scala.reflect.ClassTag
 
 object JsonUtils extends Serializable {
@@ -36,6 +39,7 @@ object JsonUtils extends Serializable {
 
   //该属性设置主要是将忽略空bean转json错误
   mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+
   //该属性设置主要是取消将对象的时间默认转换timesstamps(时间戳)形式
   mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
@@ -43,6 +47,32 @@ object JsonUtils extends Serializable {
 
   //所有日期都统一为以下样式：yyyy-MM-dd HH:mm:ss，这里可以不用我的DateTimeUtil.DATE_FORMAT，手动添加
   mapper.setDateFormat(new SimpleDateFormat(DateUtils.fullFormat))
+
+  mapper.setPropertyNamingStrategy(new PropertyNamingStrategy() {
+    override def nameForField(config: MapperConfig[_], field: AnnotatedField, defaultName: String): String = {
+      val jsonProperty = field.getAnnotation(classOf[JsonProperty])
+      if (jsonProperty != null) {
+        jsonProperty.value()
+      } else {
+        toCamelField(defaultName)
+      }
+    }
+
+    def toCamelField(defaultName: String): String = {
+      val sb = new StringBuilder(defaultName)
+      val matcher = Pattern.compile("[_\\-]").matcher(defaultName)
+      var i = 0
+      while (matcher.find) {
+        val position = matcher.end - {
+          i += 1
+          i - 1
+        }
+        sb.replace(position - 1, position + 1, sb.substring(position, position + 1).toUpperCase)
+      }
+      sb.toString
+    }
+
+  })
 
   private[streamx] def read[T](obj: AnyRef, clazz: Class[T]): T = {
     obj match {
@@ -62,5 +92,6 @@ object JsonUtils extends Serializable {
   implicit class Marshal(obj: AnyRef) {
     private[streamx] def toJson: String = write(obj)
   }
+
 
 }
