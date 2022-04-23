c1807f5c4b21a87702c89ccca81b4099b280f511
benjobs <benjobs@qq.com>
2022-04-23 09:40:47 +0800

    [Improve] curlBuilder structure adjustment

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/system/controller/AccessTokenController.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/system/controller/AccessTokenController.java
@@ -109,6 +109,7 @@ public class AccessTokenController {
                 .addFormData("savePoint", "")
                 .build();
         }
+
         return RestResponse.success(resultCURL);
     }
 
