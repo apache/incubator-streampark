060aec1327a33a464f8b5d9531a18282b0dfd95a
benjobs <benjobs@qq.com>
2022-04-24 19:03:09 +0800

    [Feature] flink 1.15 support.

--- streamx-flink/streamx-flink-shims/streamx-flink-shims_flink-1.15/src/main/scala/com/streamxhub/streamx/flink/core/FlinkSqlValidator.scala
+++ streamx-flink/streamx-flink-shims/streamx-flink-shims_flink-1.15/src/main/scala/com/streamxhub/streamx/flink/core/FlinkSqlValidator.scala
@@ -8,7 +8,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * https://www.apache.org/licenses/LICENSE-2.0
+ *    https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.streamxhub.streamx.flink.core
 
 import com.streamxhub.streamx.common.enums.SqlErrorType
