8f8dd9f02ba6c25fbf97e3fa275155adb8993c9d
wangqingrong <1109582178@qq.com>
2022-06-23 15:13:10 +0800

    Enhanced doris streamx connector

---
+++ streamx-flink/streamx-flink-connector/streamx-flink-connector-doris/src/main/java/com/streamxhub/streamx/flink/connector/doris/bean/DorisSinkRowDataWithMeta.java
@@ -0,0 +1,47 @@
+package com.streamxhub.streamx.flink.connector.doris.bean;
+
+import com.streamxhub.streamx.common.conf.ConfigConst;
+import com.streamxhub.streamx.flink.connector.doris.sink.DorisJavaSink;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DorisSinkRowDataWithMeta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String table;
+    private String database;
+    private List<String> dataRows = new ArrayList<>();
+
+
+    public DorisSinkRowDataWithMeta database(String database) {
+        this.database = database;
+        return this;
+    }
+
+    public DorisSinkRowDataWithMeta table(String table) {
+        this.table = table;
+        return this;
+    }
+
+
+    public String[] getDataRows() {
+        return dataRows.toArray(new String[]{});
+    }
+
+    public void addDataRow(String dataRow) {
+        this.dataRows.add(dataRow);
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+
+}
--- streamx-flink/streamx-flink-connector/streamx-flink-connector-doris/src/main/java/com/streamxhub/streamx/flink/connector/doris/sink/DorisJavaSink.java
+++ streamx-flink/streamx-flink-connector/streamx-flink-connector-doris/src/main/java/com/streamxhub/streamx/flink/connector/doris/sink/DorisJavaSink.java
@@ -19,10 +19,8 @@
 
 package com.streamxhub.streamx.flink.connector.doris.sink;
 
-import com.streamxhub.streamx.common.conf.ConfigConst;
 import com.streamxhub.streamx.flink.connector.doris.internal.DorisSinkFunction;
 import com.streamxhub.streamx.flink.core.scala.StreamingContext;
-
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 
@@ -42,58 +40,13 @@ public class DorisJavaSink<T> {
         this.context = context;
     }
 
-    public DorisJavaSink<T> fenodes(String fenodes) {
-        properties.put(ConfigConst.DORIS_FENODES(), fenodes);
-        return this;
-    }
-
-    public DorisJavaSink<T> database(String database) {
-        properties.put(ConfigConst.DORIS_DATABASE(), database);
-        return this;
-    }
-
-    public DorisJavaSink<T> table(String table) {
-        properties.put(ConfigConst.DORIS_TABLE(), table);
-        return this;
-    }
-
-    public DorisJavaSink<T> user(String user) {
-        properties.put(ConfigConst.DORIS_USER(), user);
-        return this;
-    }
-
-    public DorisJavaSink<T> password(String password) {
-        properties.put(ConfigConst.DORIS_PASSWORD(), password);
-        return this;
-    }
-
-    public DorisJavaSink<T> batchSize(Integer batchSize) {
-        properties.put(ConfigConst.DORIS_BATCHSIZE(), batchSize);
-        return this;
-    }
-
-    public DorisJavaSink<T> intervalMs(Long intervalMs) {
-        properties.put(ConfigConst.DORIS_INTERVALMS(), intervalMs);
-        return this;
-    }
-
-    public DorisJavaSink<T> maxRetries(Integer maxRetries) {
-        properties.put(ConfigConst.DORIS_MAXRETRIES(), maxRetries);
-        return this;
-    }
-
-    public DorisJavaSink<T> streamLoadProp(Properties streamLoadProp) {
-        this.properties = streamLoadProp;
-        return this;
-    }
-
     /**
      * java stream
      * @param source
      * @return
      */
     public DataStreamSink<T> sink(DataStream<T> source) {
-        DorisSinkFunction<T> sinkFunction = new DorisSinkFunction<>(context, properties, alias);
+        DorisSinkFunction<T> sinkFunction = new DorisSinkFunction<>(context);
         return source.addSink(sinkFunction);
     }
 
@@ -103,7 +56,7 @@ public class DorisJavaSink<T> {
      * @return
      */
     public DataStreamSink<T> sink(org.apache.flink.streaming.api.scala.DataStream<T> source) {
-        DorisSinkFunction<T> sinkFunction = new DorisSinkFunction<>(context, properties, alias);
+        DorisSinkFunction<T> sinkFunction = new DorisSinkFunction<>(context);
         return source.addSink(sinkFunction);
     }
 
