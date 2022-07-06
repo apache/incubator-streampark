0d4021b23d7b12bb3f71cc7b1923e9add57c912b
Monster <60029759+MonsterChenzhuo@users.noreply.github.com>
2022-07-06 23:52:37 +0800

    add ldap login (#1181)
    * add ldap support.

--- streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/system/security/impl/AbstractAuthenticator.java
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/system/security/impl/AbstractAuthenticator.java
@@ -17,37 +17,28 @@
  * limitations under the License.
  */
 
-package com.streamxhub.streamx.console.system.controller;
+package com.streamxhub.streamx.console.system.security.impl;
 
-import com.streamxhub.streamx.console.base.domain.RestResponse;
+import com.streamxhub.streamx.common.util.DateUtils;
 import com.streamxhub.streamx.console.base.properties.ShiroProperties;
+import com.streamxhub.streamx.console.base.util.ShaHashUtils;
+import com.streamxhub.streamx.console.base.util.WebUtils;
 import com.streamxhub.streamx.console.system.authentication.JWTToken;
+import com.streamxhub.streamx.console.system.authentication.JWTUtil;
 import com.streamxhub.streamx.console.system.entity.User;
 import com.streamxhub.streamx.console.system.security.Authenticator;
 import com.streamxhub.streamx.console.system.service.RoleService;
 import com.streamxhub.streamx.console.system.service.UserService;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.SecurityUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotBlank;
-
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * @author benjobs
- */
-@Validated
-@RestController
-@RequestMapping("passport")
-public class PassportController {
+public abstract class AbstractAuthenticator implements Authenticator {
 
     @Autowired
     private UserService userService;
@@ -58,41 +49,33 @@ public class PassportController {
     @Autowired
     private ShiroProperties properties;
 
-    @Autowired
-    private Authenticator authenticator;
-
-    @PostMapping("signin")
-    public RestResponse signin(
-        @NotBlank(message = "{required}") String username,
-        @NotBlank(message = "{required}") String password) throws Exception {
-
-        if (StringUtils.isEmpty(username)) {
-            return RestResponse.create().put("code", 0);
-        }
-
-        // verify username and password
-        Map<String, Object> userInfo = authenticator.authenticate(username, password);
-        return new RestResponse().data(userInfo);
-    }
-
-    @PostMapping("signout")
-    public RestResponse signout() {
-        SecurityUtils.getSecurityManager().logout(SecurityUtils.getSubject());
-        return new RestResponse();
-    }
-
     /**
-     * 生成前端需要的用户信息，包括： 1. token 2. Vue Router 3. 用户角色 4. 用户权限 5. 前端系统个性化配置信息
+     * user login and return user in db
      *
-     * @param token token
-     * @param user  用户信息
-     * @return UserInfo
+     * @param userId user identity field
+     * @param password user login password
+     * @return user object in databse
      */
-    private Map<String, Object> generateUserInfo(JWTToken token, User user) {
-        String username = user.getUsername();
+    public abstract User login(String userId, String password) throws Exception;
+
+    @Override
+    public Map<String, Object> authenticate(String username, String password) throws Exception {
+        User user = login(username, password);
+        String salt = user.getSalt();
+        password = ShaHashUtils.encrypt(salt, password);
+
+        // 更新用户登录时间
+        this.userService.updateLoginTime(username);
+        String token = WebUtils.encryptToken(JWTUtil.sign(username, password));
+        LocalDateTime expireTime = LocalDateTime.now().plusSeconds(properties.getJwtTimeOut());
+        String expireTimeStr = DateUtils.formatFullTime(expireTime);
+        JWTToken jwtToken = new JWTToken(token, expireTimeStr);
+        String userId = RandomStringUtils.randomAlphanumeric(20);
+        user.setId(userId);
+
         Map<String, Object> userInfo = new HashMap<>(8);
-        userInfo.put("token", token.getToken());
-        userInfo.put("expire", token.getExpireAt());
+        userInfo.put("token", jwtToken.getToken());
+        userInfo.put("expire", jwtToken.getExpireAt());
 
         Set<String> roles = this.roleService.getUserRoleName(username);
         userInfo.put("roles", roles);
