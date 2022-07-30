bca1fbd936ef12183172f2553d1f549d50286ebd
sober <soberchina0@gmail.com>
2022-07-30 16:20:19 +0800

    fix add project error and correct the wrong word (#1303)

--- streamx-console/streamx-console-webapp/src/views/flink/project/Add.vue
+++ streamx-console/streamx-console-webapp/src/views/flink/project/Add.vue
@@ -16,7 +16,8 @@
           v-decorator="['teamId',{rules: [{ required: true, message: 'please select team' }]}]">
           <a-select-option
             v-for="t in teamData"
-            :key="t.teamId">
+            :key="t.teamId"
+            :value="t.teamId">
             {{ t.teamName }}
           </a-select-option>
         </a-select>
@@ -121,7 +122,7 @@
           @click.native="handleBranches"
           v-decorator="['branches',{ rules: [{ required: true } ]}]">
           <a-select-option
-            v-for="(k ,i) in brancheList"
+            v-for="(k ,i) in branchList"
             :key="i"
             :value="k">
             {{ k }}
@@ -188,8 +189,8 @@ export default {
   name: 'BaseForm',
   data () {
     return {
-      brancheList: [],
-      searchBranche: false,
+      branchList: [],
+      searchBranch: false,
       teamData: [],
       teamId: '',
       options: {
@@ -229,6 +230,10 @@ export default {
       this.types = selected
     },
 
+    handleTeamEdit (selected) {
+      this.teamId = selected
+    },
+
     handleSchema () {
       console.log(this.url)
     },
@@ -266,10 +271,10 @@ export default {
             password: values.password || null,
           }).then((resp) => {
             if ( resp.data === 0 ) {
-              if (this.brancheList.length === 0) {
+              if (this.branchList.length === 0) {
                 this.handleBranches()
               }
-              if (this.brancheList.indexOf(values.branches) === -1) {
+              if (this.branchList.indexOf(values.branches) === -1) {
                 this.$swal.fire(
                   'Failed',
                   'branch [' + values.branches + '] does not exist<br>or authentication error,please check',
@@ -319,7 +324,7 @@ export default {
     },
 
     handleBranches() {
-      this.searchBranche = true
+      this.searchBranch = true
       const form = this.form
       const url = form.getFieldValue('url')
       if (url) {
@@ -333,10 +338,10 @@ export default {
             userName: userName ,
             password: password
           }).then((resp) => {
-            this.brancheList = resp.data
-            this.searchBranche = false
+            this.branchList = resp.data
+            this.searchBranch = false
           }).catch((error) => {
-            this.searchBranche = false
+            this.searchBranch = false
             this.$message.error(error.message)
           })
         }
