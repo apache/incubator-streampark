ba0e39777f7012be282251fa73e4766ef73340cb
VampireAchao <achao1441470436@gmail.com>
2022-08-28 16:47:07 +0800

    [Bug] fix paging failed by PaginationInnerInterceptor

---
+++ streamx-console/streamx-console-service/src/main/java/com/streamxhub/streamx/console/base/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.streamxhub.streamx.console.base.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlusConfig
+ *
+ * @author VampireAchao
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+}
