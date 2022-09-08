efdb93dd813b8f02926052c0208c712dded43c7f
wuchunfu <319355703@qq.com>
2022-09-09 00:41:08 +0800

    Add license header

--- streampark-console/streampark-console-webapp/src/config/replacer/resolve.config.js
+++ streampark-console/streampark-console-webapp/src/config/replacer/resolve.config.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * webpack-theme-color-replacer 插件的 resolve 配置<br>
  * 为特定的 css 选择器（selector）配置 resolve 规则。<br>
