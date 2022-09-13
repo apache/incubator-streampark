8d5394b266f19f272b4d6bc6fcb4d25e3dab9074
Gerry <33364356+youyangkou@users.noreply.github.com>
2022-09-13 22:32:22 +0800

    [Bug] Fixed the Alert Setting form pre-check bug (#1589)
    * [Bug] Fixed the Alert Setting form pre-check bug

--- streampark-console/streampark-console-webapp/src/views/flink/setting/View.vue
+++ streampark-console/streampark-console-webapp/src/views/flink/setting/View.vue
@@ -676,7 +676,7 @@
             type="text"
             allowClear
             placeholder="Please enter alert name"
-            v-decorator="['alertName',{ rules: [{validator: handleCheckAlertName,required: true , message: 'Alert Name is required'}]} ]"/>
+            v-decorator="['alertName',{ rules: [{ validator: handleCheckAlertName,trigger:'submit',required: true}] }]"/>
           <span
             class="conf-switch"
             style="color:darkgrey">the alert name, e.g: StreamPark team alert </span>
