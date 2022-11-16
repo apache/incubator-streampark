57f469dda97051a6599397e2d2fa5bd18308ddb5
1996fanrui <1996fanrui@gmail.com>
2022-11-16 21:32:10 +0800

    [Bug] Rename the teamId of user table to lastTeamId

--- streampark-console/streampark-console-service/src/assembly/script/schema/mysql-schema.sql
+++ streampark-console/streampark-console-service/src/assembly/script/schema/mysql-schema.sql
@@ -392,7 +392,7 @@ create table `t_user` (
   `password` varchar(128) collate utf8mb4_general_ci not null comment 'password',
   `email` varchar(128) collate utf8mb4_general_ci default null comment 'email',
   `user_type` int  not null comment 'user type 1:admin 2:user',
-  `team_id` bigint default null comment 'latest team id',
+  `last_team_id` bigint default null comment 'last team id',
   `status` char(1) collate utf8mb4_general_ci not null comment 'status 0:locked 1:active',
   `create_time` datetime not null default current_timestamp comment 'create time',
   `modify_time` datetime not null default current_timestamp on update current_timestamp comment 'modify time',
