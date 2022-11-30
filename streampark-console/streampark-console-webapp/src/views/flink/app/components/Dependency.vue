c34b33c8075a7587842d8c1818a71d8a7490c567
wsz960504 <1246458990@qq.com>
2022-11-30 20:49:10 +0800

    [Fix]: dependency upload jar bug fix

--- streampark-console/streampark-console-webapp/src/views/flink/app/components/Dependency.vue
+++ streampark-console/streampark-console-webapp/src/views/flink/app/components/Dependency.vue
@@ -53,6 +53,7 @@
   const selectedHistoryUploadJars = ref<string[]>([]);
   const dependencyRecords = ref<DependencyType[]>([]);
   const uploadJars = ref<string[]>([]);
+  const historyUploadJars = ref<string[]>([]);
   const loading = ref(false);
 
   const emit = defineEmits(['update:value']);
@@ -208,11 +209,11 @@
   async function handleReloadHistoryUploads() {
     selectedHistoryUploadJars.value = [];
     const res = await fetchUploadJars();
-    uploadJars.value = res;
+    historyUploadJars.value = res;
   }
 
   const filteredHistoryUploadJarsOptions = computed(() => {
-    return uploadJars.value.filter((o) => !Reflect.has(dependency.jar, o));
+    return historyUploadJars.value.filter((o) => !Reflect.has(dependency.jar, o));
   });
   function handleRemoveJar(jar: string) {
     delete dependency.jar[jar];
@@ -241,6 +242,14 @@
     dataSource.jar?.map((fileName: string) => {
       dependency.jar[fileName] = fileName;
     });
+  }
+  function addHistoryUploadJar(item: string) {
+    dependency.jar[item] = item;
+    handleUpdateDependency();
+  }
+  function deleteHistoryUploadJar(item: string) {
+    delete dependency.jar[item];
+    handleUpdateDependency();
   }
   onMounted(() => {
     handleReloadHistoryUploads();
@@ -275,8 +284,9 @@
           mode="multiple"
           placeholder="Search History Uploads"
           v-model:value="selectedHistoryUploadJars"
+          @select="addHistoryUploadJar"
+          @deselect="deleteHistoryUploadJar"
           style="width: 100%"
-          :showArrow="true"
         >
           <SelectOption v-for="item in filteredHistoryUploadJarsOptions" :key="item" :value="item">
             <template #suffixIcon>
