1623609dc8ec996b6943467a2e8d47fb2a075dae
benjobs <benjobs@apache.org>
2022-12-10 23:17:46 +0800

    Update useProject.tsx

--- streampark-console/streampark-console-webapp/src/views/flink/project/useProject.tsx
+++ streampark-console/streampark-console-webapp/src/views/flink/project/useProject.tsx
@@ -1,4 +1,4 @@
-/*./components/RepositoryGroup
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
