08a904275a5a03bd153d6d9fb550542104ee80df
benjobs <benjobs@apache.org>
2023-01-08 09:52:50 +0800

    [Improve] EncryptUtils DES algorithm change to AES

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/base/util/EncryptUtils.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/base/util/EncryptUtils.java
@@ -17,86 +17,49 @@
 
 package org.apache.streampark.console.base.util;
 
-import javax.crypto.Cipher;
+import org.apache.commons.codec.digest.DigestUtils;
 
-import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class EncryptUtils {
 
-  private static final String DEFAULT_KEY = "defaultKey";
-  private Cipher encryptCipher = null;
-  private Cipher decryptCipher = null;
+  private static final String DEFAULT_KEY = DigestUtils.md5Hex("ApacheStreamPark");
 
-  private static String byteArr2HexStr(byte[] arrB) {
-    int iLen = arrB.length;
-    StringBuilder sb = new StringBuilder(iLen * 2);
-    for (byte anArrB : arrB) {
-      int intTmp = anArrB;
-      while (intTmp < 0) {
-        intTmp = intTmp + 256;
-      }
-      if (intTmp < 16) {
-        sb.append("0");
-      }
-      sb.append(Integer.toString(intTmp, 16));
-    }
-    return sb.toString();
+  private static final int offset = 16;
+
+  private static final String ALGORITHM = "AES";
+
+  private static final String CIPHER_KEY = "AES/CBC/PKCS5Padding";
+
+  public static String encrypt(String content) throws Exception {
+    return encrypt(content, DEFAULT_KEY);
   }
 
-  private static byte[] hexStr2ByteArr(String strIn) {
-    byte[] arrB = strIn.getBytes();
-    int iLen = arrB.length;
-
-    byte[] arrOut = new byte[iLen / 2];
-    for (int i = 0; i < iLen; i = i + 2) {
-      String strTmp = new String(arrB, i, 2);
-      arrOut[i / 2] = (byte) Integer.parseInt(strTmp, 16);
-    }
-    return arrOut;
+  public static String decrypt(String content) throws Exception {
+    return decrypt(content, DEFAULT_KEY);
   }
 
-  public EncryptUtils() throws Exception {
-    this(DEFAULT_KEY);
+  public static String encrypt(String content, String key) throws Exception {
+    SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
+    IvParameterSpec ivSpec = new IvParameterSpec(key.getBytes(), 0, offset);
+    Cipher cipher = Cipher.getInstance(CIPHER_KEY);
+    byte[] byteContent = content.getBytes(StandardCharsets.UTF_8);
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+    byte[] result = cipher.doFinal(byteContent);
+    return Base64.getEncoder().encodeToString(result);
   }
 
-  EncryptUtils(String strKey) throws Exception {
-    /*
-     * Security.addProvider(new com.sun.crypto.provider.SunJCE());
-     */
-    Key key = getKey(strKey.getBytes());
-
-    encryptCipher = Cipher.getInstance("DES");
-    encryptCipher.init(Cipher.ENCRYPT_MODE, key);
-
-    decryptCipher = Cipher.getInstance("DES");
-    decryptCipher.init(Cipher.DECRYPT_MODE, key);
-  }
-
-  private byte[] encrypt(byte[] arrB) throws Exception {
-    return encryptCipher.doFinal(arrB);
-  }
-
-  String encrypt(String strIn) throws Exception {
-    return byteArr2HexStr(encrypt(strIn.getBytes()));
-  }
-
-  private byte[] decrypt(byte[] arrB) throws Exception {
-    return decryptCipher.doFinal(arrB);
-  }
-
-  String decrypt(String strIn) {
-    try {
-      return new String(decrypt(hexStr2ByteArr(strIn)));
-    } catch (Exception e) {
-      return "";
-    }
-  }
-
-  private Key getKey(byte[] arrBTmp) {
-    byte[] arrB = new byte[8];
-    for (int i = 0; i < arrBTmp.length && i < arrB.length; i++) {
-      arrB[i] = arrBTmp[i];
-    }
-    return new javax.crypto.spec.SecretKeySpec(arrB, "DES");
+  public static String decrypt(String content, String key) throws Exception {
+    SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
+    IvParameterSpec ivSpec = new IvParameterSpec(key.getBytes(), 0, offset);
+    Cipher cipher = Cipher.getInstance(CIPHER_KEY);
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+    byte[] result = cipher.doFinal(Base64.getDecoder().decode(content));
+    return new String(result);
   }
 }
