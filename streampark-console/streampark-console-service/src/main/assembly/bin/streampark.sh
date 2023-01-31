7922cfceddc6a41d13c8e072064694635b83611d
zhangxiangyang <1013161894@qq.com>
2023-01-31 10:55:06 +0800

    Merge Conflict

--- streampark-console/streampark-console-service/src/main/assembly/bin/streampark.sh
+++ streampark-console/streampark-console-service/src/main/assembly/bin/streampark.sh
@@ -68,6 +68,20 @@ else
   RESET=""
 fi
 
+JAVA_OPTS="""
+  $vmOption
+  -ea
+  -server
+  -Xms1024m
+  -Xmx1024m
+  -Xmn256m
+  -XX:NewSize=100m
+  -XX:+UseConcMarkSweepGC
+  -XX:CMSInitiatingOccupancyFraction=70
+  -XX:ThreadStackSize=512
+  -Xloggc:${APP_HOME}/logs/gc.log
+  """
+
 echo_r () {
     # Color red: Error, Failed
     [[ $# -ne 1 ]] && return 1
@@ -345,20 +359,6 @@ start() {
   # shellcheck disable=SC2006
   vmOption=`$_RUNJAVA -cp "$APP_CLASSPATH" $PARAM_CLI --vmopt`
 
-  JAVA_OPTS="""
-  $vmOption
-  -ea
-  -server
-  -Xms1024m
-  -Xmx1024m
-  -Xmn256m
-  -XX:NewSize=100m
-  -XX:+UseConcMarkSweepGC
-  -XX:CMSInitiatingOccupancyFraction=70
-  -XX:ThreadStackSize=512
-  -Xloggc:${APP_HOME}/logs/gc.log
-  """
-
   eval $NOHUP "\"$_RUNJAVA\"" $JAVA_OPTS \
     -classpath "\"$APP_CLASSPATH\"" \
     -Dapp.home="\"${APP_HOME}\"" \
@@ -394,6 +394,14 @@ start() {
    else
       echo_r "StreamPark start failed."
    fi
+}
+
+debug() {
+  JAVA_OPTS="""
+  $JAVA_OPTS
+  -Xdebug  -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=10002
+  """
+  start
 }
 
 # shellcheck disable=SC2120
@@ -546,6 +554,9 @@ restart() {
 main() {
   print_logo
   case "$1" in
+    "debug")
+        debug
+        ;;
     "start")
         start
         ;;
@@ -565,6 +576,7 @@ main() {
         echo_w "  start \$conf               Start StreamPark with application config."
         echo_w "  stop n -force             Stop StreamPark, wait up to n seconds and then use kill -KILL if still running"
         echo_w "  status                    StreamPark status"
+        echo_w "  debug                     StreamPark start with debug mode,remote debugger port: 10002"
         echo_w "  restart \$conf             restart StreamPark with application config."
         exit 0
         ;;
