bc0bab8b0dace38c1aab608b6daf5b9f72ec1050
hunkw <131759638+hunkw123456@users.noreply.github.com>
2023-04-25 23:19:06 +0800

    Update ApplicationServiceImpl.java

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/impl/ApplicationServiceImpl.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/impl/ApplicationServiceImpl.java
@@ -1053,7 +1053,7 @@ public class ApplicationServiceImpl extends ServiceImpl<ApplicationMapper, Appli
           application.getK8sNamespace(), application.getJobName());
       KubernetesDeploymentHelper.deleteTaskConfigMap(
           application.getK8sNamespace(), application.getJobName());
-      IngressController.deleteIngress(application.getK8sNamespace(), application.getJobName());
+      IngressController.deleteIngress(application.getJobName(), application.getK8sNamespace());
     }
     if (startFuture != null) {
       startFuture.cancel(true);
@@ -1294,7 +1294,7 @@ public class ApplicationServiceImpl extends ServiceImpl<ApplicationMapper, Appli
             (t, e) -> {
               if (isKubernetesApp(application)) {
                 IngressController.deleteIngress(
-                    application.getK8sNamespace(), application.getJobName());
+                    application.getJobName(), application.getK8sNamespace());
               }
               cancelFutureMap.remove(application.getId());
             });
