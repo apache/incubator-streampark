b26b36ff88d1d5e3ca69deb220dbb32d867f636a
benjobs <benjobs@apache.org>
2023-05-01 16:46:59 +0800

    update version to 2.1.0 (#2716)
    * update version to 2.1.0
    
    * [Improve] FE improvement
    
    * [improve] web.cancel disable
    
    * [Improve] web.cancel disable

--- streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/trait/FlinkClientTrait.scala
+++ streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/trait/FlinkClientTrait.scala
@@ -40,6 +40,7 @@ import org.apache.flink.util.FlinkException
 import org.apache.flink.util.Preconditions.checkNotNull
 
 import java.io.File
+import java.lang.{Boolean => JavaBool}
 import java.util.{Collections, List => JavaList, Map => JavaMap}
 
 import scala.annotation.tailrec
@@ -98,6 +99,7 @@ trait FlinkClientTrait extends Logger {
       .safeSet(ApplicationConfiguration.APPLICATION_MAIN_CLASS, submitRequest.appMain)
       .safeSet(ApplicationConfiguration.APPLICATION_ARGS, extractProgramArgs(submitRequest))
       .safeSet(PipelineOptionsInternal.PIPELINE_FIXED_JOB_ID, submitRequest.jobId)
+      .safeSet(WebOptions.CANCEL_ENABLE, JavaBool.FALSE)
 
     if (
       !submitRequest.properties.containsKey(CheckpointingOptions.MAX_RETAINED_CHECKPOINTS.key())
