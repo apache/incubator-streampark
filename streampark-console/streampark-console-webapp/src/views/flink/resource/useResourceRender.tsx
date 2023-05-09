40ff68e9ea794bc41bcb0f3b27d30296e7b3f7ec
zhoulii <zhouli16@hikvision.com.cn>
2023-05-09 17:16:46 +0800

    [FIX] alter the width of select component

--- streampark-console/streampark-console-webapp/src/views/flink/resource/useResourceRender.tsx
+++ streampark-console/streampark-console-webapp/src/views/flink/resource/useResourceRender.tsx
@@ -62,7 +62,6 @@ export const renderResourceType = ({ model },) => {
       <Select
         allow-clear
         placeholder={t('flink.resource.form.resourceTypePlaceholder')}
-        style="width: calc(100% - 60px)"
         value={model.resourceType}
         onChange={(value) => (model.resourceType = value)}
       >
