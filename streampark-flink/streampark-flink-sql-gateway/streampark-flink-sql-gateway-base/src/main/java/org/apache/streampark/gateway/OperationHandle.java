805bc718a5e1f5a2720c4c91de846d06fe90b83a
gongzhongqiang <764629910@qq.com>
2023-05-10 12:57:05 +0800

    init base sql gateway module (#2267)
    Co-authored-by: gongzhongqiang <gongzhongqiang@gigacloudtech.com>

---
+++ streampark-flink/streampark-flink-sql-gateway/streampark-flink-sql-gateway-base/src/main/java/org/apache/streampark/gateway/OperationHandle.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.streampark.gateway;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+/** {@link OperationHandle} to index the {@code Operation}. */
+public class OperationHandle implements Serializable {
+
+  private final UUID identifier;
+
+  public OperationHandle(UUID identifier) {
+    this.identifier = identifier;
+  }
+
+  public UUID getIdentifier() {
+    return identifier;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    OperationHandle that = (OperationHandle) o;
+    return Objects.equals(identifier, that.identifier);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(identifier);
+  }
+
+  @Override
+  public String toString() {
+    return "OperationHandle{" + "identifier=" + identifier + '}';
+  }
+}
