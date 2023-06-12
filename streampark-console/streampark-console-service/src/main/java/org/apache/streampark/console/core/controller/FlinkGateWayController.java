ebef192d0b0f7aa5423212c4617550c051de1213
gongzhongqiang <gongzhongqiang@gigacloudtech.com>
2023-06-12 20:31:33 +0800

    Add gateway manage frontend

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/controller/FlinkGateWayController.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/controller/FlinkGateWayController.java
@@ -26,7 +26,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
 
@@ -68,7 +76,8 @@ public class FlinkGateWayController {
 
   @Operation(summary = "Delete flink gateway by id")
   @DeleteMapping("delete")
-  public RestResponse delete(@NotNull(message = "The Gateway id cannot be null") @RequestParam("id") Long id) {
+  public RestResponse delete(
+      @NotNull(message = "The Gateway id cannot be null") @RequestParam("id") Long id) {
     flinkGatewayService.removeById(id);
     return RestResponse.success();
   }
