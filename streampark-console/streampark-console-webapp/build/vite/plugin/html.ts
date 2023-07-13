699bfbcc3d88c512825e3bc2ce4254c16d6e8cbf
Kriszu <1246458990@qq.com>
2023-07-13 21:25:15 +0800

    [Improve]: update FE dependencies, use pnpm (#2852)
    * [Improve]: update FE dependencies, use pnpm
    
    * [Style]: eslint code

--- streampark-console/streampark-console-webapp/build/vite/plugin/html.ts
+++ streampark-console/streampark-console-webapp/build/vite/plugin/html.ts
@@ -19,38 +19,11 @@
  * https://github.com/anncwb/vite-plugin-html
  */
 import type { PluginOption } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import pkg from '../../../package.json';
-import { GLOB_CONFIG_FILE_NAME } from '../../constant';
+import { createHtmlPlugin } from 'vite-plugin-html-transform';
 
-export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
-  const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
-
-  const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
-
-  const getAppConfigSrc = () => {
-    return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
-  };
-
+export function configHtmlPlugin(isBuild: boolean) {
   const htmlPlugin: PluginOption[] = createHtmlPlugin({
     minify: isBuild,
-    inject: {
-      // Inject data into ejs template
-      data: {
-        title: VITE_GLOB_APP_TITLE,
-      },
-      // Embed the generated app.config.js file
-      tags: isBuild
-        ? [
-            {
-              tag: 'script',
-              attrs: {
-                src: getAppConfigSrc(),
-              },
-            },
-          ]
-        : [],
-    },
   });
   return htmlPlugin;
 }
--- streampark-console/streampark-console-webapp/src/utils/env.ts
+++ streampark-console/streampark-console-webapp/src/utils/env.ts
@@ -18,8 +18,11 @@ import type { GlobEnvConfig } from '/#/config';
 
 import { warn } from '/@/utils/log';
 import pkg from '../../package.json';
-import { getConfigFileName } from '../../build/getConfigFileName';
-
+const getVariableName = (title: string) => {
+  return `__PRODUCTION__${title.replace(/\s/g, '_').replace(/-/g, '_') || '__APP'}__CONF__`
+    .toUpperCase()
+    .replace(/\s/g, '');
+};
 export function getCommonStoragePrefix() {
   const { VITE_GLOB_APP_SHORT_NAME } = getAppEnvConfig();
   return `${VITE_GLOB_APP_SHORT_NAME}__${getEnv()}`.toUpperCase();
@@ -31,7 +34,7 @@ export function getStorageShortName() {
 }
 
 export function getAppEnvConfig() {
-  const ENV_NAME = getConfigFileName(import.meta.env);
+  const ENV_NAME = getVariableName(import.meta.env.VITE_GLOB_APP_TITLE);
 
   const ENV = (import.meta.env.DEV
     ? // Get the global configuration (the configuration will be extracted independently when packaging)
