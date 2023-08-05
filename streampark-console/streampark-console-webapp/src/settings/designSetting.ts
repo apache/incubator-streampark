719112c1a6524b06f7186f464b62baf45687ecde
benjobs <benjobs@apache.org>
2023-08-05 12:14:40 +0800

    menu icon improvement

--- streampark-console/streampark-console-webapp/src/settings/designSetting.ts
+++ streampark-console/streampark-console-webapp/src/settings/designSetting.ts
@@ -22,7 +22,7 @@ export const darkMode = ThemeEnum.LIGHT;
 
 // app theme preset color
 export const APP_PRESET_COLOR_LIST: string[] = [
-  '#1890ff',
+  '#1677ff',
   '#0084f4',
   '#009688',
   '#536dfe',
