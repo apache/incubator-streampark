652ac869db655ab69203e006fe6ac72ca893afa9
ChengJie1053 <18033291053@163.com>
2023-08-14 11:50:20 +0800

    Extract common const FLINK_NATIVE_KUBERNETES_LABEL

--- streampark-flink/streampark-flink-kubernetes/src/main/scala/org/apache/streampark/flink/kubernetes/watcher/FlinkK8sEventWatcher.scala
+++ streampark-flink/streampark-flink-kubernetes/src/main/scala/org/apache/streampark/flink/kubernetes/watcher/FlinkK8sEventWatcher.scala
@@ -17,6 +17,7 @@
 
 package org.apache.streampark.flink.kubernetes.watcher
 
+import org.apache.streampark.common.conf.ConfigConst
 import org.apache.streampark.common.util.Logger
 import org.apache.streampark.flink.kubernetes.{FlinkK8sWatchController, KubernetesRetriever}
 import org.apache.streampark.flink.kubernetes.model.{K8sDeploymentEventCV, K8sEventKey}
@@ -66,7 +67,7 @@ class FlinkK8sEventWatcher(implicit watchController: FlinkK8sWatchController)
     k8sClient
       .apps()
       .deployments()
-      .withLabel("type", "flink-native-kubernetes")
+      .withLabel("type", ConfigConst.FLINK_NATIVE_KUBERNETES_LABEL)
       .watch(new CompatibleKubernetesWatcher[Deployment, CompKubernetesDeployment] {
         override def eventReceived(action: Watcher.Action, event: Deployment): Unit = {
           handleDeploymentEvent(action, event)
