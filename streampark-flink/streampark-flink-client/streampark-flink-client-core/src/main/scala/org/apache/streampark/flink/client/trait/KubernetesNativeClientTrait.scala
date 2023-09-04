499d5b0e13bd423466ae4db6b730995ac848ce61
caicancai <2356672992@qq.com>
2023-09-04 18:42:01 +0800

    Modify the default path of kube system

--- streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/trait/KubernetesNativeClientTrait.scala
+++ streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/trait/KubernetesNativeClientTrait.scala
@@ -177,6 +177,6 @@ trait KubernetesNativeClientTrait extends FlinkClientTrait {
   def getDefaultKubernetesConf(k8sConf: String): String = {
     val homePath: String = System.getProperty("user.home")
     if (k8sConf != null) k8sConf.replace("~", homePath)
-    else homePath.concat("/.kube/config")
+    else homePath.concat("~/.kube/config")
   }
 }
