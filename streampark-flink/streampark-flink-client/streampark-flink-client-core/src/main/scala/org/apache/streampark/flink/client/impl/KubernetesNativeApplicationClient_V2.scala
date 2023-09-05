b69df1e9f9b1773b32cdfea20516f7fd8ab03a1c
caicancai <2356672992@qq.com>
2023-09-05 20:00:54 +0800

    add KubernetesNativeApplicationClient_V2

--- streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/impl/KubernetesNativeApplicationClient_V2.scala
+++ streampark-flink/streampark-flink-client/streampark-flink-client-core/src/main/scala/org/apache/streampark/flink/client/impl/KubernetesNativeApplicationClient_V2.scala
@@ -1,39 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.streampark.flink.client.impl
 
 import org.apache.streampark.common.enums.ExecutionMode
 import org.apache.streampark.common.util.Utils
+import org.apache.streampark.common.zio.ZIOExt.IOOps
 import org.apache.streampark.flink.client.`trait`.KubernetesNativeClientTrait
 import org.apache.streampark.flink.client.bean._
+import org.apache.streampark.flink.kubernetes.v2.model.{FlinkDeploymentDef, JobDef, JobManagerDef, TaskManagerDef}
+import org.apache.streampark.flink.kubernetes.v2.operator.FlinkK8sOperator
 import org.apache.streampark.flink.packer.pipeline.DockerImageBuildResponse
 
 import com.google.common.collect.Lists
 import org.apache.commons.lang3.StringUtils
-import org.apache.flink.client.deployment.application.ApplicationConfiguration
 import org.apache.flink.client.program.ClusterClient
-import org.apache.flink.configuration.{Configuration, DeploymentOptions, PipelineOptions}
-import org.apache.flink.kubernetes.KubernetesClusterDescriptor
+import org.apache.flink.configuration.{Configuration, DeploymentOptions, JobManagerOptions, PipelineOptions, TaskManagerOptions}
 import org.apache.flink.kubernetes.configuration.KubernetesConfigOptions
+import org.apache.flink.v1beta1.FlinkDeploymentSpec.FlinkVersion
 
-/** kubernetes native application mode submit */
-object KubernetesNativeApplicationClient extends KubernetesNativeClientTrait {
+import scala.language.postfixOps
 
+object KubernetesNativeApplicationClient_V2 extends KubernetesNativeClientTrait {
   @throws[Exception]
   override def doSubmit(
       submitRequest: SubmitRequest,
@@ -58,24 +43,10 @@ object KubernetesNativeApplicationClient extends KubernetesNativeClientTrait {
     // add flink container image tag to flink configuration
     flinkConfig.safeSet(KubernetesConfigOptions.CONTAINER_IMAGE, buildResult.flinkImageTag)
 
-    // retrieve k8s cluster and submit flink job on application mode
-    var clusterDescriptor: KubernetesClusterDescriptor = null
-    var clusterClient: ClusterClient[String] = null
-
     try {
-      val (descriptor, clusterSpecification) = getK8sClusterDescriptorAndSpecification(flinkConfig)
-      clusterDescriptor = descriptor
-      val applicationConfig = ApplicationConfiguration.fromConfiguration(flinkConfig)
-      clusterClient = clusterDescriptor
-        .deployApplicationCluster(clusterSpecification, applicationConfig)
-        .getClusterClient
-
-      val clusterId = clusterClient.getClusterId
-      val result = SubmitResponse(
-        clusterId,
-        flinkConfig.toMap,
-        submitRequest.jobId,
-        clusterClient.getWebInterfaceURL)
+      val spec: FlinkDeploymentDef = convertFlinkDeploymentDef(submitRequest, flinkConfig)
+      FlinkK8sOperator.deployApplicationJob(submitRequest.id, spec).runIO
+      val result = SubmitResponse(null, flinkConfig.toMap, submitRequest.jobId, null)
       logInfo(
         s"[flink-submit] flink job has been submitted. ${flinkConfIdentifierInfo(flinkConfig)}")
       result
@@ -83,9 +54,7 @@ object KubernetesNativeApplicationClient extends KubernetesNativeClientTrait {
       case e: Exception =>
         logError(s"submit flink job fail in ${submitRequest.executionMode} mode")
         throw e
-    } finally {
-      Utils.close(clusterDescriptor, clusterClient)
-    }
+    } finally {}
   }
 
   override def doCancel(
@@ -102,5 +71,39 @@ object KubernetesNativeApplicationClient extends KubernetesNativeClientTrait {
       flinkConf: Configuration): SavepointResponse = {
     flinkConf.safeSet(DeploymentOptions.TARGET, ExecutionMode.KUBERNETES_NATIVE_APPLICATION.getName)
     super.doTriggerSavepoint(request, flinkConf)
+  }
+
+  private[this] def convertFlinkDeploymentDef(
+      submitRequest: SubmitRequest,
+      flinkConfig: Configuration): FlinkDeploymentDef = {
+    val spec = FlinkDeploymentDef(
+      name = submitRequest.appName,
+      namespace = submitRequest.k8sSubmitParam.kubernetesNamespace,
+      image = KubernetesConfigOptions.CONTAINER_IMAGE.defaultValue(),
+      flinkVersion = Option(submitRequest.flinkVersion.majorVersion)
+        .map(_.replace(".", "_"))
+        .map("V" + _)
+        .flatMap(v => FlinkVersion.values().find(_.name() == v)) match {
+        case Some(version) => version
+        case None => throw new IllegalArgumentException("Flink version not found")
+      },
+      jobManager = JobManagerDef(
+        cpu = 1,
+        memory = flinkConfig.get(JobManagerOptions.TOTAL_PROCESS_MEMORY).toString),
+      taskManager = TaskManagerDef(
+        cpu = 1,
+        memory = flinkConfig.get(TaskManagerOptions.TOTAL_PROCESS_MEMORY).toString),
+      job = Some(
+        JobDef(
+          jarURI =
+            submitRequest.buildResult.asInstanceOf[DockerImageBuildResponse].dockerInnerMainJarPath,
+          parallelism = 1
+        )),
+      extJarPaths = submitRequest.userJarFile match {
+        case null => Array.empty[String]
+        case file => Array(file.getAbsolutePath)
+      }
+    )
+    spec
   }
 }
