0e13f6655e265cc49a4264c462ced42009931551
lenon <lenon.here@live.com>
2023-09-07 09:59:35 +0800

    add comments

--- streampark-flink/streampark-flink-connector/streampark-flink-connector-base/src/test/scala/org/apache/streampark/flink/connector/failover/SinkRequestTest.scala
+++ streampark-flink/streampark-flink-connector/streampark-flink-connector-base/src/test/scala/org/apache/streampark/flink/connector/failover/SinkRequestTest.scala
@@ -7,6 +7,7 @@ import scala.collection.JavaConverters._
 class SinkRequestTest {
   @Test
   def sqlStatement(): Unit = {
+    // input statements
     val statementsList = List(
       "insert into table_1(col1, col2) values(1, 2)",
       "insert into table_1(col1, col2) values(11, 22)",
@@ -16,12 +17,14 @@ class SinkRequestTest {
 
     val sinkRequest = SinkRequest(statementsList.asJava)
 
+    // expected result
     val expectedSqlStatement = List(
       "insert into table_2(col1, col2, col3) VALUES (11, 22, 33)",
       "insert into table_1(col1, col2) VALUES (1, 2),(11, 22)",
       "insert into table_1(col1, col2, col3) VALUES (11, 22, 33)",
     )
 
+    // comparison of result should be based on Set, that is, there is no need to care about the order of elements
     Assertions.assertTrue(sinkRequest.sqlStatement.toSet == expectedSqlStatement.toSet)
 
   }
