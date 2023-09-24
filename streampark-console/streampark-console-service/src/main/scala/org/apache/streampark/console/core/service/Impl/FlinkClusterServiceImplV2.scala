e072c58d6d43ca1a02befdf66c4c3e42ad3c6b2c
caicancai <2356672992@qq.com>
2023-09-24 18:18:46 +0800

    mvn spotless:apply

--- streampark-console/streampark-console-service/src/main/scala/org/apache/streampark/console/core/service/Impl/FlinkClusterServiceImplV2.scala
+++ streampark-console/streampark-console-service/src/main/scala/org/apache/streampark/console/core/service/Impl/FlinkClusterServiceImplV2.scala
@@ -3,22 +3,21 @@ package org.apache.streampark.console.core.service.Impl
 import org.apache.streampark.common.util.Logger
 import org.apache.streampark.common.zio.ZIOExt.IOOps
 import org.apache.streampark.flink.client.bean.{DeployRequest, ShutDownRequest}
-
 import org.apache.streampark.flink.kubernetes.v2.operator.FlinkK8sOperator
 
 import scala.util.{Failure, Success}
 
-object FlinkClusterServiceImplV2 extends Logger{
+object FlinkClusterServiceImplV2 extends Logger {
 
   @throws[Throwable]
-  def shutdown(shutDownRequest: ShutDownRequest) : Unit = {
-    val name = shutDownRequest.clusterId
+  def shutdown(shutDownRequest: ShutDownRequest): Unit = {
+    val name      = shutDownRequest.clusterId
     val namespace = shutDownRequest.kubernetesDeployParam.kubernetesNamespace
 
     def richMsg: String => String = s"[flink-shutdown][clusterId=$name][namespace=$namespace] " + _
 
     FlinkK8sOperator.k8sCrOpr.deleteSessionJob(namespace, name).runIOAsTry match {
-      case Success(_) =>
+      case Success(_)   =>
         logInfo(richMsg("Shutdown Flink cluster successfully."))
       case Failure(err) =>
         logError(richMsg(s"Fail to shutdown Flink cluster"), err)
