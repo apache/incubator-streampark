7a6f75e121602d03e672adc742c7d3d2fee83344
ChengJie1053 <18033291053@163.com>
2023-10-24 11:32:20 +0800

    Modifying checkVersion

--- streampark-common/src/main/scala/org/apache/streampark/common/conf/FlinkVersion.scala
+++ streampark-common/src/main/scala/org/apache/streampark/common/conf/FlinkVersion.scala
@@ -23,7 +23,6 @@ import java.net.{URL => NetURL}
 import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 import java.util.function.Consumer
 import java.util.regex.Pattern
-
 import scala.collection.convert.ImplicitConversions._
 import scala.collection.mutable
 
@@ -113,7 +112,7 @@ class FlinkVersion(val flinkHome: String) extends java.io.Serializable with Logg
 
   def checkVersion(throwException: Boolean = true): Boolean = {
     version.split("\\.").map(_.trim.toInt) match {
-      case Array(1, v, _) if v >= 12 && v <= 17 => true
+      case Array(1, v, _) if v >= 12 && v <= 18 => true
       case _ =>
         if (throwException) {
           throw new UnsupportedOperationException(s"Unsupported flink version: $version")
