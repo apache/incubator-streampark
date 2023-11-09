9a4b46bbcfcc9335ae8a418b171ee310aa4e2b60
Kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2023-11-09 14:42:37 +0800

    fix: Declare the license file

--- streampark-console/streampark-console-webapp/build/vite/utils/hash.ts
+++ streampark-console/streampark-console-webapp/build/vite/utils/hash.ts
@@ -1,24 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import { createHash } from 'node:crypto';
+// #!/usr/bin/env node
 
-function createContentHash(content: string, hashLSize = 12) {
-  const hash = createHash('sha256').update(content);
-  return hash.digest('hex').slice(0, hashLSize);
-}
+import { runBuildConfig } from './buildConf';
+import colors from 'picocolors';
 
-export { createContentHash };
+import pkg from '../../package.json';
+
+export const runBuild = async () => {
+  try {
+    const argvList = process.argv.splice(2);
+
+    // Generate configuration file
+    if (!argvList.includes('disabled-config')) {
+      runBuildConfig();
+    }
+
+    console.log(`✨ ${colors.cyan(`[${pkg.name}]`)}` + ' - build successfully!');
+  } catch (error) {
+    console.log(colors.red('vite build error:\n' + error));
+    process.exit(1);
+  }
+};
+runBuild();
