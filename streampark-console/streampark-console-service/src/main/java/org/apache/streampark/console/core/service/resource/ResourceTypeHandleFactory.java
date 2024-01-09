196a707f1d69eb12629d266974c8671d662f4e3a
monrg <wangmonrg@gmail.com>
2024-01-09 19:15:50 +0800

    [Improve] improve resource management logic

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/resource/ResourceTypeHandleFactory.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/resource/ResourceTypeHandleFactory.java
@@ -20,7 +20,7 @@ package org.apache.streampark.console.core.service.resource;
 import org.apache.streampark.console.base.domain.RestResponse;
 import org.apache.streampark.console.core.entity.Resource;
 import org.apache.streampark.console.core.enums.ResourceTypeEnum;
-import org.apache.streampark.console.core.service.impl.ResourceServiceImpl;
+import org.apache.streampark.console.core.service.ResourceService;
 import org.apache.streampark.console.core.service.resource.impl.AbstractResourceHandle;
 import org.apache.streampark.console.core.service.resource.impl.ConnectorResourceHandle;
 import org.apache.streampark.console.core.service.resource.impl.FlinkAppResourceHandle;
@@ -30,7 +30,7 @@ import com.google.common.collect.ImmutableMap;
 public class ResourceTypeHandleFactory {
 
   public static ResourceHandle getResourceHandle(
-      ResourceTypeEnum type, ResourceServiceImpl resourceService) {
+      ResourceTypeEnum type, ResourceService resourceService) {
     switch (type) {
       case FLINK_APP:
         return new FlinkAppResourceHandle(resourceService);
