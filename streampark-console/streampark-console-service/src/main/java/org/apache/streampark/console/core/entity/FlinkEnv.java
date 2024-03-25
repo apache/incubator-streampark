9f43bee9cd1b4ca0c5ed85a930cb6c7b36e0e442
qinjiyong <qinjiyong@jorme.sg>
2024-03-25 15:33:38 +0800

    flink from 1.19 corresponding to the conf directory no flink-conf.yaml, has become conf.yaml, so the code should be compatible

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/entity/FlinkEnv.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/entity/FlinkEnv.java
@@ -41,99 +41,102 @@ import java.util.Map;
 @TableName("t_flink_env")
 public class FlinkEnv implements Serializable {
 
-  @TableId(type = IdType.AUTO)
-  private Long id;
+    @TableId(type = IdType.AUTO)
+    private Long id;
 
-  private String flinkName;
+    private String flinkName;
 
-  private String flinkHome;
+    private String flinkHome;
 
-  private String flinkConf;
+    private String flinkConf;
 
-  private String description;
+    private String description;
 
-  private String scalaVersion;
+    private String scalaVersion;
 
-  private String version;
+    private String version;
 
-  /** is default */
-  private Boolean isDefault;
+    /**
+     * is default
+     */
+    private Boolean isDefault;
 
-  private Date createTime;
+    private Date createTime;
 
-  private transient FlinkVersion flinkVersion;
+    private transient FlinkVersion flinkVersion;
 
-  private transient String versionOfLarge;
+    private transient String versionOfLarge;
 
-  private transient String versionOfMiddle;
+    private transient String versionOfMiddle;
 
-  private transient String versionOfLast;
+    private transient String versionOfLast;
 
-  private transient String streamParkScalaVersion = scala.util.Properties.versionNumberString();
+    private transient String streamParkScalaVersion = scala.util.Properties.versionNumberString();
 
-  public void doSetFlinkConf() throws ApiDetailException {
-    try {
-      File yaml = new File(this.flinkHome.concat("/conf/flink-conf.yaml"));
-      String flinkConf = FileUtils.readFileToString(yaml, StandardCharsets.UTF_8);
-      this.flinkConf = DeflaterUtils.zipString(flinkConf);
-    } catch (Exception e) {
-      throw new ApiDetailException(e);
+    public void doSetFlinkConf() throws ApiDetailException {
+        try {
+            File yaml = new File(this.flinkHome.concat("/conf/flink-conf.yaml"));
+            if (!yaml.exists()) yaml = new File(this.flinkHome.concat("/conf/conf.yaml"));
+            String flinkConf = FileUtils.readFileToString(yaml, StandardCharsets.UTF_8);
+            this.flinkConf = DeflaterUtils.zipString(flinkConf);
+        } catch (Exception e) {
+            throw new ApiDetailException(e);
+        }
     }
-  }
 
-  public void doSetVersion() {
-    this.setVersion(this.getFlinkVersion().version());
-    this.setScalaVersion(this.getFlinkVersion().scalaVersion());
-    if (!streamParkScalaVersion.startsWith(this.getFlinkVersion().scalaVersion())) {
-      throw new UnsupportedOperationException(
-          String.format(
-              "The current Scala version of StreamPark is %s, but the scala version of Flink to be added is %s, which does not match, Please check",
-              streamParkScalaVersion, this.getFlinkVersion().scalaVersion()));
+    public void doSetVersion() {
+        this.setVersion(this.getFlinkVersion().version());
+        this.setScalaVersion(this.getFlinkVersion().scalaVersion());
+        if (!streamParkScalaVersion.startsWith(this.getFlinkVersion().scalaVersion())) {
+            throw new UnsupportedOperationException(
+                String.format(
+                    "The current Scala version of StreamPark is %s, but the scala version of Flink to be added is %s, which does not match, Please check",
+                    streamParkScalaVersion, this.getFlinkVersion().scalaVersion()));
+        }
     }
-  }
 
-  public Map<String, String> convertFlinkYamlAsMap() {
-    String flinkYamlString = DeflaterUtils.unzipString(flinkConf);
-    return PropertiesUtils.loadFlinkConfYaml(flinkYamlString);
-  }
-
-  @JsonIgnore
-  public FlinkVersion getFlinkVersion() {
-    if (this.flinkVersion == null) {
-      this.flinkVersion = new FlinkVersion(this.flinkHome);
+    public Map<String, String> convertFlinkYamlAsMap() {
+        String flinkYamlString = DeflaterUtils.unzipString(flinkConf);
+        return PropertiesUtils.loadFlinkConfYaml(flinkYamlString);
     }
-    return this.flinkVersion;
-  }
 
-  public void unzipFlinkConf() {
-    this.flinkConf = DeflaterUtils.unzipString(this.flinkConf);
-  }
-
-  public String getLargeVersion() {
-    if (StringUtils.isNotBlank(this.version)) {
-      return this.version.substring(0, this.version.lastIndexOf("."));
+    @JsonIgnore
+    public FlinkVersion getFlinkVersion() {
+        if (this.flinkVersion == null) {
+            this.flinkVersion = new FlinkVersion(this.flinkHome);
+        }
+        return this.flinkVersion;
     }
-    return null;
-  }
 
-  public String getVersionOfFirst() {
-    if (StringUtils.isNotBlank(this.version)) {
-      return this.version.split("\\.")[0];
+    public void unzipFlinkConf() {
+        this.flinkConf = DeflaterUtils.unzipString(this.flinkConf);
     }
-    return null;
-  }
 
-  public String getVersionOfMiddle() {
-    if (StringUtils.isNotBlank(this.version)) {
-      return this.version.split("\\.")[1];
+    public String getLargeVersion() {
+        if (StringUtils.isNotBlank(this.version)) {
+            return this.version.substring(0, this.version.lastIndexOf("."));
+        }
+        return null;
     }
-    return null;
-  }
 
-  public String getVersionOfLast() {
-    if (StringUtils.isNotBlank(this.version)) {
-      return this.version.split("\\.")[2];
+    public String getVersionOfFirst() {
+        if (StringUtils.isNotBlank(this.version)) {
+            return this.version.split("\\.")[0];
+        }
+        return null;
     }
-    return null;
-  }
+
+    public String getVersionOfMiddle() {
+        if (StringUtils.isNotBlank(this.version)) {
+            return this.version.split("\\.")[1];
+        }
+        return null;
+    }
+
+    public String getVersionOfLast() {
+        if (StringUtils.isNotBlank(this.version)) {
+            return this.version.split("\\.")[2];
+        }
+        return null;
+    }
 }
