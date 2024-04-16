2df7c90030b3e9b79ccc2e67b08c40382c0c1128
jianjun.xu <128395511+jianjun159@users.noreply.github.com>
2024-04-16 16:40:47 +0800

    [ISSUES-3667] KubernetesClusterDescriptor not closed after use, cause the file handle resource leaks (#3668)
    [ISSUES-3667] KubernetesClusterDescriptor not closed after use, cause the file handle resource leaks
    ---------
    
    Co-authored-by: jianjun.xu <jianjun.xu@ly.com>

--- streampark-flink/streampark-flink-kubernetes/src/main/scala/org/apache/streampark/flink/kubernetes/KubernetesRetriever.scala
+++ streampark-flink/streampark-flink-kubernetes/src/main/scala/org/apache/streampark/flink/kubernetes/KubernetesRetriever.scala
@@ -18,8 +18,8 @@
 package org.apache.streampark.flink.kubernetes
 
 import org.apache.streampark.common.conf.ConfigKeys
+import org.apache.streampark.common.util.{Logger, Utils}
 import org.apache.streampark.common.util.ImplicitsUtils._
-import org.apache.streampark.common.util.Logger
 import org.apache.streampark.flink.kubernetes.enums.FlinkK8sExecuteModeEnum
 import org.apache.streampark.flink.kubernetes.ingress.IngressController
 import org.apache.streampark.flink.kubernetes.model.ClusterKey
@@ -83,22 +83,22 @@ object KubernetesRetriever extends Logger {
       flinkConfig.setString(KubernetesConfigOptions.NAMESPACE, namespace)
     }
     // retrieve flink cluster client
-    val clientFactory: ClusterClientFactory[String] =
-      clusterClientServiceLoader.getClusterClientFactory(flinkConfig)
-
-    val clusterProvider: KubernetesClusterDescriptor =
-      clientFactory.createClusterDescriptor(flinkConfig).asInstanceOf[KubernetesClusterDescriptor]
-
-    Try {
-      clusterProvider
-        .retrieve(flinkConfig.getString(KubernetesConfigOptions.CLUSTER_ID))
-        .getClusterClient
-    } match {
-      case Success(v) => Some(v)
-      case Failure(e) =>
-        logError(s"Get flinkClient error, the error is: $e")
-        None
-    }
+    clusterClientServiceLoader
+      .getClusterClientFactory(flinkConfig)
+      .createClusterDescriptor(flinkConfig)
+      .asInstanceOf[KubernetesClusterDescriptor]
+      .autoClose(
+        clusterProvider =>
+          Try {
+            clusterProvider
+              .retrieve(flinkConfig.getString(KubernetesConfigOptions.CLUSTER_ID))
+              .getClusterClient
+          } match {
+            case Success(v) => Some(v)
+            case Failure(e) =>
+              logError(s"Get flinkClient error, the error is: $e")
+              None
+          })
   }
 
   /**
