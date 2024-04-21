d551dd40e804f438f0350c899943558585243a8b
caicancai <2356672992@qq.com>
2024-04-21 23:02:46 +0800

    [Feature] apache flink 1.19 support
    Co-authored-by: benjobs <benjobx@gmail.com>

--- streampark-common/src/main/scala/org/apache/streampark/common/conf/FlinkVersion.scala
+++ streampark-common/src/main/scala/org/apache/streampark/common/conf/FlinkVersion.scala
@@ -115,7 +115,7 @@ class FlinkVersion(val flinkHome: String) extends java.io.Serializable with Logg
 
   def checkVersion(throwException: Boolean = true): Boolean = {
     version.split("\\.").map(_.trim.toInt) match {
-      case Array(1, v, _) if v >= 12 && v <= 18 => true
+      case Array(1, v, _) if v >= 12 && v <= 19 => true
       case _ =>
         if (throwException) {
           throw new UnsupportedOperationException(s"Unsupported flink version: $version")
