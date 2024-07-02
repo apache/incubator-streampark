b54b3298dedf6700e00042025150753774b19f6f
zzzk1 <madzhou1@gmail.com>
2024-07-03 05:27:46 +0800

    Introduce role management e2e test

---
+++ streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/cases/RoleManagementTest.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.apache.streampark.e2e.cases;
+
+import org.apache.streampark.e2e.core.StreamPark;
+import org.apache.streampark.e2e.pages.LoginPage;
+import org.apache.streampark.e2e.pages.system.RoleManagementPage;
+import org.apache.streampark.e2e.pages.system.SystemPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@StreamPark(composeFiles = "docker/basic/docker-compose.yaml")
+public class RoleManagementTest {
+  private static RemoteWebDriver browser;
+
+  private static final String userName = "admin";
+
+  private static final String password = "streampark";
+
+  private static final String teamName = "default";
+
+  private static final String newRoleName = "new_role";
+
+  private static final String newDescription = "new_description";
+
+    private static final String existMenuName = "Apache Flink";
+
+  @BeforeAll
+  public static void setup() {
+    new LoginPage(browser)
+        .login(userName, password, teamName)
+        .goToNav(SystemPage.class)
+        .goToTab(RoleManagementPage.class);
+  }
+
+  @Test
+  @Order(10)
+  void testCreateUser() {
+    final RoleManagementPage roleManagementPage = new RoleManagementPage(browser);
+      roleManagementPage.createRole(
+        newRoleName, newDescription, existMenuName);
+
+    Awaitility.await()
+        .untilAsserted(
+            () ->
+                assertThat(roleManagementPage.roleList())
+                    .as("Role list should contain newly-created role")
+                    .extracting(WebElement::getText)
+                    .anyMatch(it -> it.contains(newRoleName)));
+  }
+
+  @Test
+  @Order(20)
+  void testCreateDuplicateRole() {
+      final RoleManagementPage roleManagementPage = new RoleManagementPage(browser);
+      roleManagementPage.createRole(newRoleName, newDescription, existMenuName);
+
+      Awaitility.await()
+          .untilAsserted(
+              () ->
+                  assertThat(roleManagementPage.errorMessageList())
+                      .as("Role Name Duplicated Error message should be displayed")
+                      .extracting(WebElement::getText)
+                      .anyMatch(it -> it.contains("Sorry, the role name already exists")));
+
+      roleManagementPage.createRoleForm().buttonCancel().click();
+  }
+    @Test
+    @Order(30)
+    void testEditRole() {
+        final RoleManagementPage roleManagementPage = new RoleManagementPage(browser);
+
+        String newEditDescription = newDescription + "_edit";
+        String newEditMenuName = "System";
+        roleManagementPage.editRole(
+            newRoleName, newEditDescription, newEditMenuName);
+
+        //TODO: there is no description filed value actual exist
+//        Awaitility.await()
+//            .untilAsserted(
+//                () ->
+//                    assertThat(roleManagementPage.roleList())
+//                        .as("Role list should contain newly-created role")
+//                        .extracting(WebElement::getText)
+//                        .anyMatch(it -> it.contains(newEditDescription)));
+    }
+
+    @Test
+    @Order(40)
+    void testDeleteRole() {
+      final RoleManagementPage roleManagementPage = new RoleManagementPage(browser);
+
+      roleManagementPage.deleteRole(newRoleName);
+
+        Awaitility.await()
+            .untilAsserted(
+                () ->
+                    assertThat(roleManagementPage.roleList())
+                        .extracting(WebElement::getText)
+                        .noneMatch(it -> it.contains(newRoleName)));
+    }
+}
