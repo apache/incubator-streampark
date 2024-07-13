94e3481d16a297a387adf48dfd5fbe3950dca89c
benjobs <benjobs@apache.org>
2024-07-13 14:31:40 +0800

    [Improve] flink on yarn e2e testcase

--- streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/cases/Flink116OnYarnTest.java
+++ streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/cases/Flink116OnYarnTest.java
@@ -37,8 +37,8 @@ import org.testcontainers.shaded.org.awaitility.Awaitility;
 import static org.apache.streampark.e2e.core.Constants.TEST_FLINK_SQL;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@StreamPark(composeFiles = "docker/flink-1.17-on-yarn/docker-compose.yaml")
-public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
+@StreamPark(composeFiles = "docker/flink-1.16-on-yarn/docker-compose.yaml")
+public class Flink116OnYarnTest {
 
     private static RemoteWebDriver browser;
 
@@ -48,11 +48,11 @@ public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
 
     private static final String teamName = "default";
 
-    private static final String flinkName = "flink-1.17.2";
+    private static final String flinkName = "flink-1.16.3";
 
-    private static final String flinkHome = "/flink-1.17.2";
+    private static final String flinkHome = "/flink-1.16.3";
 
-    private static final String applicationName = "flink-117-e2e-test";
+    private static final String applicationName = "flink-116-e2e-test";
 
     @BeforeAll
     public static void setup() {
@@ -106,8 +106,8 @@ public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
                     .anyMatch(it -> it.contains("SUCCESS")));
     }
 
-    // @Test
-    // @Order(30)
+    @Test
+    @Order(30)
     void testStartFlinkApplicationOnYarnApplicationMode() {
         final ApplicationsPage applicationsPage = new ApplicationsPage(browser);
 
@@ -128,8 +128,8 @@ public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
                     .anyMatch(it -> it.contains("FINISHED")));
     }
 
-    // @Test
-    // @Order(31)
+    @Test
+    @Order(31)
     @SneakyThrows
     void testRestartAndCancelFlinkApplicationOnYarnApplicationMode() {
         Thread.sleep(Constants.DEFAULT_SLEEP_MILLISECONDS);
@@ -177,7 +177,6 @@ public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
         final ApplicationsPage applicationsPage = new ApplicationsPage(browser);
 
         ApplicationsDynamicParams applicationsDynamicParams = new ApplicationsDynamicParams();
-
         applicationsDynamicParams.flinkSQL(TEST_FLINK_SQL);
         applicationsPage
             .createApplication()
@@ -211,8 +210,8 @@ public class ApplicationsFlink117OnYarnWithFlinkSQLTest {
                     .anyMatch(it -> it.contains("SUCCESS")));
     }
 
-    // @Test
-    // @Order(70)
+    @Test
+    @Order(70)
     void testStartFlinkApplicationOnYarnPerJobMode() {
         final ApplicationsPage applicationsPage = new ApplicationsPage(browser);
 
