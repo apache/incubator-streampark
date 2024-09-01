08ec771d8aca015cee02a6ab422b129f4b8d36fa
BUAAserein <18376359@buaa.edu.cn>
2024-09-01 16:32:43 +0800

    Revert "add test"
    This reverts commit 760e39654eb5eda57dfde6c022e6d8f6b1913b45.

--- streampark-console/streampark-console-service/src/test/java/org/apache/streampark/console/base/util/ConsistentHashTest.java
+++ streampark-console/streampark-console-service/src/test/java/org/apache/streampark/console/base/util/ConsistentHashTest.java
@@ -17,75 +17,50 @@
 
 package org.apache.streampark.console.base.util;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-@Slf4j
 public class ConsistentHashTest {
 
-    Long startTime;
-
-    List<String> servers;
-    
-    ConsistentHash<String> hash;
-    
-    Integer jobNum = 300000;
-
+    List<String> servers = Arrays.asList("Server-A", "Server-B", "Server-C", "Server-D", "Server-E");
+    ConsistentHash<String> hash = new ConsistentHash<>(servers);
+    Integer jobNum = 100000;
     List<Integer> jobIds = new ArrayList<>();
 
     @BeforeEach
     public void init() {
-        startTime = System.currentTimeMillis();
-        servers = new ArrayList<>(Arrays.asList("Server-A", "Server-B", "Server-C"));
-        hash = new ConsistentHash<>(servers);
-        for (int i = 0; i < jobNum; i ++) {
+        for (int i = 1; i <= jobNum; i ++) {
             jobIds.add(i);
         }
     }
 
+
     @Test
     public void initWatch() {
-        Map<String, Long> counts = jobIds.stream()
-        .map(hash::get)
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        counts.forEach((k, v) -> {
-            log.info("node:{}, initWatch count:{}", k, v);
-        });
-        log.info("time: {}ms", System.currentTimeMillis() - startTime);
+        List<Integer> counts = Arrays.asList(0, 0, 0, 0, 0);
+        for (int i : jobIds) {
+            String v = hash.get(i);
+            switch (v) {
+        }
     }
 
     @Test
-    public void addServer() {
-        servers.add("Server-D");
-        hash.add("Server-D");
-        Map<String, Long> counts = jobIds.stream()
-        .map(hash::get)
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        counts.forEach((k, v) -> {
-            log.info("node:{}, addServer count:{}", k, v);
-        });
-        log.info("time: {}ms", System.currentTimeMillis() - startTime);
+    public void addWatch() {
+        int jobId = 99;
+        String v = hash.get(99);
+        System.out.println("node:" + v + " , item:" + jobId);
     }
 
     @Test
-    public void removeServer() {
-        servers.remove("Server-C");
-        hash.remove("Server-C");
-        Map<String, Long> counts = jobIds.stream()
-        .map(hash::get)
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        counts.forEach((k, v) -> {
-            log.info("node:{}, removeServer count:{}", k, v);
-        });
-        log.info("time: {}ms", System.currentTimeMillis() - startTime);
+    public void stopServer() {
+        servers.remove("A");
+        int jobId = 99;
+        String v = hash.get(99);
+        System.out.println("node:" + v + " , item:" + jobId);
     }
 }
