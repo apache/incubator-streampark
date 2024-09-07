44caa359ffe260d59e10050bfc21928a9cbc41ac
benjobs <benjobs@apache.org>
2024-09-07 22:48:59 +0800

    Update ApplicationsPage.java

--- streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/pages/flink/applications/ApplicationsPage.java
+++ streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/pages/flink/applications/ApplicationsPage.java
@@ -38,7 +38,7 @@ import static org.apache.streampark.e2e.pages.common.CommonFactory.WebDriverWait
 @Getter
 public class ApplicationsPage extends NavBarPage implements ApacheFlinkPage.Tab {
 
-    @FindBy(id="e2e_app_create_btn")
+    @FindBy(id = "e2e_app_create_btn")
     private WebElement buttonCreateApplication;
 
     @FindBy(xpath = "//tbody[contains(@class, 'ant-table-tbody')]")
