daf4d0ef8d66f75a5b98d640c5a68cf7b5ef022e
benjobs <benjobs@apache.org>
2024-09-24 16:03:22 +0800

    [Improve] e2e flink-home improvements

--- streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/cases/Flink116OnYarnClusterDeployTest.java
+++ streampark-e2e/streampark-e2e-case/src/test/java/org/apache/streampark/e2e/cases/Flink116OnYarnClusterDeployTest.java
@@ -42,7 +42,7 @@ public class Flink116OnYarnClusterDeployTest {
 
     private static final String flinkName = "flink-1.16.3";
 
-    private static final String flinkHome = "/opt/flink/";
+    private static final String flinkHome = "/flink-1.16.3";
 
     private static final String flinkDescription = "description test";
 
