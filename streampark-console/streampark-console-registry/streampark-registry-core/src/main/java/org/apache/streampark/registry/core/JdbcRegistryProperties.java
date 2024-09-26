360ae5ac2d0abab51178a3bc7e1e446413b1c289
benjobs <benjobs@apache.org>
2024-09-26 15:07:42 +0800

    [Improve] Adjust the package name of the console-registry module. (#4093)
    * [Improve] spark app submit bug fixed.
    
    * [Improve] known-dependencies.txt updated
    
    * [Improve] Adjust the package name of the console-registry module.

---
+++ streampark-console/streampark-console-registry/streampark-registry-core/src/main/java/org/apache/streampark/registry/core/JdbcRegistryProperties.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.streampark.registry.core;
+
+import org.apache.streampark.common.utils.NetworkUtils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.zaxxer.hikari.HikariConfig;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.Duration;
+
+@Data
+@Slf4j
+@Validated
+@Configuration
+@ConditionalOnProperty(prefix = "registry", name = "type", havingValue = "jdbc")
+@ConfigurationProperties(prefix = "registry")
+public class JdbcRegistryProperties implements Validator {
+
+    @Autowired
+    private NetworkUtils networkUtils;
+
+    private static final Duration MIN_HEARTBEAT_REFRESH_INTERVAL = Duration.ofSeconds(1);
+
+    @Value("${server.port:10000}")
+    private int serverPort;
+
+    private String jdbcRegistryClientName;
+
+    private Duration heartbeatRefreshInterval = Duration.ofSeconds(3);
+    private Duration sessionTimeout = Duration.ofSeconds(60);
+    private HikariConfig hikariConfig;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return JdbcRegistryProperties.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        JdbcRegistryProperties jdbcRegistryProperties = (JdbcRegistryProperties) target;
+        if (jdbcRegistryProperties.getHeartbeatRefreshInterval().compareTo(MIN_HEARTBEAT_REFRESH_INTERVAL) < 0) {
+            errors.rejectValue("heartbeatRefreshInterval", "heartbeatRefreshInterval",
+                "heartbeatRefreshInterval must be greater than 1s");
+        }
+
+        if (jdbcRegistryProperties.getSessionTimeout().toMillis() < 3
+            * jdbcRegistryProperties.getHeartbeatRefreshInterval().toMillis()) {
+            errors.rejectValue("sessionTimeout", "sessionTimeout",
+                "sessionTimeout must be greater than 3 * heartbeatRefreshInterval");
+        }
+        if (StringUtils.isEmpty(jdbcRegistryClientName)) {
+            jdbcRegistryClientName = NetworkUtils.getHost() + ":" + serverPort;
+        }
+
+        print();
+    }
+
+    private void print() {
+        String config =
+            "\n****************************JdbcRegistryProperties**************************************" +
+                "\n  jdbcRegistryClientName -> " + jdbcRegistryClientName +
+                "\n  heartbeatRefreshInterval -> " + heartbeatRefreshInterval +
+                "\n  sessionTimeout -> " + sessionTimeout +
+                "\n hikariConfig -> " + hikariConfig +
+                "\n****************************JdbcRegistryProperties**************************************";
+        log.info(config);
+    }
+}
